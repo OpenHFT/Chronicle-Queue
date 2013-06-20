450eae3f9a143b4e8228d2b57fcd14dbf1eb0867
Peter Lawrey <peter.lawrey@gmail.com>
2013-06-19 20:57:02 -0400

    Add cleanup of directories.

--- src/main/java/net/openhft/chronicle/ChronicleTools.java
+++ src/main/java/net/openhft/chronicle/ChronicleTools.java
@@ -1,6 +1,8 @@
 package net.openhft.chronicle;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author peter.lawrey
@@ -20,5 +22,42 @@ public enum ChronicleTools {
             file.delete();
             file.deleteOnExit();
         }
+    }
+
+    enum DeleteStatic {
+        INSTANCE;
+
+        final List<String> toDeleteList = new ArrayList<String>();
+
+        {
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (String dir : toDeleteList) {
+                        System.out.println("Deleting " + dir);
+                        deleteDir(dir);
+                    }
+                }
+            }));
+        }
+
+        synchronized void add(String dirPath) {
+            deleteDir(dirPath);
+            toDeleteList.add(dirPath);
+        }
+
+        private void deleteDir(String dirPath) {
+            File dir = new File(dirPath);
+            // delete one level.
+            if (dir.isDirectory()) {
+                for (File file : dir.listFiles())
+                    file.delete();
+            }
+            dir.delete();
+        }
+    }
+
+    public static void deleteDirOnExit(String dirPath) {
+        DeleteStatic.INSTANCE.add(dirPath);
     }
 }
--- src/test/java/net/openhft/chronicle/RollingFilesTest.java
+++ src/test/java/net/openhft/chronicle/RollingFilesTest.java
@@ -3,6 +3,7 @@ package net.openhft.chronicle;
 import net.openhft.lang.Jvm;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -10,8 +11,19 @@ import java.io.IOException;
  */
 public class RollingFilesTest {
     @Test
+    public void findLast() throws IOException {
+        String dirPath = Jvm.TMP + File.separatorChar + "findLast";
+        ChronicleTools.deleteDirOnExit(dirPath);
+        RollingChronicle chronicle = new RollingChronicle(dirPath, ChronicleConfig.TEST);
+        chronicle.close();
+
+    }
+
+    @Test
     public void rollOnDemand() throws IOException {
-        RollingChronicle chronicle = new RollingChronicle(Jvm.TMP + "/rollOnDemand", ChronicleConfig.TEST);
+        String dirPath = Jvm.TMP + File.separatorChar + "rollOnDemand";
+        ChronicleTools.deleteDirOnExit(dirPath);
+        RollingChronicle chronicle = new RollingChronicle(dirPath, ChronicleConfig.TEST);
 
     }
 }
