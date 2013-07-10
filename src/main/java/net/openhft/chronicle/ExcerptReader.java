c26d1029aa6a1657e51615c7712b8aa3b30de9e3
Peter Lawrey <peter.lawrey@gmail.com>
2013-07-09 21:17:28 -0500

    Update the copyright

--- src/main/java/net/openhft/chronicle/ExcerptReader.java
+++ src/main/java/net/openhft/chronicle/ExcerptReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright ${YEAR} Peter Lawrey
+ * Copyright 2013 Peter Lawrey
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
