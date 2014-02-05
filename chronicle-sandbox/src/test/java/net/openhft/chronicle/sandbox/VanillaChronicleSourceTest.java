8d8da3b6e5de0b3b8a83679fda3a21528d32e202
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-02-04 20:28:21 -0500

    Update VanillaChronicleSourceTest.java
    Added import static org.junit.Assert.assertEquals

--- chronicle-sandbox/src/test/java/net/openhft/chronicle/sandbox/VanillaChronicleSourceTest.java
+++ chronicle-sandbox/src/test/java/net/openhft/chronicle/sandbox/VanillaChronicleSourceTest.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
