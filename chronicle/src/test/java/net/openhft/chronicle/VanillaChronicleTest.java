3ab6849d69aee6dfa20536f003998cf8045f8671
Rob Austin <rob.austin@boundedbuffer>
2014-04-11 16:21:22 +0100

    HCOLL-36 Release all the project using new team city setup

--- chronicle/src/test/java/net/openhft/chronicle/VanillaChronicleTest.java
+++ chronicle/src/test/java/net/openhft/chronicle/VanillaChronicleTest.java
@@ -17,7 +17,6 @@
 package net.openhft.chronicle;
 
 import net.openhft.lang.io.IOTools;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
@@ -88,8 +87,6 @@ public class VanillaChronicleTest {
     }
 
     @Test
-    @Ignore
-    // fails the release build only
     public void testAppend() throws IOException {
         final int RUNS = 1000;
         String baseDir = System.getProperty("java.io.tmpdir") + "/testAppend";
@@ -285,7 +282,6 @@ public class VanillaChronicleTest {
     }
 
     @Test
-    @Ignore
     public void testAppenderTailer() throws IOException {
         String basepath = System.getProperty("java.io.tmpdir") + "/test-appender-tailer";
 
@@ -333,7 +329,6 @@ public class VanillaChronicleTest {
     }
 
     @Test
-    @Ignore
     public void testTailerToStart() throws IOException {
         String basepath = System.getProperty("java.io.tmpdir") + "/test-tailer-tostart";
 
