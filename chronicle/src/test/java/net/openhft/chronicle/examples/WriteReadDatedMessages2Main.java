f4d65241425e5ef0c14f2847833eacb65da10e64
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2014-05-29 08:46:20 +0100

    Testing writing strings.

--- chronicle/src/test/java/net/openhft/chronicle/examples/WriteReadDatedMessages2Main.java
+++ chronicle/src/test/java/net/openhft/chronicle/examples/WriteReadDatedMessages2Main.java
@@ -25,8 +25,12 @@ import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.util.Date;
 
+/*
+Writing 1,000,000,000, the chronicle took 345.815 seconds
+Reading 1,000,000,000, the chronicle uses 66409812 KB, took 213.442 seconds
+ */
 public class WriteReadDatedMessages2Main {
-    public static void main(String... ignored) throws IOException {
+    public static void main(String... ignored) throws IOException, InterruptedException {
         String basePath = "/tmp/index";
         VanillaChronicleConfig config = new VanillaChronicleConfig();
         System.out.println("cycleFormat " + config.cycleFormat());
@@ -36,67 +40,40 @@ public class WriteReadDatedMessages2Main {
         System.out.println("entriesPerCycle " + config.entriesPerCycle());
         System.out.println("indexBlockSize " + config.indexBlockSize());
         System.out.println("synchronous " + config.synchronous());
-        System.out.println("MIN_CYCLE_LENGTH "
-                + VanillaChronicleConfig.MIN_CYCLE_LENGTH);
+
         VanillaChronicle chronicle = new VanillaChronicle(basePath);
-        int messages = 100000000;// 1000 * 50000;
+        long messages = 10 * 1000 * 1000L;// 1000 * 50000;
         chronicle.clear();
         long start = System.nanoTime();
-        VanillaChronicle.VanillaAppender appender;
-        int i = 0;
-        appender = chronicle.createAppender();
+        VanillaChronicle.VanillaAppender appender = chronicle.createAppender();
         String msg = "writer1 " + new Date();
-        // count of records to be written in this index
-        appender.startExcerpt();
-        appender.writeInt(messages);
-        appender.finish();
-        for (i = 1; i <= messages; i++) {
+        for (long i = 0; i < messages; i++) {
             appender.startExcerpt();
-            appender.writeInt(i);
+            appender.writeLong(i);
             appender.writeLong(System.currentTimeMillis());
             appender.writeUTFΔ(msg);
             appender.finish();
         }
 
-        System.out.println(chronicle.lastWrittenIndex());
-        System.out.printf("After %,d, the chronicle took %.3f seconds%n", i,
-                (System.nanoTime() - start) / 1e9);
-        System.out.println("writing done");
-        ExcerptTailer tailer;
-        tailer = chronicle.createTailer();
+        System.out.println(Long.toHexString(chronicle.lastWrittenIndex()));
+        System.out.printf("Writing %,d, the chronicle took %.3f seconds%n",
+                messages, (System.nanoTime() - start) / 1e9);
+
+        ExcerptTailer tailer = chronicle.createTailer();
         StringBuilder msgBuffer = new StringBuilder();
         start = System.nanoTime();
-        int counter = 0;
+        long counter = 0;
         while (tailer.nextIndex()) {
-            // read how much is written in this
-            if (counter == 0) {
-                counter = (tailer.readInt());
-                tailer.finish();
-                continue;
-            }
-            int readInt = tailer.readInt();
-            tailer.readLong();
+            long index = tailer.readLong();
+            long timeStamp = tailer.readLong();
             tailer.readUTFΔ(msgBuffer);
             tailer.finish();
-            if (readInt == counter) {
-                // reading complete
-                // do something
-            }
+            counter++;
         }
         long time = System.nanoTime() - start;
-        // why cannot I clear the chronicle, I get
-        // net.openhft.lang.io.IOTools deleteDir
-        // unable to delete E:\~4V\chronicle-data\index\20140529\data-5628-8
-        // chronicle.close();
-        // chronicle.clear();
 
-        System.out.printf("After %,d, the chronicle, took %.3f seconds%n",
-                messages, (time / 1e9));
-        // I run in Windows
-        /**System.out
-         .printf("After %,d, the chronicle uses %s KB, took %.3f seconds%n",
-         messages, run("du", "-s", basePath).split("\\s")[0],
-         time / 1e9);*/
+        System.out.printf("Reading %,d, the chronicle uses %s KB, took %.3f seconds%n",
+                counter, run("du", "-s", basePath).split("\\s")[0], time / 1e9);
     }
 
     static String run(String... cmd) throws IOException, InterruptedException {
