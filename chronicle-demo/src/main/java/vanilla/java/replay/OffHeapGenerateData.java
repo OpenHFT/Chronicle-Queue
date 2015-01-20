4960b83718f7a55b9393e6af4bc6a245f6008acb
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-01-20 22:09:48 +0000

    OffHeap example of write/read from chronicle queue using a generated data type.

---
+++ chronicle-demo/src/main/java/vanilla/java/replay/OffHeapGenerateData.java
@@ -0,0 +1,44 @@
+package vanilla.java.replay;
+
+import net.openhft.chronicle.Chronicle;
+import net.openhft.chronicle.ChronicleQueueBuilder;
+import net.openhft.chronicle.ExcerptAppender;
+import net.openhft.lang.model.DataValueClasses;
+
+import java.io.IOException;
+
+/**
+ * Created by peter on 20/01/15.
+ */
+public class OffHeapGenerateData {
+    /*
+    On an i7-3970X prints.
+
+    Took 1.392 seconds to write 10,000,000 records
+     */
+    static final long RECORDS = Long.getLong("RECORDS", 10000000);
+
+    public static void main(String[] args) throws IOException {
+        OffHeapTestData td = DataValueClasses.newDirectInstance(OffHeapTestData.class);
+        String path = "/tmp/test2";
+        StringBuilder name = new StringBuilder();
+
+        long start = System.nanoTime();
+        try (Chronicle chronicle = ChronicleQueueBuilder.indexed(path).build()) {
+            ExcerptAppender appender = chronicle.createAppender();
+            for (long i = 0; i < RECORDS; i++) {
+                name.setLength(0);
+                td.setName(name.append("Name").append(i));
+                td.setAge(i);
+                td.setImportance((double) i / RECORDS);
+                td.setTimestamp(System.currentTimeMillis());
+
+                appender.startExcerpt();
+                appender.write(td.bytes());
+                appender.finish();
+            }
+        }
+        System.out.printf("Took %.3f seconds to write %,d records%n",
+                (System.nanoTime() - start) / 1e9, RECORDS);
+    }
+}
---
+++ chronicle-demo/src/main/java/vanilla/java/replay/OffHeapTestData.java
@@ -0,0 +1,35 @@
+package vanilla.java.replay;
+
+import net.openhft.lang.model.Byteable;
+import net.openhft.lang.model.constraints.MaxSize;
+
+/**
+ * Created by peter on 20/01/15.
+ */
+public interface OffHeapTestData extends Byteable {
+    /*
+    CREATE TABLE testSet(
+        id BIGINT NOT NULL AUTO_INCREMENT,
+        name LONGTEXT NOT NULL,
+        age BIGINT NOT NULL,
+        importance DOUBLE NOT NULL,
+        time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+    ) ENGINE=InnoDB//
+    */
+    void setName(@MaxSize(16) CharSequence name);
+
+    String getName();
+
+    void setAge(long age);
+
+    long getAge();
+
+    void setImportance(double importance);
+
+    double getImportance();
+
+    void setTimestamp(long timestamp);
+
+    long getTimestamp();
+}
