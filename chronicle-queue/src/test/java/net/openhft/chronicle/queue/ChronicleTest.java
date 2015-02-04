0ef6864573cd9af59ed04957f38f9b5e7e2920c4
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-02-04 12:39:45 +0000

    Fix a positioning bug.

--- chronicle-queue/src/test/java/net/openhft/chronicle/queue/ChronicleTest.java
+++ chronicle-queue/src/test/java/net/openhft/chronicle/queue/ChronicleTest.java
@@ -1,30 +1,60 @@
 package net.openhft.chronicle.queue;
 
 import net.openhft.chronicle.wire.WireIn;
+import net.openhft.chronicle.wire.WireKey;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertTrue;
 
 public class ChronicleTest {
 
-    public static final int RUNS = 2000000;
+    public static final int RUNS = 1000000;
 
     @Test
     public void testCreateAppender() throws Exception {
-        for (int t = 0; t < 10; t++) {
-            String name = "/tmp/single" + System.nanoTime() + ".q";
-            new File(name).deleteOnExit();
-            Chronicle chronicle = new ChronicleQueueBuilder(name).build();
-            long start = System.nanoTime();
-            writeSome(chronicle);
-            long mid = System.nanoTime();
-            readSome(chronicle);
-            long end = System.nanoTime();
-            System.out.printf("Write time %,d - Read time %,d %n", (mid - start) / RUNS, (end - mid) / RUNS);
+        for (int r = 0; r < 2; r++) {
+            for (int t = 1; t <= 12; t++) {
+                List<Future<?>> futureList = new ArrayList<>();
+                long start = System.nanoTime();
+                for (int j = 0; j < 4; j++) {
+                    String name = "single" + start + "-" + j + ".q";
+                    new File(name).deleteOnExit();
+                    Chronicle chronicle = new ChronicleQueueBuilder(name).build();
+
+                    futureList.add(ForkJoinPool.commonPool().submit(() -> {
+                        writeSome(chronicle);
+                        return null;
+                    }));
+                }
+                for (Future<?> future : futureList) {
+                    future.get();
+                }
+                futureList.clear();
+                long mid = System.nanoTime();
+                for (int j = 0; j < 4; j++) {
+                    String name = "single" + start + "-" + j + ".q";
+                    new File(name).deleteOnExit();
+                    Chronicle chronicle = new ChronicleQueueBuilder(name).build();
+
+                    futureList.add(ForkJoinPool.commonPool().submit(() -> {
+                        readSome(chronicle);
+                        return null;
+                    }));
+                }
+                for (Future<?> future : futureList) {
+                    future.get();
+                }
+                long end = System.nanoTime();
+                System.out.printf("Threads: %,d - Write rate %.1f M/s - Read rate %.1f M/s%n", t, t * RUNS * 1e3 / (mid - start), t * RUNS * 1e3 / (end - mid));
+            }
         }
     }
 
@@ -32,7 +62,7 @@ public class ChronicleTest {
         ExcerptTailer tailer = chronicle.createTailer();
         StringBuilder sb = new StringBuilder();
         // TODO check this is still needed in future versions.
-        Function<WireIn, WireIn> reader = wire -> wire.read().text(sb);
+        Function<WireIn, WireIn> reader = wire -> wire.read(TestKey.test).text(sb);
         for (int i = 0; i < RUNS; i++) {
             assertTrue(tailer.readDocument(reader));
         }
@@ -41,7 +71,11 @@ public class ChronicleTest {
     private void writeSome(Chronicle chronicle) throws IOException {
         ExcerptAppender appender = chronicle.createAppender();
         for (int i = 0; i < RUNS; i++) {
-            appender.writeDocument(wire -> wire.write().text("Hello World"));
+            appender.writeDocument(wire -> wire.write(TestKey.test).text("Hello World23456789012345678901234567890"));
         }
+    }
+
+    enum TestKey implements WireKey {
+        test
     }
 }
