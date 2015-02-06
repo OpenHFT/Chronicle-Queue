3bef244a09179aa3f894a858043db4471cbc9577
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-02-06 11:49:46 +0000

    Add index details to the header

--- chronicle-queue/src/main/java/net/openhft/chronicle/queue/impl/Header.java
+++ chronicle-queue/src/main/java/net/openhft/chronicle/queue/impl/Header.java
@@ -1,10 +1,8 @@
 package net.openhft.chronicle.queue.impl;
 
 import net.openhft.chronicle.queue.Compression;
-import net.openhft.chronicle.wire.Marshallable;
-import net.openhft.chronicle.wire.WireIn;
-import net.openhft.chronicle.wire.WireKey;
-import net.openhft.chronicle.wire.WireOut;
+import net.openhft.chronicle.wire.*;
+import net.openhft.lang.io.DirectStore;
 import net.openhft.lang.values.LongValue;
 
 import java.time.ZonedDateTime;
@@ -17,13 +15,15 @@ import static net.openhft.lang.model.DataValueClasses.newDirectInstance;
  * data structure.
  */
 class Header implements Marshallable {
-    private static final long PADDED_SIZE = 256;
+    private static final long PADDED_SIZE = 512;
     // fields which can be serialized/deserialized in the normal way.
     UUID uuid;
     ZonedDateTime created;
     String user;
     String host;
     String compression;
+    int indexCount = 128 << 10;
+    int indexSpacing = 64;
 
     // support binding to off heap memory with thread safe operations.
     final LongValue writeByte = newDirectInstance(LongValue.class);
@@ -41,7 +41,8 @@ class Header implements Marshallable {
 
     enum Field implements WireKey {
         type,
-        uuid, created, user, host,
+        uuid, created, user, host, compression,
+        indexCount, indexSpacing,
         writeByte, index2Index
     }
 
@@ -52,8 +53,11 @@ class Header implements Marshallable {
                 .write(Field.created).zonedDateTime(created)
                 .write(Field.user).text(user)
                 .write(Field.host).text(host)
+                .write(Field.compression).text(compression)
+                .write(Field.indexCount).int32(indexCount)
+                .write(Field.indexSpacing).int32(indexSpacing)
                 .write(Field.index2Index).int64(index2Index);
-        out.addPadding((int) (256 - out.bytes().position()));
+        out.addPadding((int) (PADDED_SIZE - out.bytes().position()));
     }
 
     @Override
@@ -63,6 +67,9 @@ class Header implements Marshallable {
                 .read(Field.created).zonedDateTime(c -> created = c)
                 .read(Field.user).text(u -> user = u)
                 .read(Field.host).text(h -> host = h)
+                .read(Field.compression).text(h -> compression = h)
+                .read(Field.indexCount).int32(h -> indexCount = h)
+                .read(Field.indexSpacing).int32(h -> indexSpacing = h)
                 .read(Field.index2Index).int64(index2Index);
     }
 
@@ -72,5 +79,14 @@ class Header implements Marshallable {
 
     public void setWriteByteLazy(long writeByte) {
         this.writeByte.setOrderedValue(writeByte);
+    }
+
+    public static void main(String... args) {
+        Header h = new Header();
+        h.init(Compression.NONE);
+        TextWire tw = new TextWire(new DirectStore(1024).bytes());
+        tw.writeDocument(() -> tw.write("header", null).writeMarshallable(h));
+        System.out.println(tw.bytes().flip().toString());
+
     }
 }
