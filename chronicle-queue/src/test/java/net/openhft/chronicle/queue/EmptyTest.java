e94d562509aea6f7b210d850f12ad64ce5c13f2b
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-02-06 13:12:28 +0000

    Also test nothing

--- chronicle-queue/src/test/java/net/openhft/chronicle/queue/EmptyTest.java
+++ chronicle-queue/src/test/java/net/openhft/chronicle/queue/EmptyTest.java
@@ -1,7 +1,13 @@
 package net.openhft.chronicle.queue;
 
+import org.junit.Test;
+
 /**
  * Created by peter.lawrey on 06/02/15.
  */
 public class EmptyTest {
+    @Test
+    public void testNothing() {
+
+    }
 }
