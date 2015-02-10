263bf4530aa88022756398a5db953cf4117f0ce2
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-02-10 17:32:30 +0000

    QUEUE-14 - reverted back to using attomic

--- chronicle-queue/src/main/java/net/openhft/chronicle/queue/impl/ringbuffer/BytesQueue.java
+++ chronicle-queue/src/main/java/net/openhft/chronicle/queue/impl/ringbuffer/BytesQueue.java
@@ -4,6 +4,8 @@ import net.openhft.lang.io.Bytes;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * Multi writer single Reader, zero GC, ring buffer
  *
@@ -200,15 +202,18 @@ public class BytesQueue {
 
         }
 
-        // has to be synchronized because the compareAndSwapLong is not writeOrdered
-        private synchronized boolean compareAndSetWriteLocation(long expectedValue, long newValue) {
-            return buffer.compareAndSwapLong(writeLocationOffset, expectedValue, newValue);
+
+        // added as the method below has visibility issues
+        final AtomicLong writeLocationValue = new AtomicLong();
+
+        public synchronized boolean compareAndSetWriteLocation(long expectedValue, long newValue) {
+            return writeLocationValue.compareAndSet(expectedValue, newValue);
         }
 
-        // has to be synchronized because the compareAndSwapLong is not writeOrdered
-        private synchronized long getWriteLocation() {
-            return buffer.readVolatileLong(writeLocationOffset);
+        public synchronized long getWriteLocation() {
+            return writeLocationValue.get();
         }
+
 
         /**
          * sets the point at which you should not write any additional bits
