be43c902a8353147b9a8f898573238b90321fc69
lburgazzoli <lburgazzoli@gmail.com>
2015-04-07 23:48:55 +0200

    CHRON-114 - VanillaChronicle AssertionError

--- chronicle/src/test/java/net/openhft/chronicle/VanillaBlockSizeTest.java
+++ chronicle/src/test/java/net/openhft/chronicle/VanillaBlockSizeTest.java
@@ -1,7 +1,30 @@
+/*
+ * Copyright 2015 Higher Frequency Trading
+ *
+ * http://www.higherfrequencytrading.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.openhft.chronicle;
 
+import net.openhft.lang.Jvm;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.rules.TestName;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
@@ -9,62 +32,44 @@ import java.util.Random;
  * @author luke
  *         Date: 4/7/15
  */
+@Ignore
 public class VanillaBlockSizeTest {
+
+    @Rule
+    public final TemporaryFolder tmpdir = new TemporaryFolder(new File(Jvm.TMP));
+
+    @Rule
+    public final TestName testName = new TestName();
+
+    @Ignore
     @Test
     public void testMaxSize() throws IOException {
-        final int SAMPLE = 10000000;
-        final int ITEM_SIZE = 256;
-        byte[][] byteArrays;
-        Random random = new Random();
+        final File root = tmpdir.newFolder(testName.getMethodName());
+        root.deleteOnExit();
 
-        Chronicle chronicle = ChronicleQueueBuilder.vanilla("blocksizetest")
-                .dataBlockSize(1073741824)
-                .indexBlockSize(1073741824)
-                .build();
+        final int SAMPLE = 1000000; // 10000000
+        final int ITEM_SIZE = 8; // 256
+
+        byte[] byteArrays = new byte[ITEM_SIZE];
+        new Random().nextBytes(byteArrays);
+
+        Chronicle chronicle = ChronicleQueueBuilder
+            .vanilla(root.getAbsolutePath() + "/blocksize-test")
+            .dataBlockSize(1073741824)
+            .indexBlockSize(1073741824)
+            .build();
+
         ExcerptAppender appender = chronicle.createAppender();
 
-        System.out.println("Allocating...");
-        byteArrays = new byte[SAMPLE][ITEM_SIZE];
-
-        // Randomize
-        System.out.println("Randomize...");
-        for (int i=0;i<SAMPLE;i++) {
-            random.nextBytes(byteArrays[0]);
-        }
-
-        System.out.println("GC...");
-        System.gc();
-
-        // Warmup
-        System.out.println("Warmup...");
-        for (int i=0;i<SAMPLE;i++) {
-            appender.startExcerpt(ITEM_SIZE);
-            appender.write(byteArrays[i], 0, ITEM_SIZE);
-            appender.finish();
-        }
-
         System.out.println("Test...");
-        int index = 0;
-        int count = 0;
-        while (true) {
-            if (index >= SAMPLE) {
-                index = 0;
-            }
-            long start = System.nanoTime();
+        for (int count=0 ; ; count++) {
             appender.startExcerpt(ITEM_SIZE);
-            appender.write(byteArrays[index], 0, ITEM_SIZE);
+            appender.write(byteArrays, 0, ITEM_SIZE);
             appender.finish();
 
-            if (count % 100000000 == 0) {
-                System.out.println(count+" written");
+            if (count % SAMPLE == 0) {
+                System.out.println(count + " written");
             }
-
-            index++;
-            count++;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        new VanillaBlockSizeTest().testMaxSize();
     }
 }
--- chronicle/src/test/java/net/openhft/chronicle/VanillaLatencyTest.java
+++ chronicle/src/test/java/net/openhft/chronicle/VanillaLatencyTest.java
@@ -1,8 +1,30 @@
+/*
+ * Copyright 2015 Higher Frequency Trading
+ *
+ * http://www.higherfrequencytrading.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.openhft.chronicle;
 
+import net.openhft.lang.Jvm;
 import org.HdrHistogram.Histogram;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
@@ -10,14 +32,22 @@ import java.util.Random;
  * @author luke
  *         Date: 4/7/15
  */
+@Ignore
 public class VanillaLatencyTest {
 
-    final int COUNT = 100000000;
-    final int SAMPLE = 10000000;
+    @Rule
+    public final TemporaryFolder tmpdir = new TemporaryFolder(new File(Jvm.TMP));
+
+    final int SAMPLE = 1000000;  //  10000000
+    final int COUNT = SAMPLE * 10; // 100000000
     final int ITEM_SIZE = 256;
 
+    @Ignore
     @Test
-    public void testIndexedVsVanilla() throws IOException {
+    public void testIndexedVsVanillaLatency() throws IOException {
+        File root = tmpdir.newFolder("IndexedVsVanillaLatency");
+        root.deleteOnExit();
+
         Random random = new Random();
 
         System.out.println("Allocating...");
@@ -29,12 +59,18 @@ public class VanillaLatencyTest {
             random.nextBytes(byteArrays[0]);
         }
 
-        Chronicle indexedChronicle = ChronicleQueueBuilder.indexed("indexedlatency").build();
+        Chronicle indexedChronicle = ChronicleQueueBuilder
+            .indexed(root.getAbsolutePath() + "/indexed-latency")
+            .build();
+
         System.out.println("----------------------- INDEXED -----------------------");
         latencyTest(indexedChronicle, COUNT, byteArrays);
         indexedChronicle.close();
 
-        Chronicle vanillaChronicle = ChronicleQueueBuilder.vanilla("vanillalatency").build();
+        Chronicle vanillaChronicle = ChronicleQueueBuilder
+            .vanilla(root.getAbsolutePath() + "/vanilla-latency")
+            .build();
+
         System.out.println("----------------------- VANILLA -----------------------");
         latencyTest(vanillaChronicle, COUNT, byteArrays);
         vanillaChronicle.close();
@@ -49,7 +85,7 @@ public class VanillaLatencyTest {
 
         // Warmup
         System.out.println("Warmup...");
-        for (int i=0;i<SAMPLE;i++) {
+        for (int i=0; i<SAMPLE; i++) {
             appender.startExcerpt(ITEM_SIZE);
             appender.write(byteArrays[i], 0, ITEM_SIZE);
             appender.finish();
@@ -57,27 +93,24 @@ public class VanillaLatencyTest {
 
         System.out.println("Test...");
         int index = 0;
-        for (int i=0; i<count; i++) {
-            if (index >= SAMPLE) {
-                index = 0;
-            }
+        for (int i=0; i<count; i++, index++) {
             long start = System.nanoTime();
+
             appender.startExcerpt(ITEM_SIZE);
-            appender.write(byteArrays[index], 0, ITEM_SIZE);
+            appender.write(byteArrays[index % SAMPLE], 0, ITEM_SIZE);
             appender.finish();
+
             long end = System.nanoTime();
             histogram.recordValue(end - start);
 
-            if (i > 0 && i % 10000000 == 0) {
-                System.out.println("Total: "+i+" Mean: "+histogram.getMean()+" Max: "+histogram.getMaxValue()+
-                        " StdDev: "+histogram.getStdDeviation());
+            if (i > 0 && i % SAMPLE == 0) {
+                System.out.println(
+                    "Total: " + i +
+                    " Mean: " + histogram.getMean() +
+                    " Max: " + histogram.getMaxValue()+
+                    " StdDev: " + histogram.getStdDeviation()
+                );
             }
-
-            index++;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        new VanillaLatencyTest().testIndexedVsVanilla();
     }
 }
