6ac665633eef3378c17f1842982c57694e07b514
lburgazzoli <lburgazzoli@gmail.com>
2015-08-13 15:46:36 +0200

    Cleanup ChronicleQueue V4

--- chronicle-queue/src/main/java/net/openhft/chronicle/queue/ExcerptAppender.java
+++ chronicle-queue/src/main/java/net/openhft/chronicle/queue/ExcerptAppender.java
@@ -1,28 +1,23 @@
 /*
- * Copyright 2014 Higher Frequency Trading
+ *     Copyright (C) 2015  higherfrequencytrading.com
  *
- * http://www.higherfrequencytrading.com
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package net.openhft.chronicle.queue;
 
 import net.openhft.chronicle.wire.WireOut;
 import net.openhft.chronicle.wire.WriteMarshallable;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.function.Consumer;
 
 /**
  * The component that facilitates sequentially writing data to a {@link ChronicleQueue}.
