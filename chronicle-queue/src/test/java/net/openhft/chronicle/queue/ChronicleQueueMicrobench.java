228fb2a89563685b76b3403b5df11fa775f794c5
lburgazzoli <lburgazzoli@gmail.com>
2015-09-15 14:57:15 +0200

    QUEUE-24 jmh helpers

--- chronicle-queue/src/test/java/net/openhft/chronicle/queue/ChronicleQueueMicrobench.java
+++ chronicle-queue/src/test/java/net/openhft/chronicle/queue/ChronicleQueueMicrobench.java
@@ -15,5 +15,104 @@
  */
 package net.openhft.chronicle.queue;
 
+import org.junit.Test;
+import org.openjdk.jmh.results.format.ResultFormatType;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.File;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Idea borrowed from Netty
+ *   - https://github.com/netty/netty
+ *   - microbench/src/main/java/io/netty/microbench/util/AbstractMicrobenchmarkBase.java
+ */
 public class ChronicleQueueMicrobench {
+
+    protected static final int DEFAULT_WARMUP_ITERATIONS  = 10;
+    protected static final int DEFAULT_MEASURE_ITERATIONS = 10;
+
+    protected static final String[] EMPTY_JVM_ARGS = {};
+
+    protected static final String[] BASE_JVM_ARGS = {
+        "-server",
+        "-dsa",
+        "-da",
+        "-ea:net.openhft...",
+        "-XX:+AggressiveOpts",
+        "-XX:+UseBiasedLocking",
+        "-XX:+UseFastAccessorMethods",
+        "-XX:+OptimizeStringConcat",
+        "-XX:+HeapDumpOnOutOfMemoryError"
+    };
+
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    protected ChainedOptionsBuilder newOptionsBuilder() throws Exception {
+        String className = getClass().getSimpleName();
+
+        final ChainedOptionsBuilder runnerOptions = new OptionsBuilder()
+            .include(".*" + className + ".*")
+            .jvmArgs(BASE_JVM_ARGS)
+            .jvmArgsAppend(jvmArgs()
+        );
+
+        if (getWarmupIterations() > 0) {
+            runnerOptions.warmupIterations(getWarmupIterations());
+        }
+
+        if (getMeasureIterations() > 0) {
+            runnerOptions.measurementIterations(getMeasureIterations());
+        }
+
+        if (null != getReportDir()) {
+            String filePath = getReportDir() + className + ".json";
+            File file = new File(filePath);
+            if (file.exists()) {
+                file.delete();
+            } else {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+
+            runnerOptions.resultFormat(ResultFormatType.JSON);
+            runnerOptions.result(filePath);
+        }
+
+        return runnerOptions;
+    }
+
+    protected String[] jvmArgs() {
+        return EMPTY_JVM_ARGS;
+    }
+
+    protected int getWarmupIterations() {
+        return Integer.getInteger("warmupIterations", -1);
+    }
+
+    protected int getMeasureIterations() {
+        return Integer.getInteger("measureIterations", -1);
+    }
+
+    protected String getReportDir() {
+        return System.getProperty("perfReportDir");
+    }
+
+    public static void handleUnexpectedException(Throwable t) {
+        assertNull(t);
+    }
+
+    // *************************************************************************
+    //
+    // *************************************************************************
+
+    @Test
+    public void run() throws Exception {
+        new Runner(newOptionsBuilder().build()).run();
+    }
 }
