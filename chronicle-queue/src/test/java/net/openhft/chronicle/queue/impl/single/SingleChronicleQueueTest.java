514093f2103c9ff3deacaf91b4ff8a47d08a1262
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-01-07 22:10:02 +0000

    updated to release BOM

--- chronicle-queue/src/test/java/net/openhft/chronicle/queue/impl/single/SingleChronicleQueueTest.java
+++ chronicle-queue/src/test/java/net/openhft/chronicle/queue/impl/single/SingleChronicleQueueTest.java
@@ -179,7 +179,7 @@ public class SingleChronicleQueueTest extends ChronicleQueueTestBase {
         }
     }
 
-    @Ignore("todo - rob to fix")
+
     @Test
     public void testAppendAndReadWithRolling2() throws IOException {
         final File dir = getTmpDir();
@@ -442,6 +442,29 @@ public class SingleChronicleQueueTest extends ChronicleQueueTestBase {
             tailer.readDocument(wire -> wire.read(() -> "key").text(sb));
 
             Assert.assertEquals("value=2", sb.toString());
+
+        } finally {
+            file.delete();
+        }
+    }
+
+
+    @Test
+    public void testEPOC() throws Exception {
+
+        File file = File.createTempFile("chronicle.", "q");
+        file.deleteOnExit();
+        try {
+
+            final ChronicleQueue chronicle = new SingleChronicleQueueBuilder(getTmpDir())
+                    .wireType(this.wireType)
+                    .epoc(System.currentTimeMillis())
+                    .rollCycle(RollCycles.HOURS)
+                    .build();
+
+            final ExcerptAppender appender = chronicle.createAppender();
+            appender.writeDocument(wire -> wire.write(() -> "key").text("value=v"));
+            Assert.assertEquals(0, appender.cycle());
 
         } finally {
             file.delete();
