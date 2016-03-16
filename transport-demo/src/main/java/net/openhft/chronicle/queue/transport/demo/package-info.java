235eda0e9bfc18699bb702d0062b54414112dc30
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-03-16 09:04:40 +0000

    Start a demo of how to use queue as a transport

---
+++ transport-demo/src/main/java/net/openhft/chronicle/queue/transport/demo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by peter on 16/03/16.
+ */
+package net.openhft.chronicle.queue.transport.demo;
--- transport-demo/src/main/java/net/optnhft/chronicle/queue/transport/demo/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by peter on 16/03/16.
- */
-package net.optnhft.chronicle.queue.transport.demo;
