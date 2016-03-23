5ed5c99afedb02f8187d88d26f6dd03d29a8e0ea
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-03-23 13:58:04 +0000

    Add copyright

--- src/test/java/net/openhft/chronicle/queue/impl/single/IndexTest.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/IndexTest.java
@@ -1,6 +1,6 @@
 /*
  *
- *  *     Copyright (C) ${YEAR}  higherfrequencytrading.com
+ *  *     Copyright (C) 2016  higherfrequencytrading.com
  *  *
  *  *     This program is free software: you can redistribute it and/or modify
  *  *     it under the terms of the GNU Lesser General Public License as published by
