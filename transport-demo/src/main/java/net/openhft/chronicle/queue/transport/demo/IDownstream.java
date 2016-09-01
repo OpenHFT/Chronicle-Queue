1cd1b8298259e5f6ebe86bfea1b8fee597a87d0b
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-09-01 09:29:59 +0100

    remove chroncle demo as it is confusing as it only works with chronicle Q3 and we have move to chroncile Q4

--- transport-demo/src/main/java/net/openhft/chronicle/queue/transport/demo/IDownstream.java
+++
@@ -1,8 +0,0 @@
-package net.openhft.chronicle.queue.transport.demo;
-
-/**
- * Interface for message downstream from your core system.
- */
-public interface IDownstream extends IUpstream {
-    // keep downstream and upstream the same for simplicity.
-}
