c82c093143ee3602485df2cd2c4b0f51597f3fdb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-10-12 12:57:28 +0100

    example gateway class

---
+++ src/test/java/net/openhft/chronicle/queue/micros/Gateway.java
@@ -0,0 +1,15 @@
+package net.openhft.chronicle.queue.micros;
+
+/**
+ * @author Rob Austin.
+ */
+public class Gateway implements SidedMarketDataListener {
+
+
+
+    @Override
+    public void onSidedPrice(SidedPrice sidedPrice) {
+        throw new UnsupportedOperationException("todo");
+    }
+
+}
