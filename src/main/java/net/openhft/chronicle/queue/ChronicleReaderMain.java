6dd859f67add618f73c1a569bd0926191f2275bc
epickrram <mark@epickrram.com>
2017-06-20 09:06:07 +0100

    parse cmdline, call reader from main class

--- src/main/java/net/openhft/chronicle/queue/ChronicleReaderMain.java
+++ src/main/java/net/openhft/chronicle/queue/ChronicleReaderMain.java
@@ -18,21 +18,17 @@
 
 package net.openhft.chronicle.queue;
 
-import net.openhft.chronicle.bytes.Bytes;
-import net.openhft.chronicle.core.Jvm;
-import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
-import net.openhft.chronicle.wire.BinaryWire;
-import net.openhft.chronicle.wire.DocumentContext;
-import net.openhft.chronicle.wire.TextWire;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.regex.Pattern;
-
-import static java.lang.System.err;
-import static java.lang.System.exit;
-import static java.lang.System.out;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
 
 /**
  * Display records in a Chronicle in a text form.
@@ -42,65 +38,59 @@ import static java.lang.System.out;
 public enum ChronicleReaderMain {
     ;
 
-    static final String excludes = System.getProperty("excludes");
+    public static void main(@NotNull String[] args) throws IOException {
 
-    public static void main(@NotNull String... args) throws IOException {
-        if (args.length < 1) {
-            err.println("Usage: java " + ChronicleReaderMain.class.getName() + " {chronicle-base-path} {regex} [from-index]");
-            exit(-1);
-        }
+        final Options options = options();
+        final CommandLine commandLine = parseCommandLine(args, options);
 
-        String basePath = args[0];
-        String regex = args.length > 1 ? args[1] : "";
-        long index = args.length > 2 ? Long.decode(args[2]) : -1;
+        final ChronicleReader chronicleReader = new ChronicleReader().
+                withBasePath(Paths.get(commandLine.getOptionValue('d')));
 
-        tailFileFrom(basePath, regex, index, false);
+        configureReader(options, chronicleReader);
+
+        chronicleReader.execute();
     }
 
-    public static void tailFileFrom(@NotNull String basePath, @NotNull String regex, long index, boolean stopAtEnd) {
-        ChronicleQueue ic = SingleChronicleQueueBuilder.binary(new File(basePath)).build();
-        ExcerptTailer tailer = ic.createTailer();
+    private static CommandLine parseCommandLine(final @NotNull String[] args, final Options options) {
+        final CommandLineParser parser = new DefaultParser();
+        CommandLine commandLine = null;
+        try {
+            commandLine = parser.parse(options, args);
+        } catch (ParseException e) {
+            new HelpFormatter().printUsage(new PrintWriter(System.out), 120,
+                    ChronicleReaderMain.class.getSimpleName(), options);
+            System.exit(1);
+        }
+        return commandLine;
+    }
 
-        if (index > 0) {
-            out.println("Waiting for index " + index);
-            for (; ; ) {
-                if (tailer.moveToIndex(index))
-                    break;
-                Jvm.pause(100);
-            }
+    private static void configureReader(final Options options, final ChronicleReader chronicleReader) {
+        if (options.hasOption("i")) {
+            chronicleReader.withInclusionRegex(options.getOption("i").getValue());
         }
-        else if (index < 0) {
-            out.println("Skipping to the end");
-            tailer.toEnd();
+        if (options.hasOption("e")) {
+            chronicleReader.withExclusionRegex(options.getOption("e").getValue());
         }
+        if (options.hasOption("f")) {
+            chronicleReader.tail();
+        }
+        if (options.hasOption("m")) {
+            chronicleReader.historyRecords(Long.parseLong(options.getOption("m").getValue()));
+        }
+        if (options.hasOption("n")) {
+            chronicleReader.withStartIndex(Long.decode(options.getOption("n").getValue()));
+        }
+    }
 
-        Bytes bytes2 = Bytes.elasticByteBuffer();
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            try (DocumentContext dc = tailer.readingDocument()) {
-                if (!dc.isPresent()) {
-                    if (stopAtEnd)
-                        break;
-                    Jvm.pause(50);
-                    continue;
-                }
-                Bytes<?> bytes = dc.wire().bytes();
-                byte b0 = bytes.readByte(bytes.readPosition());
-                String text;
-                if (b0 < 0) {
-                    bytes2.clear();
-                    new BinaryWire(bytes).copyTo(new TextWire(bytes2));
-                    text = bytes2.toString();
-                } else {
-                    text = bytes.toString();
-                }
-                if (regex.isEmpty() || Pattern.compile(regex).matcher(text).find()) {
-                    if (excludes == null || !text.matches(excludes)) {
-                        out.print("0x" + Long.toHexString(tailer.index()) + ": ");
-                        out.println(text);
-                    }
-                }
-            }
-        }
+    @NotNull
+    private static Options options() {
+        final Options options = new Options();
+        options.addRequiredOption("d", "directory", true, "Directory containing chronicle queue files");
+        options.addOption("i", "include-regex", true, "Display records containing this regular expression");
+        options.addOption("e", "exclude-regex", true, "Do not display records containing this regular expression");
+        options.addOption("f", "follow", false, "Tail behaviour - wait for new records to arrive");
+        options.addOption("m", "max-history", true, "Show this many records from the end of the data set");
+        options.addOption("n", "from-index", true, "Start reading from this index (e.g. 0x123ABE)");
+        return options;
     }
 }
