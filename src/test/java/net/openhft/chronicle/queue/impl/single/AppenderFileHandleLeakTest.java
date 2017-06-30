358cfe54f7b5330d5b67a81f4e7e19d8b08208f0
epickrram <mark@epickrram.com>
2017-06-30 08:35:34 +0100

    incorporate test case that demonstrates file-handle leak

--- src/test/java/net/openhft/chronicle/queue/impl/single/AppenderFileHandleLeakTest.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/AppenderFileHandleLeakTest.java
@@ -2,10 +2,13 @@ package net.openhft.chronicle.queue.impl.single;
 
 import net.openhft.chronicle.core.OS;
 import net.openhft.chronicle.queue.DirectoryUtils;
+import net.openhft.chronicle.queue.ExcerptAppender;
+import net.openhft.chronicle.queue.RollCycles;
+import net.openhft.chronicle.wire.WireType;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.lang.management.ManagementFactory;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,7 +18,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.LockSupport;
 import java.util.stream.Stream;
 
 import static java.lang.Math.abs;
@@ -24,66 +26,46 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assume.assumeThat;
 
 public final class AppenderFileHandleLeakTest {
-    private static final SingleChronicleQueue QUEUE;
-    private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors() * 3;
+    private static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors() / 2;
     private final ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
 
-    static {
-        QUEUE = createQueue();
-    }
-
+    @Ignore("#357 WIP")
     @Test
     public void shouldNotLeakFileHandles() throws Exception {
         assumeThat(OS.isLinux(), is(true));
+        try (SingleChronicleQueue queue = createQueue()) {
 
-        final long openFileHandleCount = countFileHandlesOfCurrentProcess();
-        System.out.printf("start count: %d%n", openFileHandleCount);
-        final List<Future<Boolean>> futures = new LinkedList<>();
 
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            futures.add(threadPool.submit(() -> {
-                for (int j = 0; j < 1000; j++) {
-                    writeMessage(j);
-                    LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(1L));
-                }
-                return Boolean.TRUE;
-            }));
-        }
+            final long openFileHandleCount = countFileHandlesOfCurrentProcess();
+            System.out.printf("start count: %d%n", openFileHandleCount);
+            final List<Future<Boolean>> futures = new LinkedList<>();
 
-        for (Future<Boolean> future : futures) {
-            assertThat(future.get(1, TimeUnit.DAYS), is(true));
-        }
+            for (int i = 0; i < THREAD_COUNT; i++) {
+                futures.add(threadPool.submit(() -> {
+                    for (int j = 0; j < 50; j++) {
+                        writeMessage(j, queue);
+                        GcControls.requestGcCycle();
+                    }
+                    return Boolean.TRUE;
+                }));
+            }
 
-        waitForGcCycle();
-        waitForGcCycle();
+            for (Future<Boolean> future : futures) {
+                assertThat(future.get(1, TimeUnit.MINUTES), is(true));
+            }
 
-        System.out.printf("end count: %d%n", openFileHandleCount);
-        assertThat(abs(openFileHandleCount - countFileHandlesOfCurrentProcess()) < 50, is(true));
+            GcControls.waitForGcCycle();
 
-    }
-
-    private static void writeMessage(final int j) {
-        QUEUE.acquireAppender().writeText(Integer.toString(j));
-    }
-
-    private void waitForGcCycle() {
-        final long gcCount = getGcCount();
-        System.gc();
-        final long timeoutAt = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(1L);
-        while ((getGcCount() == gcCount) && System.currentTimeMillis() < timeoutAt) {
-            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100L));
-        }
-
-        if (getGcCount() == gcCount) {
-            throw new IllegalStateException("GC did not occur within timeout");
+            System.out.printf("end count: %d%n", countFileHandlesOfCurrentProcess());
+            assertThat(abs(openFileHandleCount - countFileHandlesOfCurrentProcess()) < 5, is(true));
         }
     }
 
-    private static long getGcCount() {
-        return ManagementFactory.getGarbageCollectorMXBeans().stream().
-                reduce(0L,
-                        (count, gcBean) -> count + gcBean.getCollectionCount(),
-                        (a, b) -> a + b);
+    private static void writeMessage(final int j, final SingleChronicleQueue queue) {
+        final ExcerptAppender appender = queue.acquireAppender();
+        appender.writeBytes(b -> {
+            b.writeInt(j);
+        });
     }
 
     private static long countFileHandlesOfCurrentProcess() throws IOException {
@@ -94,6 +76,9 @@ public final class AppenderFileHandleLeakTest {
 
     private static SingleChronicleQueue createQueue() {
         return SingleChronicleQueueBuilder.
-                binary(DirectoryUtils.tempDir(AppenderFileHandleLeakTest.class.getSimpleName())).build();
+                binary(DirectoryUtils.tempDir(AppenderFileHandleLeakTest.class.getSimpleName())).
+                rollCycle(RollCycles.TEST_SECONDLY).
+                wireType(WireType.BINARY_LIGHT).
+                build();
     }
 }
