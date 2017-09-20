1246294deb089ad240a52da4405ca4e3c9e0ba40
epickrram <mark@epickrram.com>
2017-09-20 09:05:24 +0100

    add scripts to use/package reader tools

---
+++ bin/dump_queue.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+PROJECT_DIR="$SCRIPT_DIR/.."
+
+if [[ "$1" == "" ]]; then
+    echo "Usage: $0 [chronicle-queue-directory]"
+    exit 1
+fi
+
+UBER_JAR=$(find $PROJECT_DIR/target/chronicle-queue-*-all.jar)
+
+if [[ "$?" != "0" ]]; then
+    echo "Could not find uber-jar, please run 'mvn package' in the project root"
+    exit 1
+fi
+
+java -cp "$UBER_JAR" net.openhft.chronicle.queue.DumpQueueMain "$1"
---
+++ bin/package_scripts.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+PROJECT_DIR="$SCRIPT_DIR/.."
+TOOL_ARCHIVE=/tmp/chronicle-queue-tools.tar
+
+UBER_JAR=$(find $PROJECT_DIR/target/chronicle-queue-*-all.jar)
+
+if [[ "$?" != "0" ]]; then
+    echo "Could not find uber-jar, please run 'mvn package' in the project root"
+    exit 1
+fi
+
+TMP_DIR=$(mktemp -d)
+
+JAR_FILE=$(echo $UBER_JAR | awk -F/ '{print $NF}')
+
+cp $UBER_JAR $TMP_DIR/
+
+echo "java -cp $JAR_FILE \\" > $TMP_DIR/dump_queue.sh
+echo 'net.openhft.chronicle.queue.DumpQueueMain "$1"' >> $TMP_DIR/dump_queue.sh
+chmod +x $TMP_DIR/dump_queue.sh
+
+echo "java -cp $JAR_FILE \\" > $TMP_DIR/queue_reader.sh
+echo 'net.openhft.chronicle.queue.ChronicleReaderMain "$@"' >> $TMP_DIR/queue_reader.sh
+chmod +x $TMP_DIR/queue_reader.sh
+
+cd $TMP_DIR
+tar cf $TOOL_ARCHIVE *.*
+cd $OLD_PWD
+
+echo "Created $TOOL_ARCHIVE"
