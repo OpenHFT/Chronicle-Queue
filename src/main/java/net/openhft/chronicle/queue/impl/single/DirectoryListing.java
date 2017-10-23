c1ef25e27f7edc79412afc53effbc3a109de71e4
epickrram <mark@epickrram.com>
2017-10-23 10:40:52 +0100

    add exclusive lock for performing refresh of directory listing

--- src/main/java/net/openhft/chronicle/queue/impl/single/DirectoryListing.java
+++ src/main/java/net/openhft/chronicle/queue/impl/single/DirectoryListing.java
@@ -5,17 +5,20 @@ import net.openhft.chronicle.queue.impl.TableStore;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
+import java.util.function.IntSupplier;
 import java.util.function.ToIntFunction;
 
-public final class DirectoryListing {
+final class DirectoryListing {
     private static final String HIGHEST_CREATED_CYCLE = "listing.highestCycle";
     private static final String LOWEST_CREATED_CYCLE = "listing.lowestCycle";
+    private static final String LOCK = "listing.exclusiveLock";
     private final TableStore tableStore;
     private final Path queuePath;
     private final ToIntFunction<File> fileToCycleFunction;
     private final LongValue maxCycleValue;
     private final LongValue minCycleValue;
-
+    private final LongValue lock;
 
     DirectoryListing(
             final TableStore tableStore, final Path queuePath,
@@ -25,40 +28,81 @@ public final class DirectoryListing {
         this.fileToCycleFunction = fileToCycleFunction;
         maxCycleValue = tableStore.acquireValueFor(HIGHEST_CREATED_CYCLE);
         minCycleValue = tableStore.acquireValueFor(LOWEST_CREATED_CYCLE);
+        lock = tableStore.acquireValueFor(LOCK);
     }
 
+    // TODO timeouts as system properties
     void refresh() {
-        // TODO this must take out a lock
+        tryWithLock(this::refreshIndex);
+    }
+
+    void onFileCreated(final File file, final int cycle) {
+        tryWithLock(() -> {
+            maxCycleValue.setMaxValue(cycle);
+            minCycleValue.setMinValue(cycle);
+            return 0;
+        });
+    }
+
+    int getMaxCreatedCycle() {
+        return tryWithLock(this::getMaxCycleValue);
+    }
+
+    int getMinCreatedCycle() {
+        return tryWithLock(this::getMinCycleValue);
+    }
+
+    private int getMaxCycleValue() {
+        return (int) maxCycleValue.getVolatileValue();
+    }
+
+    private int getMinCycleValue() {
+        return (int) minCycleValue.getVolatileValue();
+    }
+
+    private int tryWithLock(final IntSupplier function) {
+        final long lockAcquisitionTimeout = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(10L);
+        long currentTime;
+        while ((currentTime = System.currentTimeMillis()) < lockAcquisitionTimeout) {
+            if (lock.compareAndSwapValue(0, currentTime)) {
+                try {
+                    return function.getAsInt();
+                } finally {
+                    lock.setOrderedValue(0);
+                }
+
+            } else  {
+                // assume that previous lock holder has died
+                final long lastLockTime = lock.getValue();
+                if (lastLockTime < currentTime - TimeUnit.SECONDS.toMillis(5L)) {
+                    if (lock.compareAndSwapValue(lastLockTime, currentTime)) {
+                        try {
+                            refreshIndex();
+                        } finally {
+                            lock.setOrderedValue(0);
+                        }
+                        return function.getAsInt();
+                    }
+                }
+                Thread.yield();
+            }
+        }
+
+        throw new IllegalStateException("Unable to acquire exclusive lock on directory listing");
+    }
+
+    private int refreshIndex() {
         maxCycleValue.setOrderedValue(Integer.MIN_VALUE);
         minCycleValue.setOrderedValue(Integer.MAX_VALUE);
         final File[] queueFiles = queuePath.toFile().
                 listFiles((d, f) -> f.endsWith(SingleChronicleQueue.SUFFIX));
         if (queueFiles != null) {
             for (File queueFile : queueFiles) {
-                onFileCreated(queueFile, fileToCycleFunction.applyAsInt(queueFile));
+                maxCycleValue.setMaxValue(fileToCycleFunction.applyAsInt(queueFile));
+                minCycleValue.setMinValue(fileToCycleFunction.applyAsInt(queueFile));
+
             }
         }
-    }
-
-    void onFileCreated(final File file, final int cycle) {
-//        System.out.printf("cycle file created: %d%n", cycle);
-        maxCycleValue.setMaxValue(cycle);
-        minCycleValue.setMinValue(cycle);
-    }
-
-    int getMaxCreatedCycle() {
-        final int value = (int) maxCycleValue.getVolatileValue();
-//        System.out.printf("read max cycle: %d%n", value);
-        return value;
-    }
-
-    int getMinCreatedCycle() {
-        final int value = (int) minCycleValue.getVolatileValue();
-//        System.out.printf("read min cycle: %d%n", value);
-        return value;
-    }
-
-    boolean fileExists(final File file) {
-        return tableStore.acquireValueFor(file.getPath()).getValue() != 0;
+        return 0;
     }
 }
