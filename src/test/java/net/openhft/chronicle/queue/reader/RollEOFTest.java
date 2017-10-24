e0998482ec0edf080aa50ea70affcd332480fc59
Dmitry Pisklov <dpisklov@gmail.com>
2017-10-24 17:08:52 +0100

    #389 More tests and some documentation

--- src/test/java/net/openhft/chronicle/queue/reader/RollEOFTest.java
+++ src/test/java/net/openhft/chronicle/queue/reader/RollEOFTest.java
@@ -15,17 +15,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.openhft.chronicle.queue.impl.single;
+package net.openhft.chronicle.queue.reader;
 
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.MappedBytes;
-import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.OS;
 import net.openhft.chronicle.core.time.TimeProvider;
 import net.openhft.chronicle.queue.DirectoryUtils;
 import net.openhft.chronicle.queue.ExcerptAppender;
 import net.openhft.chronicle.queue.RollCycles;
-import net.openhft.chronicle.queue.reader.ChronicleReader;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueStore;
 import net.openhft.chronicle.wire.DocumentContext;
 import net.openhft.chronicle.wire.Wire;
 import net.openhft.chronicle.wire.WireType;
@@ -34,20 +35,22 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueue.SUFFIX;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class RollEOFTest {
 
-    public static final long TIMEOUT = Jvm.isDebug() ? 500000L : 5000L;
     private final File path = DirectoryUtils.tempDir(getClass().getName());
 
     @Test(timeout = 5000L)
@@ -71,7 +74,7 @@ public class RollEOFTest {
         assertEquals(4, l.size());
     }
 
-    @Test//(timeout = 5000L)
+    @Test(timeout = 5000L)
     public void testRollWithoutEOFDoesntBlowup() throws Exception {
 
         final MutableTimeProvider timeProvider = new MutableTimeProvider();
@@ -99,6 +102,34 @@ public class RollEOFTest {
         assertEquals(4, l.size());
     }
 
+    @Test(timeout = 5000L)
+    public void testRollWithoutEOF() throws Exception {
+
+        final MutableTimeProvider timeProvider = new MutableTimeProvider();
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, -3);
+        timeProvider.setTime(cal.getTimeInMillis());
+        createQueueAndWriteData(timeProvider);
+        assertEquals(1, getNumberOfQueueFiles());
+
+        // adjust time
+        timeProvider.setTime(System.currentTimeMillis());
+        createQueueAndWriteData(timeProvider);
+        assertEquals(2, getNumberOfQueueFiles());
+
+        Optional<Path> firstQueueFile = Files.list(path.toPath()).filter(p -> p.toString().endsWith(SUFFIX)).sorted().findFirst();
+
+        assertTrue(firstQueueFile.isPresent());
+
+        // remove EOF from first file
+        removeEOF(firstQueueFile.get());
+
+        List<String> l = new LinkedList<>();
+        new ChronicleReader().withMessageSink(l::add).withBasePath(path.toPath()).withReadOnly(false).execute();
+        // 2 entries per message
+        assertEquals(4, l.size());
+    }
+
     private void removeEOF(Path path) throws IOException {
         long blockSize = 64 << 10;
         long chunkSize = OS.pageAlign(blockSize);
@@ -111,7 +142,7 @@ public class RollEOFTest {
             bytes.readLimit(bytes.capacity());
             bytes.readSkip(4);
             // move past header
-            try (final SingleChronicleQueueStore qs = SingleChronicleQueueBuilder.loadStore(wire)) {
+            try (final SingleChronicleQueueStore qs = loadStore(wire)) {
                 assertNotNull(qs);
                 long l = qs.writePosition();
                 long len = Wires.lengthOf(bytes.readVolatileInt(l));
@@ -121,6 +152,16 @@ public class RollEOFTest {
             }
         } finally {
             mappedBytes.release();
+        }
+    }
+
+    private SingleChronicleQueueStore loadStore(Wire wire) {
+        try {
+            Method loadStoreMethod = SingleChronicleQueueBuilder.class.getDeclaredMethod("loadStore", Wire.class);
+            loadStoreMethod.setAccessible(true);
+            return (SingleChronicleQueueStore) loadStoreMethod.invoke(null, wire);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
         }
     }
 
