de08d9b1d75637a189fdebe8fe4e343fd9c3d5dd
Rob Austin <rob.austin@boundedbuffer.co.uk>
2017-10-30 20:48:07 +0000

    #392 fixed test

--- src/test/java/net/openhft/chronicle/queue/impl/single/RollingNotCompleteTest.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/RollingNotCompleteTest.java
@@ -1,13 +1,11 @@
 package net.openhft.chronicle.queue.impl.single;
 
 import net.openhft.chronicle.bytes.MappedFile;
-import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.queue.*;
 import net.openhft.chronicle.wire.DocumentContext;
 import net.openhft.chronicle.wire.Wire;
 import org.jetbrains.annotations.NotNull;
 import org.junit.After;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -22,12 +20,12 @@ import static org.junit.Assert.assertTrue;
  */
 public class RollingNotCompleteTest {
 
-    @Ignore("Intermittent failures:" +
-            "java.lang.AssertionError: Nothing should have been written until timeout \n" +
-            "Expected :0\n" +
-            "Actual   :xxxx\n" +
-            "\tat org.junit.Assert.assertEquals(Assert.java:645)\n" +
-            "\tat net.openhft.chronicle.queue.impl.single.RollingNotCompleteTest.concurrentLockItUp(RollingNotCompleteTest.java:60)")
+    // @Ignore("Intermittent failures:" +
+    ////         "java.lang.AssertionError: Nothing should have been written until timeout \n" +
+    ///       "Expected :0\n" +
+    //       "Actual   :xxxx\n" +
+    //     "\tat org.junit.Assert.assertEquals(Assert.java:645)\n" +
+    //     "\tat net.openhft.chronicle.queue.impl.single.RollingNotCompleteTest.concurrentLockItUp(RollingNotCompleteTest.java:60)")
     @Test
     public void concurrentLockItUp() throws InterruptedException {
         final AtomicBoolean started = new AtomicBoolean();
@@ -43,10 +41,11 @@ public class RollingNotCompleteTest {
         builder.build().acquireAppender().writingDocument().wire().write("abcd");
         // don't close context. We should not be able to write to this queue until timeout
 
+        long start0 = System.currentTimeMillis();
         Thread writerThread = new Thread(() -> {
             ExcerptAppender appender = builder.build().acquireAppender();
             started.set(true);
-            while (! Thread.currentThread().isInterrupted()) {
+            while (!Thread.currentThread().isInterrupted()) {
                 try (@NotNull DocumentContext context = appender.writingDocument()) {
                     // only get in here after unlocked
                     written.incrementAndGet();
@@ -57,18 +56,22 @@ public class RollingNotCompleteTest {
             }
         });
         writerThread.start();
-        while (! started.get())
+        while (!started.get())
             ;
 
         Thread.sleep(timeoutMS - 50);
-        assertEquals("Nothing should have been written until timeout", 0, written.get());
+        long duration = start0 - System.currentTimeMillis();
 
-        long start = System.currentTimeMillis();
-        while (System.currentTimeMillis() < start + timeoutMS) {
-            if (written.get() > 0)
-                break;
+        if (duration < timeoutMS) {
+            assertEquals("Nothing should have been written until timeout", 0, written.get());
+
+            long start = System.currentTimeMillis();
+            while (System.currentTimeMillis() < start + timeoutMS) {
+                if (written.get() > 0)
+                    break;
+            }
+            assertTrue("Nothing was written after header was repaired", written.get() > 0);
         }
-        assertTrue("Nothing was written after header was repaired", written.get() > 0);
     }
 
     @After
