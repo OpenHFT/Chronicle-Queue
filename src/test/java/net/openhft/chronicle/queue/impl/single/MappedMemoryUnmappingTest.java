b880940f39d9c60c695fb9084560f2d643299cb6
epickrram <mark@epickrram.com>
2018-02-08 13:38:01 +0000

    Update to latest bom version

--- src/test/java/net/openhft/chronicle/queue/impl/single/MappedMemoryUnmappingTest.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/MappedMemoryUnmappingTest.java
@@ -14,11 +14,11 @@ import static org.junit.Assert.fail;
 public final class MappedMemoryUnmappingTest {
     @Rule
     public TemporaryFolder tmp = new TemporaryFolder();
-    private long baselineMappedMemory = 0L;
 
     @Test
     public void shouldUnmapMemoryAsCycleRolls() throws Exception {
         final AtomicLong clock = new AtomicLong(System.currentTimeMillis());
+        long initialQueueMappedMemory = 0L;
 
         try (final SingleChronicleQueue queue = SingleChronicleQueueBuilder.
                 binary(tmp.newFolder()).testBlockSize().rollCycle(RollCycles.TEST_SECONDLY).
@@ -26,20 +26,22 @@ public final class MappedMemoryUnmappingTest {
             for (int i = 0; i < 100; i++) {
                 queue.acquireAppender().writeDocument(System.nanoTime(), (d, t) -> d.int64(t));
                 clock.addAndGet(TimeUnit.SECONDS.toMillis(1L));
-                if (baselineMappedMemory == 0L) {
-                    baselineMappedMemory = OS.memoryMapped();
+                if (initialQueueMappedMemory == 0L) {
+                    initialQueueMappedMemory = OS.memoryMapped();
                 }
             }
         }
 
+        GcControls.waitForGcCycle();
+
         final long timeoutAt = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(10L);
         while (System.currentTimeMillis() < timeoutAt) {
-            if (OS.memoryMapped() < 2 * baselineMappedMemory) {
+            if (OS.memoryMapped() < 2 * initialQueueMappedMemory) {
                 return;
             }
         }
 
         fail(String.format("Mapped memory (%dB) did not fall below threshold (%dB)",
-                OS.memoryMapped(), 2 * baselineMappedMemory));
+                OS.memoryMapped(), 2 * initialQueueMappedMemory));
     }
 }
