8ce3239bc8d9de3bd960afa3b3d94004a8a07a0b
danielshaya <daniel@familyshaya.com>
2018-02-15 21:31:03 +0000

    Added custom queue handling.

---
+++ src/main/java/net/openhft/chronicle/queue/reader/ChronicleReaderPlugin.java
@@ -0,0 +1,12 @@
+package net.openhft.chronicle.queue.reader;
+
+import net.openhft.chronicle.wire.DocumentContext;
+
+/**
+ * Handle the document from the queue that is read in <code>ChronicleReader</>.
+ * Particularly useful when you need more than the text representation e.g.
+ * when your queue is written in binary.
+ */
+public interface ChronicleReaderPlugin {
+    void onReadDocument(DocumentContext dc);
+}
