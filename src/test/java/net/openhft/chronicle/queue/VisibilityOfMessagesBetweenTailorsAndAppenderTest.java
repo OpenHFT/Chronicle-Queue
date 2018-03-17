db0ce42b5d0ec3565bf43a8238efd572ab4e5910
rob <rob.austin@boundedbuffer.com>
2018-03-17 22:18:05 +0000

    added test check if a message is written with an appender its visible to the tailor, without locks etc.

---
+++ src/test/java/net/openhft/chronicle/queue/VisibilityOfMessagesBetweenTailorsAndAppenderTest.java
@@ -0,0 +1,74 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.wire.DocumentContext;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import java.io.File;
+import java.util.UUID;
+import java.util.concurrent.*;
+
+public class VisibilityOfMessagesBetweenTailorsAndAppenderTest {
+    @Rule
+    public final TestName testName = new TestName();
+    volatile long lastWrittenIndex = Long.MIN_VALUE;
+
+    /**
+     * check if a message is written with an appender its visible to the tailor, without locks etc.
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Test
+    public void test() throws InterruptedException, ExecutionException {
+
+        SingleChronicleQueue x = SingleChronicleQueueBuilder.binary(getTmpDir()).build();
+
+        ExecutorService e1 = Executors.newSingleThreadExecutor();
+        e1.submit(() -> {
+            ExcerptAppender excerptAppender = x.acquireAppender();
+            for (long i = 0; i < 1_000_000; i++) {
+                try (DocumentContext dc = excerptAppender.writingDocument()) {
+                    dc.wire().getValueOut().int64(i);
+                }
+                lastWrittenIndex = excerptAppender.lastIndexAppended();
+
+            }
+
+        });
+
+        ExecutorService e2 = Executors.newSingleThreadExecutor();
+        Future f2 = e2.submit(() -> {
+            ExcerptTailer tailer = x.createTailer();
+
+            for (; ; ) {
+                long i = lastWrittenIndex;
+                if (i != Long.MIN_VALUE)
+                    if (!tailer.moveToIndex(i))
+                        throw new ExecutionException("non atomic, index=" + Long.toHexString(i), null);
+            }
+        });
+
+        try {
+            f2.get(5, TimeUnit.SECONDS);
+        } catch (TimeoutException ignore) {
+
+        }
+
+        e2.shutdownNow();
+        e1.shutdownNow();
+
+    }
+
+    @NotNull
+    protected File getTmpDir() {
+        final String methodName = testName.getMethodName();
+        return DirectoryUtils.tempDir(methodName != null ?
+                methodName.replaceAll("[\\[\\]\\s]+", "_") : "NULL-" + UUID.randomUUID());
+    }
+
+}
