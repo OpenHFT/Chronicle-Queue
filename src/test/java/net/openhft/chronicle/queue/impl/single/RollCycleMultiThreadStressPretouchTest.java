0bcc189b35feddf9e1314f8ce214e122a1af66c7
Jerry Shea <jerry.shea@chronicle.software>
2018-06-08 11:56:50 +1000

    Ignore #492

--- src/test/java/net/openhft/chronicle/queue/impl/single/RollCycleMultiThreadStressPretouchTest.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/RollCycleMultiThreadStressPretouchTest.java
@@ -1,10 +1,12 @@
 package net.openhft.chronicle.queue.impl.single;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RollCycleMultiThreadStressPretouchTest extends RollCycleMultiThreadStressTest {
 
     @Test
+    @Ignore("https://github.com/OpenHFT/Chronicle-Queue/issues/492")
     public void stress() {
         System.setProperty("pretouch", "true");
         super.stress();
