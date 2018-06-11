a9ebedc58541d9aea89eeaae5234165cfd9f9dcd
Rob Austin <rob.austin@boundedbuffer.co.uk>
2018-06-11 13:18:28 +0100

    fixes around reference counting - https://github.com/OpenHFT/Chronicle-Core/issues/70

--- src/main/java/net/openhft/chronicle/queue/impl/single/SingleChronicleQueueExcerpts.java
+++ src/main/java/net/openhft/chronicle/queue/impl/single/SingleChronicleQueueExcerpts.java
@@ -241,12 +241,13 @@ public class SingleChronicleQueueExcerpts {
 
             SingleChronicleQueue queue = this.queue;
 
-            if (this.store != null) {
-                storePool.release(this.store);
+            WireStore store = this.store;
+            if (store != null) {
+                storePool.release(store);
             }
 
             this.store = storePool.acquire(cycle, queue.epoch(), createIfAbsent);
-            closableResources.storeReference = store;
+            closableResources.storeReference = this.store;
             resetWires(queue);
 
             // only set the cycle after the wire is set.
@@ -268,7 +269,7 @@ public class SingleChronicleQueueExcerpts {
                 Wire oldw = this.wire;
                 this.wire = wireType.apply(store.bytes());
                 closableResources.wireReference = this.wire.bytes();
-
+                assert wire != oldw;
                 if (oldw != null) {
                     releaseWireResources(oldw);
                 }
@@ -277,7 +278,7 @@ public class SingleChronicleQueueExcerpts {
                 Wire old = this.wireForIndex;
                 this.wireForIndex = wireType.apply(store.bytes());
                 closableResources.wireForIndexReference = wireForIndex.bytes();
-
+                assert wire != old;
                 if (old != null) {
                     releaseWireResources(old);
                 }
@@ -1051,10 +1052,7 @@ public class SingleChronicleQueueExcerpts {
         }
 
         private void close() {
-            final Wire wire = context.wire();
-            if (wire != null) {
-                wire.bytes().release();
-            }
+            // the wire ref count will be released here by setting it to null
             context.wire(null);
             Wire w0 = wireForIndex;
             if (w0 != null)
@@ -1639,6 +1637,7 @@ public class SingleChronicleQueueExcerpts {
             closableResources.wireForIndexReference = wireForIndex.bytes();
             closableResources.wireReference = wire.bytes();
             assert headerNumberCheck((AbstractWire) wireForIndex);
+            assert wire != wireForIndexOld;
 
             if (wireForIndexOld != null) {
                 releaseWireResources(wireForIndexOld);
@@ -2124,6 +2123,8 @@ public class SingleChronicleQueueExcerpts {
             }
 
             public void wire(@Nullable AbstractWire wire) {
+                if (wire == this.wire)
+                    return;
 
                 AbstractWire oldWire = this.wire;
                 this.wire = wire;
