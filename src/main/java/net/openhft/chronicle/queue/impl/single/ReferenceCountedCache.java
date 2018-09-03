0f9cba1d827439c9c893bbf0b04aa3988fbe71d0
rob <rob.austin@boundedbuffer.com>
2018-09-03 14:25:16 +0100

    fixed https://github.com/OpenHFT/Chronicle-Queue/issues/479

--- src/main/java/net/openhft/chronicle/queue/impl/single/ReferenceCountedCache.java
+++ src/main/java/net/openhft/chronicle/queue/impl/single/ReferenceCountedCache.java
@@ -11,7 +11,7 @@ import java.util.function.Function;
 
 /**
  * Thread-safe, self-cleaning cache for ReferenceCounted objects.
- *
+ * <p>
  * Created by Jerry Shea on 27/04/18.
  */
 public class ReferenceCountedCache<K, T extends ReferenceCounted, V, E extends Throwable> {
@@ -26,26 +26,24 @@ public class ReferenceCountedCache<K, T extends ReferenceCounted, V, E extends T
 
     @NotNull
     V get(@NotNull final K key) throws E {
-        boolean created = false;
 
         // remove all which have been dereferenced. Garbagy but rare
         cache.entrySet().removeIf(entry -> entry.getValue().refCount() == 0);
 
         @Nullable T value = cache.get(key);
+
         // another thread may have reduced refCount since removeIf above
-        if (value == null || value.refCount() == 0) {
+        if (value == null || !value.tryReserve()) {
             // worst case is that 2 threads create at 'same' time
             value = creator.apply(key);
             cache.put(key, value);
-            created = true;
         }
 
         try {
             return transformer.apply(value);
         } finally {
-            // release if we just created it
-            if (created)
-                value.release();
+            // release whether we created a new one or not
+            value.release();
         }
     }
 }
