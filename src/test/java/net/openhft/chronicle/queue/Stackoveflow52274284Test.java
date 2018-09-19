77757c22e5e7dc7767d2002794ce2048711102fa
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-09-19 19:09:42 +0100

    Add queue based ring buffer

---
+++ src/test/java/net/openhft/chronicle/queue/Stackoveflow52274284Test.java
@@ -0,0 +1,61 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.wire.DocumentContext;
+import net.openhft.chronicle.wire.Wire;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Stackoveflow52274284Test {
+    @Test
+    public void fails() throws Exception {
+        String basePath = OS.TMP;
+        String path = Files.createTempDirectory(Paths.get(basePath), "chronicle-")
+                .toAbsolutePath()
+                .toString();
+        System.out.printf("Using temp path '%s'%n", path);
+
+        try (ChronicleQueue chronicleQueue = ChronicleQueue.single(path)) {
+
+            // Create Appender
+            ExcerptAppender appender = chronicleQueue.acquireAppender();
+
+            // Create Tailer
+            ExcerptTailer tailer = chronicleQueue.createTailer();
+            tailer.toStart();
+
+            int numberOfRecords = 10;
+
+            // Write
+            for (int i = 0; i <= numberOfRecords; i++) {
+                System.out.println("Writing " + i);
+                try (final DocumentContext dc = appender.writingDocument()) {
+                    dc.wire().write(() -> "msg").text("Hello World!");
+                    System.out.println("your data was store to index=" + dc.index());
+                } catch (Exception e) {
+                    System.err.println("Unable to store value to chronicle");
+                    e.printStackTrace();
+                }
+            }
+            // Read
+            for (int i = 0; i <= numberOfRecords; i++) {
+                System.out.println("Reading " + i);
+                try (DocumentContext documentContext = tailer.readingDocument()) {
+                    long currentOffset = documentContext.index();
+                    System.out.println("Current offset: " + currentOffset);
+
+                    Wire wire = documentContext.wire();
+
+                    if (wire != null) {
+                        String msg = wire
+                                .read("msg")
+                                .text();
+                        System.out.println(msg);
+                    }
+                }
+            }
+        }
+    }
+}
