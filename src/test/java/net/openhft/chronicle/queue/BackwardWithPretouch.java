6030564057769e80f1efd3dfa72ff112e7588c20
Rob Austin <rob.austin@boundedbuffer.co.uk>
2018-10-26 20:04:17 +0100

    https://github.com/OpenHFT/Chronicle-Queue/issues/541 added failing test

---
+++ src/test/java/net/openhft/chronicle/queue/BackwardWithPretouch.java
@@ -0,0 +1,31 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * Created by Rob Austin
+ */
+public class BackwardWithPretouch {
+
+    @Test
+    public void excerptAppender() throws Exception {
+
+        SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(UUID.randomUUID().toString()).rollCycle(RollCycles.TEST_SECONDLY).build();
+        ExcerptAppender excerptAppender = queue.acquireAppender();
+        excerptAppender.writeText("hello");
+
+        // sleep til next cycle
+        Thread.sleep(1000);
+
+        // pretouch to create next cycle file  ----- IF YOU UNCOMMENT THIS LINE THE TEST PASSES
+        excerptAppender.pretouch();
+
+        ExcerptTailer tailer = queue.createTailer().direction(TailerDirection.BACKWARD);
+        Assert.assertEquals("hello", tailer.toEnd().readText());
+    }
+}
