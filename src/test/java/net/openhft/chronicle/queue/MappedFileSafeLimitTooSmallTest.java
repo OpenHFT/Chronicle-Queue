37ccdb758d59d2ab475c24e87c012ba5134bc7ba
Rob Austin <rob.austin@boundedbuffer.co.uk>
2018-10-31 10:44:12 +0000

    https://github.com/OpenHFT/Chronicle-Queue/issues/535  now throws a DecoratedBufferOverflowException rather than crashing the JVM

---
+++ src/test/java/net/openhft/chronicle/queue/MappedFileSafeLimitTooSmallTest.java
@@ -0,0 +1,87 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.bytes.util.DecoratedBufferOverflowException;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.wire.DocumentContext;
+import net.openhft.chronicle.wire.WireType;
+import org.junit.Assert;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * see https://github.com/OpenHFT/Chronicle-Queue/issues/535
+ * Created by Rob Austin
+ */
+public class MappedFileSafeLimitTooSmallTest extends ChronicleQueueTestBase {
+
+    @org.junit.Test(expected = DecoratedBufferOverflowException.class)
+    public void testMappedFileSafeLimitTooSmall() {
+
+        final int arraySize = 40_000;
+        final int blockSize = arraySize * 6;
+        byte[] data = new byte[arraySize];
+        Arrays.fill(data, (byte) 'x');
+        File tmpDir = getTmpDir();
+
+        try (final ChronicleQueue queue =
+                     SingleChronicleQueueBuilder.builder(tmpDir, WireType.BINARY).blockSize(blockSize).build()) {
+
+            for (int i = 0; i < 5; i++) {
+                try (DocumentContext dc = queue.acquireAppender().writingDocument()) {
+                    System.out.println(dc.wire().bytes().writeRemaining());
+                    dc.wire().write("data").bytes(data);
+                }
+
+            }
+        }
+
+        try (final ChronicleQueue queue =
+                     SingleChronicleQueueBuilder.builder(tmpDir, WireType.BINARY).blockSize(blockSize).build()) {
+
+            for (int i = 0; i < 5; i++) {
+                try (DocumentContext dc = queue.createTailer().readingDocument()) {
+                    Assert.assertArrayEquals(data, dc.wire().read("data").bytes());
+                }
+
+            }
+
+        }
+
+    }
+
+    @org.junit.Test
+    public void testMappedFileSafeLimit() {
+
+        final int arraySize = 40_000;
+        final int blockSize = arraySize * 7;
+        byte[] data = new byte[arraySize];
+        Arrays.fill(data, (byte) 'x');
+        File tmpDir = getTmpDir();
+
+        try (final ChronicleQueue queue =
+                     SingleChronicleQueueBuilder.builder(tmpDir, WireType.BINARY).blockSize(blockSize).build()) {
+
+            for (int i = 0; i < 5; i++) {
+                try (DocumentContext dc = queue.acquireAppender().writingDocument()) {
+                    System.out.println(dc.wire().bytes().writeRemaining());
+                    dc.wire().write("data").bytes(data);
+                }
+
+            }
+        }
+
+        try (final ChronicleQueue queue =
+                     SingleChronicleQueueBuilder.builder(tmpDir, WireType.BINARY).blockSize(blockSize).build()) {
+
+            for (int i = 0; i < 5; i++) {
+                try (DocumentContext dc = queue.createTailer().readingDocument()) {
+                    Assert.assertArrayEquals(data, dc.wire().read("data").bytes());
+                }
+
+            }
+
+        }
+
+    }
+}
