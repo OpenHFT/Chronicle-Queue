f22530ba59a56a6a47b73839d8d420f573d23bd8
Dmitry Pisklov <dpisklov@gmail.com>
2019-04-10 16:07:05 +0100

    Cleanup

--- src/main/java/net/openhft/chronicle/queue/TailerDirection.java
+++ src/main/java/net/openhft/chronicle/queue/TailerDirection.java
@@ -16,9 +16,6 @@
 
 package net.openhft.chronicle.queue;
 
-/*
- * Created by Peter Lawrey on 05/03/2016.
- */
 public enum TailerDirection {
     NONE(0), // don't move after a read.
     FORWARD(+1), // move to the next entry
