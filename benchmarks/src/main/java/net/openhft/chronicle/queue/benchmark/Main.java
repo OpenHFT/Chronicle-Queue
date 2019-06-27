6f04a6bf8b7961dd989de57d146dcb0d12d9f7de
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-06-27 16:00:58 +0100

    Benchmark module for Chronicle-Queue

--- benchmarks/src/main/java/net/openhft/chronicle/queue/benchmark/Main.java
+++ benchmarks/src/main/java/net/openhft/chronicle/queue/benchmark/Main.java
@@ -6,14 +6,18 @@ public class Main {
     static final int time = Integer.getInteger("time", 20);
     static final int size = Integer.getInteger("size", 60);
     static final String path = System.getProperty("path", OS.TMP);
-    static final int throughput = Integer.parseInt("throughput", 1_000_000);
+    static final int throughput = Integer.getInteger("throughput", 1_000_000);
 
     static final boolean SAMPLING = Boolean.getBoolean("sampling");
     static final int ITERATIONS = Integer.getInteger("iterations", 60_000_000);
     static final int WARMUP = 500_000;
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Writing with -Dtime=" + time + " -Dsize=" + size + " -Dpath=" + path);
+        System.out.println("Writing with " +
+                "-Dtime=" + time + " " +
+                "-Dsize=" + size + " " +
+                "-Dpath=" + path + " " +
+                "-Dthroughput=" + throughput);
         ThroughputMain.main(args);
         LatencyDistributionMain.main(args);
     }
