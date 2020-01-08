18708f7e0c3db17bcac58a39ab95a097b1037f75
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-01-08 13:51:36 +0000

    added net.openhft.chronicle.queue.ChronicleQueueIndexTest.testIndexQueue

--- src/test/java/net/openhft/chronicle/queue/ChronicleQueueIndexTest.java
+++ src/test/java/net/openhft/chronicle/queue/ChronicleQueueIndexTest.java
@@ -12,6 +12,8 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.requireNonNull;
@@ -95,4 +97,49 @@ public class ChronicleQueueIndexTest {
         System.out.println(eof);
     }
 
+
+    @Test
+    public void testIndexQueue() {
+
+        ChronicleQueue queue = SingleChronicleQueueBuilder.builder()
+                .path("test-chronicle")
+                .rollCycle(RollCycles.DAILY)
+                .build();
+        InternalAppender appender = (InternalAppender)queue.acquireAppender();
+
+        Bytes<byte[]> hello_world = Bytes.fromString("Hello World 1");
+        appender.writeBytes(RollCycles.DAILY.toIndex(18264, 0L), hello_world);
+        hello_world = Bytes.fromString("Hello World 2");
+        appender.writeBytes(RollCycles.DAILY.toIndex(18264, 1L), hello_world);
+
+
+        // Simulate the end of the day i.e the queue closes the day rolls
+        // (note the change of index from 18264 to 18265)
+        queue.close();
+        queue = SingleChronicleQueueBuilder.builder()
+                .path("test-chronicle")
+                .rollCycle(RollCycles.DAILY)
+                .build();
+        appender = (InternalAppender)queue.acquireAppender();
+
+        // add a message for the new day
+        hello_world = Bytes.fromString("Hello World 3");
+        appender.writeBytes(RollCycles.DAILY.toIndex(18265, 0L), hello_world);
+
+        final ExcerptTailer tailer = queue.createTailer();
+
+        final Bytes forRead = Bytes.elasticByteBuffer();
+        final List<String> results = new ArrayList<>();
+        while(tailer.readBytes(forRead)) {
+            results.add(forRead.toString());
+            forRead.clear();
+        }
+        Assert.assertTrue(results.contains("Hello World 1"));
+        Assert.assertTrue(results.contains("Hello World 2"));
+        // The reader fails to read the third message. The reason for this is
+        // that there was no EOF marker placed at end of the 18264 indexed file
+        // so when the reader started reading through the queues it got stuck on
+        // that file and never progressed to the latest queue file.
+        Assert.assertTrue(results.contains("Hello World 3"));
+    }
 }
