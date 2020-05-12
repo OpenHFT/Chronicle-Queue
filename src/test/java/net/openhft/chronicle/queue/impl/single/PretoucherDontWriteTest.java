56c9c772c6841d933259a4a858f4554bb02a532d
Jerry Shea <jerry.shea@chronicle.software>
2020-05-12 18:29:09 +1000

    refactor PretoucherTest* to make use of separate JVM per test class

---
+++ src/test/java/net/openhft/chronicle/queue/impl/single/PretoucherDontWriteTest.java
@@ -0,0 +1,56 @@
+package net.openhft.chronicle.queue.impl.single;
+
+import net.openhft.chronicle.core.Jvm;
+import net.openhft.chronicle.core.threads.InvalidEventHandlerException;
+import net.openhft.chronicle.wire.DocumentContext;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.util.stream.IntStream.range;
+import static net.openhft.chronicle.queue.DirectoryUtils.tempDir;
+import static org.junit.Assert.assertEquals;
+
+public class PretoucherDontWriteTest {
+    private final AtomicLong clock = new AtomicLong(System.currentTimeMillis());
+    private final List<Integer> capturedCycles = new ArrayList<>();
+    private final PretoucherTest.CapturingChunkListener chunkListener = new PretoucherTest.CapturingChunkListener();
+
+    @Test
+    public void dontWrite() {
+
+        System.setProperty("SingleChronicleQueueExcerpts.dontWrite", "true");
+        File dir = tempDir("shouldNotRoll");
+
+        try (final SingleChronicleQueue queue = PretoucherTest.createQueue(dir, clock::get);
+             final Pretoucher pretoucher = new Pretoucher(PretoucherTest.createQueue(dir, clock::get), chunkListener, capturedCycles::add)) {
+
+            range(0, 10).forEach(i -> {
+                try (final DocumentContext ctx = queue.acquireAppender().writingDocument()) {
+                    assertEquals(i, capturedCycles.size());
+                    ctx.wire().write().int32(i);
+                    ctx.wire().write().bytes(new byte[1024]);
+                }
+                try {
+                    pretoucher.execute();
+                } catch (InvalidEventHandlerException e) {
+                    throw Jvm.rethrow(e);
+                }
+                assertEquals(i + 1, capturedCycles.size());
+                clock.addAndGet(TimeUnit.SECONDS.toMillis(5L));
+                try {
+                    pretoucher.execute();
+                } catch (InvalidEventHandlerException e) {
+                    throw Jvm.rethrow(e);
+                }
+                assertEquals(i + 1, capturedCycles.size());
+            });
+
+            assertEquals(10, capturedCycles.size());
+        }
+    }
+}
