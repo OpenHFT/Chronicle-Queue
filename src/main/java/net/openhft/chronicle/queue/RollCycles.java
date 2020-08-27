242decca6422cb3a022ccc9f994f78caecdd05a2
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-08-27 09:39:54 +0100

    Added some options for rolling every 5m, 10m, 20m, 30m, 2h, 4h, 6h and restructured existing options to be clearer.

--- src/main/java/net/openhft/chronicle/queue/RollCycles.java
+++ src/main/java/net/openhft/chronicle/queue/RollCycles.java
@@ -29,29 +29,69 @@ import java.util.Arrays;
  */
 public enum RollCycles implements RollCycle {
     /**
-     * Only good for testing
-     */
-    TEST_SECONDLY(/*---*/"yyyyMMdd-HHmmss'T'", 1000, 1 << 15, 4),
-    /**
-     * Only good for testing
-     */
-    TEST4_SECONDLY(/*---*/"yyyyMMdd-HHmmss'T4'", 1000, 32, 4),
-    /**
-     * 64 million entries per minute
+     * 64 million entries per minute, indexing every 64th entry
      */
     MINUTELY(/*--------*/"yyyyMMdd-HHmm", 60 * 1000, 2 << 10, 16),
     /**
-     * 512 entries per hour
+     * 1 billion entries per 5 minutes, indexing every 64th entry
      */
-    TEST_HOURLY(/*-----*/"yyyyMMdd-HH'T'", 60 * 60 * 1000, 16, 4),
+    FIVE_MINUTELY(/*------*/"yyyyMMdd-HHmm'V'", 5 * 60 * 1000, 4 << 10, 64),
+    /**
+     * 1 billion entries per 10 minutes, indexing every 64th entry
+     */
+    TEN_MINUTELY(/*------*/"yyyyMMdd-HHmm'X'", 10 * 60 * 1000, 4 << 10, 64),
+    /**
+     * 1 billion entries per 20 minutes, indexing every 64th entry
+     */
+    TWENTY_MINUTELY(/*------*/"yyyyMMdd-HHmm'XX'", 20 * 60 * 1000, 4 << 10, 64),
+    /**
+     * 1 billion entries per half hour, indexing every 64th entry
+     */
+    HALF_HOURLY(/*------*/"yyyyMMdd-HHmm'H'", 30 * 60 * 1000, 4 << 10, 64),
     /**
      * 256 million entries per hour, indexing every 16th entry
      */
     HOURLY(/*----------*/"yyyyMMdd-HH", 60 * 60 * 1000, 4 << 10, 16),
     /**
-     * 2 billion entries per hour, indexing every 64th entry
+     * 4 billion entries per hour, indexing every 64th entry
      */
     LARGE_HOURLY(/*----*/"yyyyMMdd-HH'L'", 60 * 60 * 1000, 8 << 10, 64),
+    /**
+     * 4 billion entries per 2 hours, indexing every 64th entry
+     */
+    TWO_HOURLY(/*----*/"yyyyMMdd-HH'II'", 2 * 60 * 60 * 1000, 8 << 10, 64),
+    /**
+     * 4 billion entries per four hours, indexing every 64th entry
+     */
+    FOUR_HOURLY(/*----*/"yyyyMMdd-HH'IV'", 4 * 60 * 60 * 1000, 8 << 10, 64),
+    /**
+     * 4 billion entries per six hours, indexing every 64th entry
+     */
+    SIX_HOURLY(/*----*/"yyyyMMdd-HH'VI'", 6 * 60 * 60 * 1000, 8 << 10, 64),
+    /**
+     * 4 billion entries per day, indexing every 64th entry
+     */
+    DAILY(/*-----------*/"yyyyMMdd", 24 * 60 * 60 * 1000, 8 << 10, 64),
+
+    // these are used to minimise rolls but do create very large files, possibly too large.
+    /**
+     * 128 billion entries per day, indexing every 128th entry
+     */
+    LARGE_DAILY(/*-----*/"yyyyMMdd'L'", 24 * 60 * 60 * 1000, 32 << 10, 128),
+    /**
+     * 4 trillion entries per day, indexing every 256th entry
+     */
+    XLARGE_DAILY(/*----*/"yyyyMMdd'X'", 24 * 60 * 60 * 1000, 128 << 10, 256),
+    /**
+     * 256 trillion entries per day with sparse indexing (every 1024th entry)
+     */
+    HUGE_DAILY(/*------*/"yyyyMMdd'H'", 24 * 60 * 60 * 1000, 512 << 10, 1024),
+
+    // these are largely used for testing and benchmarks to almost turn off indexing.
+    /**
+     * 512 million entries per day
+     */
+    SMALL_DAILY(/*-----*/"yyyyMMdd'S'", 24 * 60 * 60 * 1000, 8 << 10, 8),
     /**
      * 16 billion entries per hour with sparse indexing (every 1024th entry)
      */
@@ -60,6 +100,24 @@ public enum RollCycles implements RollCycle {
      * 16 billion entries per hour with super-sparse indexing (every (2^20)th entry)
      */
     LARGE_HOURLY_XSPARSE("yyyyMMdd-HH'LX'", 60 * 60 * 1000, 2 << 10, 1 << 20),
+    /**
+     * 256 trillion entries per day with super-sparse indexing (every (2^20)th entry)
+     */
+    HUGE_DAILY_XSPARSE("yyyyMMdd'HX'", 24 * 60 * 60 * 1000, 16 << 10, 1 << 20),
+
+    // these are used for test to reduce the size of a queue dump when doing a small test.
+    /**
+     * Only good for testing
+     */
+    TEST_SECONDLY(/*---*/"yyyyMMdd-HHmmss'T'", 1000, 1 << 15, 4),
+    /**
+     * Only good for testing
+     */
+    TEST4_SECONDLY(/*---*/"yyyyMMdd-HHmmss'T4'", 1000, 32, 4),
+    /**
+     * 512 entries per hour
+     */
+    TEST_HOURLY(/*-----*/"yyyyMMdd-HH'T'", 60 * 60 * 1000, 16, 4),
     /**
      * Only good for testing - 63 entries per day
      */
@@ -76,30 +134,6 @@ public enum RollCycles implements RollCycle {
      * Only good for testing
      */
     TEST8_DAILY(/*-----*/"yyyyMMdd'T8'", 24 * 60 * 60 * 1000, 128, 8),
-    /**
-     * 512 million entries per day
-     */
-    SMALL_DAILY(/*-----*/"yyyyMMdd'S'", 24 * 60 * 60 * 1000, 8 << 10, 8),
-    /**
-     * 4 billion entries per day, indexing every 64th entry
-     */
-    DAILY(/*-----------*/"yyyyMMdd", 24 * 60 * 60 * 1000, 8 << 10, 64),
-    /**
-     * 128 billion entries per day, indexing every 128th entry
-     */
-    LARGE_DAILY(/*-----*/"yyyyMMdd'L'", 24 * 60 * 60 * 1000, 32 << 10, 128),
-    /**
-     * 4 trillion entries per day, indexing every 256th entry
-     */
-    XLARGE_DAILY(/*----*/"yyyyMMdd'X'", 24 * 60 * 60 * 1000, 128 << 10, 256),
-    /**
-     * 256 trillion entries per day with sparse indexing (every 1024th entry)
-     */
-    HUGE_DAILY(/*------*/"yyyyMMdd'H'", 24 * 60 * 60 * 1000, 512 << 10, 1024),
-    /**
-     * 256 trillion entries per day with super-sparse indexing (every (2^20)th entry)
-     */
-    HUGE_DAILY_XSPARSE("yyyyMMdd'HX'", 24 * 60 * 60 * 1000, 16 << 10, 1 << 20),
     ;
 
     // don't alter this or you will confuse yourself.
@@ -112,10 +146,6 @@ public enum RollCycles implements RollCycle {
     private final int indexSpacing;
     private final long sequenceMask;
 
-    public static Iterable<RollCycles> all() {
-        return VALUES;
-    }
-
     RollCycles(String format, int length, int indexCount, int indexSpacing) {
         this.format = format;
         this.length = length;
@@ -123,6 +153,10 @@ public enum RollCycles implements RollCycle {
         this.indexSpacing = Maths.nextPower2(indexSpacing, 1);
         cycleShift = Math.max(32, Maths.intLog2(indexCount) * 2 + Maths.intLog2(indexSpacing));
         sequenceMask = (1L << cycleShift) - 1;
+    }
+
+    public static Iterable<RollCycles> all() {
+        return VALUES;
     }
 
     @Override
@@ -136,7 +170,6 @@ public enum RollCycles implements RollCycle {
     }
 
     /**
-     *
      * @return this is the size of each index array, note: indexCount^2 is the maximum number of index queue entries.
      */
     @Override
