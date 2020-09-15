e024c57637fa1a1f7e86ace74d9bf5d99835a85c
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-09-15 08:45:02 +0100

    Added support for mapping interfaces to a default implementation, https://github.com/OpenHFT/Chronicle-Wire/issues/200

--- src/test/java/net/openhft/chronicle/queue/RollCycleDefaultingTest.java
+++ src/test/java/net/openhft/chronicle/queue/RollCycleDefaultingTest.java
@@ -1,20 +1,27 @@
 package net.openhft.chronicle.queue;
 
 import net.openhft.chronicle.core.time.TimeProvider;
+import net.openhft.chronicle.core.util.ObjectUtils;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder.DEFAULT_ROLL_CYCLE_PROPERTY;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RollCycleDefaultingTest extends QueueTestCommon {
+    @Test
+    public void alias() {
+        assertEquals(RollCycles.class, ObjectUtils.implementationToUse(RollCycle.class));
+    }
+
     @Test
     public void correctConfigGetsLoaded() {
         String aClass = RollCycles.HOURLY.getClass().getName();
         String configuredCycle = aClass + ":HOURLY";
         System.setProperty(DEFAULT_ROLL_CYCLE_PROPERTY, configuredCycle);
         SingleChronicleQueueBuilder builder = SingleChronicleQueueBuilder.binary("test");
-        Assert.assertEquals(RollCycles.HOURLY, builder.rollCycle());
+        assertEquals(RollCycles.HOURLY, builder.rollCycle());
     }
 
     @Test
@@ -23,7 +30,7 @@ public class RollCycleDefaultingTest extends QueueTestCommon {
         System.setProperty(DEFAULT_ROLL_CYCLE_PROPERTY, configuredCycle);
         SingleChronicleQueueBuilder builder = SingleChronicleQueueBuilder.binary("test");
 
-        Assert.assertTrue(builder.rollCycle() instanceof MyRollcycle);
+        assertTrue(builder.rollCycle() instanceof MyRollcycle);
     }
 
     @Test
@@ -31,7 +38,7 @@ public class RollCycleDefaultingTest extends QueueTestCommon {
         String configuredCycle = "foobarblah";
         System.setProperty(DEFAULT_ROLL_CYCLE_PROPERTY, configuredCycle);
         SingleChronicleQueueBuilder builder = SingleChronicleQueueBuilder.binary("test");
-        Assert.assertEquals(RollCycles.DEFAULT, builder.rollCycle());
+        assertEquals(RollCycles.DEFAULT, builder.rollCycle());
 
     }
 
@@ -40,7 +47,7 @@ public class RollCycleDefaultingTest extends QueueTestCommon {
         String configuredCycle = String.class.getName();
         System.setProperty(DEFAULT_ROLL_CYCLE_PROPERTY, configuredCycle);
         SingleChronicleQueueBuilder builder = SingleChronicleQueueBuilder.binary("test");
-        Assert.assertEquals(RollCycles.DEFAULT, builder.rollCycle());
+        assertEquals(RollCycles.DEFAULT, builder.rollCycle());
     }
 
     public static class MyRollcycle implements RollCycle {
