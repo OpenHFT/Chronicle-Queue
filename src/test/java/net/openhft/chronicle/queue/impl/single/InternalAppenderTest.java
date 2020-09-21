b2424a0309c6136a94d1845e23a28115fcba4137
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-09-21 14:00:45 +0100

    Added support for custom replication allowing you to add any cycle, even if another appender has been used, closes https://github.com/OpenHFT/Chronicle-Queue/issues/705

---
+++ src/test/java/net/openhft/chronicle/queue/impl/single/InternalAppenderTest.java
@@ -0,0 +1,50 @@
+package net.openhft.chronicle.queue.impl.single;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.io.IOTools;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+
+public class InternalAppenderTest {
+
+    @Test
+    public void replicationTest() throws Exception {
+        final File file = Files.createTempDirectory("queue").toFile();
+        try (final SingleChronicleQueue queue =
+                     SingleChronicleQueueBuilder.single(file).build()) {
+            final long index = queue.rollCycle().toIndex(queue.cycle(), 0);
+            final InternalAppender appender = (InternalAppender) queue.acquireAppender();
+
+            // First, we replicate a message, using the InternalAppender
+            // interface because we need to preserve index numbers.
+            appender.writeBytes(index, Bytes.from("Replicated"));
+
+            // Next, a message is written locally by another app (usually a
+            // different process).
+            try (final SingleChronicleQueue app = SingleChronicleQueueBuilder.single(file).build()) {
+                app.acquireAppender().writeBytes(Bytes.from("Written locally"));
+            }
+
+            // The other app exits, and at some point later we need to start
+            // replicating again.
+            //
+            // BUG!! This throws IllegalStateException because the Wire instance
+            // in the appender caches the old header number.
+
+            // Uncomment the next line as a work-around:
+            //appender.wire().headerNumber(queue.createTailer().toEnd().index() - 1);
+
+            appender.writeBytes(index + 2, Bytes.from("Replicated 2"));
+
+            // We should have three messages in our queue.
+            assertEquals(queue.entryCount(), 3);
+
+        } finally {
+            IOTools.deleteDirWithFiles(file);
+        }
+    }
+}
