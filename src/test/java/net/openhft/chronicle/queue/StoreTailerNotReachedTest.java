191a858298067a53d5c74c622ae5922c471977b2
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-10-07 18:04:05 +0100

    Check a tailer recovers after getting into a NOT_REACHED state

---
+++ src/test/java/net/openhft/chronicle/queue/StoreTailerNotReachedTest.java
@@ -0,0 +1,26 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class StoreTailerNotReachedTest {
+    @Test
+    public void afterNotReached() {
+        String path = OS.getTarget() + "/afterNotReached-" + System.nanoTime();
+        try (ChronicleQueue q = SingleChronicleQueueBuilder.binary(path)
+                .testBlockSize()
+                .build()) {
+            q.acquireAppender().writeText("Hello");
+            ExcerptTailer tailer = q.createTailer();
+            assertEquals("Hello", tailer.readText());
+            assertNull(tailer.readText());
+            q.acquireAppender().writeText("World");
+            assertEquals("World", tailer.readText());
+            assertNull(tailer.readText());
+        }
+    }
+}
