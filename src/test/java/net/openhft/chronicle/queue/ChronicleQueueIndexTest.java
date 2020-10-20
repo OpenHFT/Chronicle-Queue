ca0b4203fb2274a4d73b1def0bad05c22d5a62b2
Ivan Rakov <ivan.glukos@gmail.com>
2020-10-20 16:13:44 +0300

    Don't reset the cycle on a NOT_REACHED state + adapt data grid to new behaviour, closes https://github.com/OpenHFT/Chronicle-Queue/issues/734

--- src/test/java/net/openhft/chronicle/queue/ChronicleQueueIndexTest.java
+++ src/test/java/net/openhft/chronicle/queue/ChronicleQueueIndexTest.java
@@ -7,7 +7,10 @@ import net.openhft.chronicle.core.time.SetTimeProvider;
 import net.openhft.chronicle.queue.impl.single.InternalAppender;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import net.openhft.chronicle.wire.DocumentContext;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Test;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -213,7 +216,6 @@ public class ChronicleQueueIndexTest extends ChronicleQueueTestBase {
         AbstractReferenceCounted.assertReferencesReleased();
     }
 
-    @Ignore("see https://github.com/OpenHFT/Chronicle-Queue/issues/734")
     @Test
     public void read5thMessageTest() throws InterruptedException {
         SetTimeProvider stp = new SetTimeProvider();
