02dfd0f491f1a3d3c570042fcc420ecc766d83bb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-10-29 15:55:49 +0000

    attempting to make the test less flaky

--- src/test/java/net/openhft/chronicle/queue/VisibilityOfMessagesBetweenTailorsAndAppenderTest.java
+++ src/test/java/net/openhft/chronicle/queue/VisibilityOfMessagesBetweenTailorsAndAppenderTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 import java.util.concurrent.*;
 
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
+
 @RequiredForClient
 public class VisibilityOfMessagesBetweenTailorsAndAppenderTest extends ChronicleQueueTestBase {
 
@@ -28,29 +30,34 @@ public class VisibilityOfMessagesBetweenTailorsAndAppenderTest extends Chronicle
                 .rollCycle(RollCycles.MINUTELY)
                 .build()) {
 
-            ExecutorService e1 = Executors.newSingleThreadExecutor(
-                    new NamedThreadFactory("e1"));
-            e1.submit(() -> {
-                ExcerptAppender excerptAppender = x.acquireAppender();
-                for (long i = 0; i < 1_000_000; i++) {
-                    try (DocumentContext dc = excerptAppender.writingDocument()) {
-                        dc.wire().getValueOut().int64(i);
-                    }
-                    lastWrittenIndex = excerptAppender.lastIndexAppended();
+            ExecutorService e1 = newSingleThreadExecutor(new NamedThreadFactory("e1"));
 
+            e1.submit(() -> {
+                try (ExcerptAppender excerptAppender = x.acquireAppender()) {
+                    for (long i = 0; i < 1_000_000; i++) {
+                        try (DocumentContext dc = excerptAppender.writingDocument()) {
+                            dc.wire().getValueOut().int64(i);
+                        }
+                        lastWrittenIndex = excerptAppender.lastIndexAppended();
+                        if (Thread.currentThread().isInterrupted())
+                            return null;
+                    }
                 }
+                return null;
             });
 
-            ExecutorService e2 = Executors.newSingleThreadExecutor(
-                    new NamedThreadFactory("e2"));
-            Future f2 = e2.submit(() -> {
-                ExcerptTailer tailer = x.createTailer();
+            ExecutorService e2 = newSingleThreadExecutor(new NamedThreadFactory("e2"));
+            Future<Void> f2 = e2.submit(() -> {
+                try (ExcerptTailer tailer = x.createTailer()) {
 
-                for (; ; ) {
-                    long i = lastWrittenIndex;
-                    if (i != Long.MIN_VALUE)
-                        if (!tailer.moveToIndex(i))
-                            throw new ExecutionException("non atomic, index=" + Long.toHexString(i), null);
+                    for (; ; ) {
+                        long i = lastWrittenIndex;
+                        if (i != Long.MIN_VALUE)
+                            if (!tailer.moveToIndex(i))
+                                throw new ExecutionException("non atomic, index=" + Long.toHexString(i), null);
+                        if (Thread.currentThread().isInterrupted())
+                            return null;
+                    }
                 }
             });
 
@@ -60,8 +67,15 @@ public class VisibilityOfMessagesBetweenTailorsAndAppenderTest extends Chronicle
 
             }
 
-            e2.shutdownNow();
-            e1.shutdownNow();
+            e1.shutdown();
+            e2.shutdown();
+
+            if (!e1.awaitTermination(1, TimeUnit.SECONDS)) {
+                e1.shutdownNow();
+            }
+            if (!e2.awaitTermination(1, TimeUnit.SECONDS)) {
+                e2.shutdownNow();
+            }
         }
     }
 }
