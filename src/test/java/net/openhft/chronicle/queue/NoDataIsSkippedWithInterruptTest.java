4d807fc344bc2467b917f4ec9f2184571bcb3749
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-10-29 12:13:48 +0000

    #465 included a version of the test that also rolls

--- src/test/java/net/openhft/chronicle/queue/NoDataIsSkippedWithInterruptTest.java
+++ src/test/java/net/openhft/chronicle/queue/NoDataIsSkippedWithInterruptTest.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.queue;
 
+import net.openhft.chronicle.core.time.SetTimeProvider;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import org.junit.Assert;
@@ -14,12 +15,20 @@ public class NoDataIsSkippedWithInterruptTest {
 
         Thread.currentThread().interrupt();
 
-        try (SingleChronicleQueue q = SingleChronicleQueueBuilder.single(DirectoryUtils.tempDir(".")).build();
+        final SetTimeProvider timeProvider = new SetTimeProvider();
+        try (SingleChronicleQueue q = SingleChronicleQueueBuilder.single(DirectoryUtils.tempDir("."))
+                .rollCycle(RollCycles.MINUTELY).timeProvider(timeProvider).build();
              final ExcerptAppender excerptAppender = q.acquireAppender();
              final ExcerptTailer tailer = q.createTailer()) {
 
             excerptAppender.writeText(EXPECTED);
+            timeProvider.advanceMillis(60_000);
+
+            excerptAppender.writeText(EXPECTED);
+
             Assert.assertEquals(EXPECTED, tailer.readText());
+            Assert.assertEquals(EXPECTED, tailer.readText());
+            Assert.assertTrue(Thread.currentThread().isInterrupted());
         }
     }
 
