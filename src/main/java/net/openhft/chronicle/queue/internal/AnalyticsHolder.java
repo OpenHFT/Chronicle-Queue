dcada511ea5ad48150ee5727950e89fd828a65da
Per Minborg <minborg@speedment.com>
2020-12-02 09:01:40 +0100

    Update analytics settings

--- src/main/java/net/openhft/chronicle/queue/internal/AnalyticsHolder.java
+++ src/main/java/net/openhft/chronicle/queue/internal/AnalyticsHolder.java
@@ -5,10 +5,17 @@ import net.openhft.chronicle.core.pom.PomProperties;
 
 public enum AnalyticsHolder {;
 
+    static {
+        System.setProperty("chronicle.analytics.enable","true");
+    }
+
+
     private static final String VERSION = PomProperties.version("net.openhft", "chronicle-queue");
 
     private static final AnalyticsFacade ANALYTICS = AnalyticsFacade.standardBuilder("G-4K5MBLGPLE", "k1hK3x2qQaKk4F5gL-PBhQ", VERSION)
             //.withReportDespiteJUnit()
+            .withDebugLogger(System.out::println)
+            //.withUrl("https://www.google-analytics.com/debug/mp/collect")
             .build();
 
     public static AnalyticsFacade instance() {
--- src/main/java/net/openhft/chronicle/queue/internal/AnalyticsMain.java
+++ src/main/java/net/openhft/chronicle/queue/internal/AnalyticsMain.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.queue.internal;
 
+import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.queue.RollCycles;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
@@ -12,6 +13,7 @@ public final class AnalyticsMain {
     public static final String QUEUE_NAME2 = BASE_QUEUE_NAME + "2";
 
     public static void main(String[] args) {
+
         System.out.println("Creating two queues and sending analytics...");
         System.out.println("Currently, there is a limit of four message per h per JVM instance so only some analytics message might be sent upstream.");
         try (SingleChronicleQueue q1 = SingleChronicleQueueBuilder.binary(BASE_QUEUE_NAME + "1").build()) {
@@ -21,6 +23,7 @@ public final class AnalyticsMain {
         } finally {
             IOTools.deleteDirWithFiles(QUEUE_NAME1, QUEUE_NAME2);
         }
+        Jvm.pause(100_000);
         System.out.println("Completed successfully");
         System.exit(0);
     }
