8688d25fe29f51f65c895dc2c88d85e82d8a8e5b
Per Minborg <minborg@speedment.com>
2020-12-03 15:05:31 +0100

    Mark impl as an internal package

---
+++ src/main/java/net/openhft/chronicle/queue/impl/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package and any and all sub-packages contains strictly internal classes for this Chronicle library.
+ * Internal classes shall <em>never</em> be used directly.
+ * <p>
+ *  Specifically, the following actions (including, but not limited to) are not allowed
+ *  on internal classes and packages:
+ *  <ul>
+ *      <li>Casting to</li>
+ *      <li>Reflection of any kind</li>
+ *      <li>Explicit Serialize/deserialize</li>
+ *  </ul>
+ * <p>
+ * The classes in this package and any sub-package are subject to
+ * changes at any time for any reason.
+ */
+package net.openhft.chronicle.queue.impl;
