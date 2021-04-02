845e5ef11fc4fd1b60bc72d118d00ba46ade7329
Jerry Shea <jerry.shea@chronicle.software>
2021-04-02 12:46:13 +1100

    make TestBinarySearch more comprehensive

--- src/test/java/net/openhft/chronicle/queue/impl/single/TestBinarySearch.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/TestBinarySearch.java
@@ -1,7 +1,6 @@
 package net.openhft.chronicle.queue.impl.single;
 
 import net.openhft.chronicle.bytes.Bytes;
-import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.core.time.SetTimeProvider;
 import net.openhft.chronicle.queue.*;
 import net.openhft.chronicle.wire.DocumentContext;
@@ -11,72 +10,72 @@ import net.openhft.chronicle.wire.WireType;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import java.io.File;
 import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 
+@RunWith(Parameterized.class)
 public class TestBinarySearch extends ChronicleQueueTestBase {
+
+    private final int numberOfMessages;
+
+    public TestBinarySearch(int numberOfMessages) {
+        this.numberOfMessages = numberOfMessages;
+    }
+
+    @Parameterized.Parameters(name = "items in queue: {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {0},
+                {1},
+                {2},
+                {100}
+        });
+    }
 
     @Test
     public void testBinarySearch() throws ParseException {
-        test(50);
-    }
-
-    @Test
-    public void testBinarySearchOne() throws ParseException {
-        test(1);
-    }
-
-    private void test(int numberOfMessages) throws ParseException {
         final SetTimeProvider stp = new SetTimeProvider();
         long time = 0;
         stp.currentTimeMillis(time);
 
-        final File tmpDir = getTmpDir();
         try (SingleChronicleQueue queue = ChronicleQueue.singleBuilder(getTmpDir())
                 .rollCycle(RollCycles.TEST_SECONDLY)
                 .timeProvider(stp)
                 .build()) {
 
             final ExcerptAppender appender = queue.acquireAppender();
-
             for (int i = 0; i < numberOfMessages; i++) {
-
                 try (final DocumentContext dc = appender.writingDocument()) {
-
                     final MyData myData = new MyData();
                     myData.key = i;
-                    myData.value = "some value where the key=" + String.valueOf(i);
+                    myData.value = "some value where the key=" + i;
                     dc.wire().getValueOut().typedMarshallable(myData);
                     time += 300;
                     stp.currentTimeMillis(time);
                 }
             }
-                // System.out.println(queue.dump());
 
             final Comparator<Wire> comparator = (o1, o2) -> {
-
                 final long readPositionO1 = o1.bytes().readPosition();
                 final long readPositionO2 = o2.bytes().readPosition();
                 try {
-                    MyData myDataO1;
-                    MyData myDataO2;
-
+                    final MyData myDataO1;
                     try (final DocumentContext dc = o1.readingDocument()) {
                         myDataO1 = dc.wire().getValueIn().typedMarshallable();
-                        assert myDataO1.value != null;
                     }
 
+                    final MyData myDataO2;
                     try (final DocumentContext dc = o2.readingDocument()) {
                         myDataO2 = dc.wire().getValueIn().typedMarshallable();
-                        assert myDataO2.value != null;
                     }
 
                     final int compare = Integer.compare(myDataO1.key, myDataO2.key);
-
                     return compare;
-
                 } finally {
                     o1.bytes().readPosition(readPositionO1);
                     o2.bytes().readPosition(readPositionO2);
@@ -85,25 +84,17 @@ public class TestBinarySearch extends ChronicleQueueTestBase {
 
             try (final ExcerptTailer tailer = queue.createTailer()) {
                 for (int j = 0; j < numberOfMessages; j++) {
-
-                    Wire key = toWire(j);
-
-                    long index = BinarySearch.search(queue, key, comparator);
-                      // assert index != -1 : "i=" + j;
-
-                    tailer.moveToIndex(index);
-                    try (final DocumentContext documentContext = tailer.readingDocument()) {
-                        Assert.assertTrue(documentContext.toString().contains("some value where the key=" + j));
+                    try (DocumentContext ignored = tailer.readingDocument()) {
+                        Wire key = toWire(j);
+                        long index = BinarySearch.search(queue, key, comparator);
+                        Assert.assertEquals(tailer.index(), index);
+                        key.bytes().releaseLast();
                     }
-                    key.bytes().releaseLast();
                 }
             }
 
             Wire key = toWire(numberOfMessages);
             Assert.assertTrue("Should not find non-existent", BinarySearch.search(queue, key, comparator) < 0);
-        } finally {
-            System.gc();
-            IOTools.deleteDirWithFiles(tmpDir);
         }
     }
 
