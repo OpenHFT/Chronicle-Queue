dc2a4a657a6c295f7d92c71cf678489d80a905f9
Jerry Shea <jerry.shea@chronicle.software>
2021-04-11 12:41:32 +1000

    move StoreAppenderTest to InternalAppenderWriteBytesTest

--- src/test/java/net/openhft/chronicle/queue/InternalAppenderWriteBytesTest.java
+++ src/test/java/net/openhft/chronicle/queue/InternalAppenderWriteBytesTest.java
@@ -9,6 +9,10 @@ import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static net.openhft.chronicle.queue.DirectoryUtils.tempDir;
+import static net.openhft.chronicle.queue.RollCycles.MINUTELY;
+import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder.binary;
+
 public class InternalAppenderWriteBytesTest {
 
     @Test
@@ -99,6 +103,27 @@ public class InternalAppenderWriteBytesTest {
         } finally {
             result.releaseLast();
             test.releaseLast();
+        }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testJumpingAMessageThrowsAIllegalStateException() {
+
+        try (SingleChronicleQueue q = binary(tempDir("q"))
+                .rollCycle(MINUTELY)
+                .timeProvider(() -> 0).build();
+
+             ExcerptAppender appender = q.acquireAppender()) {
+            appender.writeText("hello");
+            appender.writeText("hello");
+            try (final DocumentContext dc = appender.writingDocument()) {
+                dc.wire().bytes().writeLong(1);
+            }
+
+            final long l = appender.lastIndexAppended();
+            final RollCycle rollCycle = q.rollCycle();
+            final long index = rollCycle.toIndex(rollCycle.toCycle(l) + 1, 1);
+            ((InternalAppender) appender).writeBytes(index, Bytes.from("text"));
         }
     }
 }
