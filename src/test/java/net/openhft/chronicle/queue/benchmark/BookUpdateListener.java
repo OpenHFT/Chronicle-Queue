078f6b2bde9ce6e1c0663427a5fcb23a2350058f
Rob Austin <rob.austin@boundedbuffer.co.uk>
2021-05-21 14:25:16 +0100

    removed JLBH benchmark

--- src/test/java/net/openhft/chronicle/queue/benchmark/BookUpdateListener.java
+++
@@ -1,9 +0,0 @@
-package net.openhft.chronicle.queue.benchmark;
-
-import net.openhft.chronicle.bytes.MethodId;
-
-
-public interface BookUpdateListener {
-    @MethodId(1)
-    void bookUpdate(BookUpdate bookUpdate);
-}
