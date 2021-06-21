d11afbd1bf6f10079f264fb71cb015c56715cfdb
Jerry Shea <jerry.shea@chronicle.software>
2021-06-21 17:04:42 +1000

    get rid of sonar warning

--- src/main/java/net/openhft/chronicle/queue/BenchmarkMain.java
+++ src/main/java/net/openhft/chronicle/queue/BenchmarkMain.java
@@ -146,10 +146,7 @@ public class BenchmarkMain {
                 return;
             }
         }
-        if (counter > 0)
-            Jvm.safepoint();
-        else
-            Jvm.safepoint();
+        Jvm.safepoint();
         counter = 0;
         try (DocumentContext dc = tailer.readingDocument(false)) {
             Jvm.safepoint();
