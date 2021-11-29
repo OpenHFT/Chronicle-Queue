ffc8714f109d2d0239e938e168e327e73d979ca5
Peter Lawrwy <peter.lawrey@chronicle.software>
2021-11-29 14:02:17 +0000

    Add support for listening to messages as they are appended #954

---
+++ src/test/java/net/openhft/chronicle/queue/AppenderListenerTest.java
@@ -0,0 +1,44 @@
+package net.openhft.chronicle.queue;
+
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.core.io.IOTools;
+import net.openhft.chronicle.core.time.SetTimeProvider;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppenderListenerTest {
+
+    @Test
+    public void appenderListenerTest() {
+        String path = OS.getTarget() + "/appenderListenerTest";
+        StringBuilder results = new StringBuilder();
+        try (ChronicleQueue q = SingleChronicleQueueBuilder.single(path)
+                .testBlockSize()
+                .appenderListener((wire, index) -> {
+                    long offset = ((index >>> 32) << 40) | wire.bytes().readPosition();
+                    String event = wire.readEvent(String.class);
+                    String text = wire.getValueIn().text();
+                    results.append(event)
+                            .append(" ").append(text)
+                            .append(", addr:").append(Long.toHexString(offset))
+                            .append(", index: ").append(Long.toHexString(index)).append("\n");
+                })
+                .timeProvider(new SetTimeProvider("2021/11/29T13:53:59").advanceMillis(1000))
+                .build();
+             ExcerptAppender appender = q.acquireAppender()) {
+            final HelloWorld writer = appender.methodWriter(HelloWorld.class);
+            writer.hello("G'Day");
+            writer.hello("Bye-now");
+        }
+        IOTools.deleteDirWithFiles(path);
+        assertEquals("" +
+                "hello G'Day, addr:4a10000001010c, index: 4a1000000000\n" +
+                "hello Bye-now, addr:4a100000010120, index: 4a1000000001\n", results.toString());
+    }
+
+    public interface HelloWorld {
+        void hello(String s);
+    }
+}
