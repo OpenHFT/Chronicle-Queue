a86c1052a47cf30ee13a6f612399a54cd45aad8c
Nick Tindall <nick@tindall.id.au>
2021-12-08 18:02:34 +1100

    Make QueueMultiThreadedJLBHBenchmark more configurable, consolidate QueueMultiThreadedJLBHBenchmark2 to use it.

--- src/test/java/net/openhft/chronicle/queue/bench/QueueMultiThreadedJLBHBenchmark.java
+++ src/test/java/net/openhft/chronicle/queue/bench/QueueMultiThreadedJLBHBenchmark.java
@@ -30,45 +30,67 @@ import net.openhft.chronicle.jlbh.JLBHTask;
 import net.openhft.chronicle.jlbh.TeamCityHelper;
 import net.openhft.chronicle.queue.ExcerptAppender;
 import net.openhft.chronicle.queue.ExcerptTailer;
+import net.openhft.chronicle.queue.RollCycle;
+import net.openhft.chronicle.queue.RollCycles;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.threads.NamedThreadFactory;
 import net.openhft.chronicle.wire.DocumentContext;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.file.Paths;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static net.openhft.chronicle.queue.bench.BenchmarkUtils.join;
 import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder.single;
 
 public class QueueMultiThreadedJLBHBenchmark implements JLBHTask {
-    private static final int ITERATIONS = 1_000_000;
+    private final int iterations;
+    private final String path;
+    private final int messageSize;
+    private final Long blockSize;
+    private final String tailerAffinity;
+    private final RollCycle rollCycle;
+    private final boolean usePretoucher;
+    private final boolean useSingleQueueInstance;
     private SingleChronicleQueue sourceQueue;
     private SingleChronicleQueue sinkQueue;
     private ExcerptTailer tailer;
     private ExcerptAppender appender;
-    private Datum datum = new Datum();
+    private final Datum datum;
     private boolean stopped = false;
     private Thread tailerThread;
     private JLBH jlbh;
     private NanoSampler writeProbe;
+    private ScheduledExecutorService pretoucherExecutorService;
 
     public static void main(String[] args) {
-        JLBHOptions lth = new JLBHOptions()
-                .warmUpIterations(50000)
-                .iterations(ITERATIONS)
-                .throughput(100_000)
-                // disable as otherwise single GC event skews results heavily
-                .recordOSJitter(false)
-                .accountForCoordinatedOmission(false)
-                .skipFirstRun(true)
-                .runs(5)
-                .jlbhTask(new QueueMultiThreadedJLBHBenchmark());
-        new JLBH(lth).start();
+        new Builder().run();
+    }
+
+    public QueueMultiThreadedJLBHBenchmark(int iterations, String path, String tailerAffinity, @Nullable RollCycle rollCycle,
+                                           int messageSize, @Nullable Long blockSize, boolean usePretoucher,
+                                           boolean useSingleQueueInstance) {
+        this.iterations = iterations;
+        this.path = path;
+        this.tailerAffinity = tailerAffinity;
+        this.rollCycle = rollCycle;
+        this.messageSize = messageSize;
+        this.blockSize = blockSize;
+        this.usePretoucher = usePretoucher;
+        this.useSingleQueueInstance = useSingleQueueInstance;
+        this.datum = new Datum(messageSize);
     }
 
     @Override
     public void init(JLBH jlbh) {
         this.jlbh = jlbh;
-        IOTools.deleteDirWithFiles("replica", 10);
+        IOTools.deleteDirWithFiles(path, 10);
 
-        sourceQueue = single("replica").build();
-        sinkQueue = single("replica").build();
+        sourceQueue = createQueueInstance();
+        sinkQueue = useSingleQueueInstance ? sourceQueue : createQueueInstance();
         appender = sourceQueue.acquireAppender()
                 .disableThreadSafetyCheck(true);
         tailer = sinkQueue.createTailer()
@@ -76,9 +98,16 @@ public class QueueMultiThreadedJLBHBenchmark implements JLBHTask {
 
         NanoSampler readProbe = jlbh.addProbe("read");
         writeProbe = jlbh.addProbe("write");
+
+        if (usePretoucher) {
+            pretoucherExecutorService = Executors.newSingleThreadScheduledExecutor(
+                    new NamedThreadFactory("pretoucher", true));
+            pretoucherExecutorService.scheduleAtFixedRate(() -> sourceQueue.acquireAppender().pretouch(), 1, 200, TimeUnit.MILLISECONDS);
+        }
+
         tailerThread = new Thread(() -> {
-            try (final AffinityLock affinityLock = AffinityLock.acquireCore()) {
-                Datum datum2 = new Datum();
+            try (final AffinityLock affinityLock = AffinityLock.acquireLock(tailerAffinity)) {
+                Datum datum2 = new Datum(messageSize);
                 while (!stopped) {
                     long beforeReadNs = System.nanoTime();
                     try (DocumentContext dc = tailer.readingDocument()) {
@@ -95,6 +124,17 @@ public class QueueMultiThreadedJLBHBenchmark implements JLBHTask {
         tailerThread.start();
     }
 
+    private SingleChronicleQueue createQueueInstance() {
+        final SingleChronicleQueueBuilder builder = single(path);
+        if (blockSize != null) {
+            builder.blockSize(blockSize);
+        }
+        if (rollCycle != null) {
+            builder.rollCycle(rollCycle);
+        }
+        return builder.build();
+    }
+
     @Override
     public void run(long startTimeNS) {
         datum.ts = startTimeNS;
@@ -106,16 +146,23 @@ public class QueueMultiThreadedJLBHBenchmark implements JLBHTask {
 
     @Override
     public void complete() {
+        if (pretoucherExecutorService != null) {
+            pretoucherExecutorService.shutdownNow();
+        }
         stopped = true;
         join(tailerThread);
         sinkQueue.close();
         sourceQueue.close();
-        TeamCityHelper.teamCityStatsLastRun(getClass().getSimpleName(), jlbh, ITERATIONS, System.out);
+        TeamCityHelper.teamCityStatsLastRun(getClass().getSimpleName(), jlbh, iterations, System.out);
     }
 
     private static class Datum implements BytesMarshallable {
         public long ts = 0;
-        public byte[] filler = new byte[4088];
+        public final byte[] filler;
+
+        public Datum(int messageSize) {
+            this.filler = new byte[messageSize - 8];
+        }
 
         @Override
         public void readMarshallable(BytesIn bytes) throws IORuntimeException {
@@ -127,6 +174,106 @@ public class QueueMultiThreadedJLBHBenchmark implements JLBHTask {
         public void writeMarshallable(BytesOut bytes) {
             bytes.writeLong(ts);
             bytes.writeSkip(filler.length);
+        }
+    }
+
+    public static class Builder {
+        private int runs = Integer.getInteger("runs", 5);
+        private String path = Paths.get(System.getProperty("path", ".")).resolve("replica").normalize().toString();
+        private Integer messageSize = Integer.getInteger("messageSize", 4_096);
+        private Long blockSize = Long.getLong("blockSize"); // use default when not specified
+        private String producerAffinity = System.getProperty("producerAffinity", "last-1");
+        private String consumerAffinity = System.getProperty("consumerAffinity", "last-2");
+        private int warmupIterations = Integer.getInteger("warmupIterations", 50_000);
+        private int iterations = Integer.getInteger("iterations", 1_000_000);
+        private int throughput = Integer.getInteger("throughput", 100_000);
+        private boolean usePretoucher = Boolean.getBoolean("usePretoucher");
+        private boolean useSingleQueueInstance = Boolean.getBoolean("useSingleQueue");
+        private RollCycle rollCycle = getRollCycle();
+
+        public void run() {
+            final QueueMultiThreadedJLBHBenchmark jlbhTask = new QueueMultiThreadedJLBHBenchmark(iterations, path, consumerAffinity,
+                    rollCycle, messageSize, blockSize, usePretoucher, useSingleQueueInstance);
+            JLBHOptions lth = new JLBHOptions()
+                    .warmUpIterations(warmupIterations)
+                    .iterations(iterations)
+                    .throughput(throughput)
+                    // disable as otherwise single GC event skews results heavily
+                    .recordOSJitter(false)
+                    .accountForCoordinatedOmission(false)
+                    .skipFirstRun(true)
+                    .acquireLock(() -> AffinityLock.acquireLock(producerAffinity))
+                    .runs(runs)
+                    .jlbhTask(jlbhTask);
+            new JLBH(lth).start();
+        }
+
+        private RollCycle getRollCycle() {
+            final String rollCycle = System.getProperty("rollCycle");
+            if (rollCycle != null) {
+                return RollCycles.valueOf(rollCycle);
+            }
+            return null;
+        }
+
+        public Builder runs(int runs) {
+            this.runs = runs;
+            return this;
+        }
+
+        public Builder path(String path) {
+            this.path = path;
+            return this;
+        }
+
+        public Builder messageSize(Integer messageSize) {
+            this.messageSize = messageSize;
+            return this;
+        }
+
+        public Builder blockSize(Long blockSize) {
+            this.blockSize = blockSize;
+            return this;
+        }
+
+        public Builder producerAffinity(String producerAffinity) {
+            this.producerAffinity = producerAffinity;
+            return this;
+        }
+
+        public Builder consumerAffinity(String consumerAffinity) {
+            this.consumerAffinity = consumerAffinity;
+            return this;
+        }
+
+        public Builder warmupIterations(int warmupIterations) {
+            this.warmupIterations = warmupIterations;
+            return this;
+        }
+
+        public Builder iterations(int iterations) {
+            this.iterations = iterations;
+            return this;
+        }
+
+        public Builder throughput(int throughput) {
+            this.throughput = throughput;
+            return this;
+        }
+
+        public Builder usePretoucher(boolean usePretoucher) {
+            this.usePretoucher = usePretoucher;
+            return this;
+        }
+
+        public Builder useSingleQueueInstance(boolean useSingleQueueInstance) {
+            this.useSingleQueueInstance = useSingleQueueInstance;
+            return this;
+        }
+
+        public Builder rollCycle(RollCycle rollCycle) {
+            this.rollCycle = rollCycle;
+            return this;
         }
     }
 }
