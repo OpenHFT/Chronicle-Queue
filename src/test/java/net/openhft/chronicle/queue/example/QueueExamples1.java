4da2dbb54c3fd89634c9e642eb159c6df44522ba
Rob Austin <rob.austin@boundedbuffer.co.uk>
2021-12-21 16:43:17 +0000

    update examples

--- src/test/java/net/openhft/chronicle/queue/example/QueueExamples1.java
+++ src/test/java/net/openhft/chronicle/queue/example/QueueExamples1.java
@@ -1,14 +1,13 @@
 package net.openhft.chronicle.queue.example;
 
 import net.openhft.chronicle.queue.ChronicleQueue;
-import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 
 public class QueueExamples1 {
 
     public static void main(String[] args) {
 
-        ChronicleQueue chronicleQueue = SingleChronicleQueueBuilder.binary("./myQueueDir").build();
-        final Printer printer = chronicleQueue.methodWriter(Printer.class);
+        ChronicleQueue queue = ChronicleQueue.single("./myQueueDir");
+        Printer printer = queue.methodWriter(Printer.class);
         printer.print("hello world");
     }
 
--- src/test/java/net/openhft/chronicle/queue/example/QueueExamples2.java
+++ src/test/java/net/openhft/chronicle/queue/example/QueueExamples2.java
@@ -2,14 +2,13 @@ package net.openhft.chronicle.queue.example;
 
 import net.openhft.chronicle.bytes.MethodReader;
 import net.openhft.chronicle.queue.ChronicleQueue;
-import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 
 public class QueueExamples2 {
 
     public static void main(String[] args) {
 
-        ChronicleQueue chronicleQueue = SingleChronicleQueueBuilder.binary("./myQueueDir").build();
-        final MethodReader methodReader = chronicleQueue.createTailer().methodReader((Printer) System.out::println);
+        ChronicleQueue queue = ChronicleQueue.single("./myQueueDir");
+        final MethodReader methodReader = queue.createTailer().methodReader((Printer) System.out::println);
 
         for (; ; ) {
             final boolean successIfMessageRead = methodReader.readOne();
