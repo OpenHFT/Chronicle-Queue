72381b62a93431be5c47ebb1e029acb3be60a94f
Peter Lawrey <peter.lawrey@chronicle.software>
2022-03-28 16:42:27 +0100

    Resolve merge issues re StoreFIleListeners

--- src/main/java/net/openhft/chronicle/queue/impl/StoreFileListeners.java
+++ src/main/java/net/openhft/chronicle/queue/impl/StoreFileListeners.java
@@ -9,12 +9,22 @@ public enum StoreFileListeners implements StoreFileListener {
         @Override
         public void onReleased(int cycle, File file) {
         }
+
+        @Override
+        public boolean isActive() {
+            return false;
+        }
     },
     DEBUG {
         @Override
         public void onReleased(int cycle, File file) {
             if (Jvm.isDebugEnabled(getClass()))
                 Jvm.debug().on(getClass(), "File released " + file);
+        }
+
+        @Override
+        public boolean isActive() {
+            return Jvm.isDebugEnabled(getClass());
         }
     }
 }
