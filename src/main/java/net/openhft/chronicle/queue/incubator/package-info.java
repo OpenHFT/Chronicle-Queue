809988e4a70d4de67de79d535e8ce6954fd6e50c
Per Minborg <minborg@speedment.com>
2022-04-11 16:04:19 +0200

    Add incubator

---
+++ src/main/java/net/openhft/chronicle/queue/incubator/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This package and any and all sub-packages contains <em>incubating functionality</em> for this Chronicle library and
+ * are not yet a part of the official API.
+ * <p>
+ * Incubator packages are a means of putting non-final APIs and non-final tools in the hands of developers, while
+ * the APIs/tools progress towards either finalization or removal in a future release.
+ * <p>
+ * The classes in this package and any sub-package are subject to change at any time for any reason and shall
+ * <em>not be used in production code</em>.
+ */
+package net.openhft.chronicle.queue.incubator;
