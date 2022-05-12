690210b0fc5bbf7394630650c1259c2fdf56e71c
rogersimmons <roger.simmons@higherfrequencytrading.com>
2022-05-12 12:25:24 +0100

    Stabilise metrics: add reader/writer affinity, add separate read thread, applied some host tuning

--- src/test/java/net/openhft/chronicle/queue/bench/QueueSparseFilesJLBHBenchmark.java
+++ src/test/java/net/openhft/chronicle/queue/bench/QueueSparseFilesJLBHBenchmark.java
@@ -17,6 +17,7 @@
  */
 package net.openhft.chronicle.queue.bench;
 
+import net.openhft.affinity.AffinityLock;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.core.util.NanoSampler;
@@ -28,10 +29,12 @@ import net.openhft.chronicle.queue.ExcerptTailer;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 import net.openhft.chronicle.wire.DocumentContext;
 
+import static net.openhft.chronicle.queue.bench.BenchmarkUtils.join;
 import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder.single;
 
 /*
 Chronicle Queue performance is compared when using sparse files and chunking( with standard and large blockSize).
+mvn exec:java -Dexec.mainClass=net.openhft.chronicle.queue.bench.QueueSparseFilesJLBHBenchmark -Dexec.classpathScope=test
  */
 public class QueueSparseFilesJLBHBenchmark implements JLBHTask {
     private static final int throughput = Integer.getInteger("throughput", 100_000);
@@ -45,7 +48,22 @@ public class QueueSparseFilesJLBHBenchmark implements JLBHTask {
     private JLBH jlbh;
     private NanoSampler writeProbe;
     private NanoSampler readProbe;
-    private int count = 0;
+    private boolean stopped = false;
+    private Thread tailerThread;
+    private static final long CHUNKSIZE = 4L << 10;
+    private static final int WARMUP = 200_000;
+    private static final int WRITE_CPU = 8;
+    private static final int READ_CPU = 9;
+    private static final int RUNS = 5;
+    private static final long SPARSEBLOCK = 1L << 40;
+    private static final long NORMALBLOCK = 64L << 20;
+    private static final String ROOTDIR="/mnt/local/benchmark/";
+
+    static {
+        System.setProperty("disable.thread.safety", "true");
+        System.setProperty("jvm.resource.tracing", "false");
+        System.setProperty("check.thread.safety", "false");
+    }
 
     public QueueSparseFilesJLBHBenchmark(int round) {
         this.round = round;
@@ -54,16 +72,16 @@ public class QueueSparseFilesJLBHBenchmark implements JLBHTask {
     public static void main(String... args) {
         System.out.println("-Dthroughput=" + throughput
                 + " -DrunTime=" + runTime);
-        int warmUp = 200_000;
         for (int round = 1; round <= 3; round++) {
             System.out.println("========\nround= " + round + "\n========\n");
             JLBHOptions lth = new JLBHOptions()
-                    .warmUpIterations(warmUp)
+                    .warmUpIterations(WARMUP)
                     .iterations(iterations)
                     .throughput(throughput)
                     .recordOSJitter(false)
                     .skipFirstRun(true)
-                    .runs(5)
+                    .acquireLock(()->AffinityLock.acquireLock(WRITE_CPU))
+                    .runs(RUNS)
                     .jlbhTask(new QueueSparseFilesJLBHBenchmark(round));
             new JLBH(lth).start();
         }
@@ -71,54 +89,67 @@ public class QueueSparseFilesJLBHBenchmark implements JLBHTask {
 
     @Override
     public void init(JLBH jlbh) {
-        IOTools.deleteDirWithFiles("benchmark", 5);
+        IOTools.deleteDirWithFiles(ROOTDIR, 5);
 
         if (round == 1) {
             // Uses sparse file for memory mapping
-            sourceQueue = single("benchmark/sparseFile").useSparseFiles(true).sparseCapacity(1L << 40).build();
-            sinkQueue = single("benchmark/sparseFile").useSparseFiles(true).sparseCapacity(1L << 40).build();
+            sourceQueue = single(ROOTDIR + "sparseFile").useSparseFiles(true).sparseCapacity(SPARSEBLOCK).build();
+            sinkQueue = single(ROOTDIR + "sparseFile").useSparseFiles(true).sparseCapacity(SPARSEBLOCK).build();
         } else if (round == 2) {
             // UsesConfigures chunking for memory mapping using a large memory block
-            sourceQueue = single("benchmark/chunking-largeBlockSize").blockSize(1L << 40).build();
-            sinkQueue = single("benchmark/chunking-largeBlockSize").blockSize(1L << 40).build();
+            sourceQueue = single(ROOTDIR + "chunking-largeBlockSize").blockSize(SPARSEBLOCK).build();
+            sinkQueue = single(ROOTDIR + "chunking-largeBlockSize").blockSize(SPARSEBLOCK).build();
         } else {
             // Configures chunking for memory mapping using a small memory block
-            sourceQueue = single("benchmark/chunking-standardBlockSize").blockSize(64L << 20).build();
-            sinkQueue = single("benchmark/chunking-standardBlockSize").blockSize(64L << 20).build();
+            sourceQueue = single(ROOTDIR + "chunking-standardBlockSize").blockSize(NORMALBLOCK).build();
+            sinkQueue = single(ROOTDIR + "chunking-standardBlockSize").blockSize(NORMALBLOCK).build();
         }
         appender = sourceQueue.acquireAppender().disableThreadSafetyCheck(true);
         tailer = sinkQueue.createTailer().disableThreadSafetyCheck(true);
         this.jlbh = jlbh;
         writeProbe = round == 1 ? jlbh.addProbe("write (sparse file)") : round == 2 ? jlbh.addProbe("write (chunking-largeBlockSize)") : jlbh.addProbe("write (chunking-standardBlockSize)");
         readProbe = round == 1 ? jlbh.addProbe("read (sparse file)") : round == 2 ? jlbh.addProbe("read (chunking-largeBlockSize)") : jlbh.addProbe("read (chunking-standardBlockSize)");
+
+        tailerThread = new Thread( () -> {
+            try(final AffinityLock affinityLock = AffinityLock.acquireLock(READ_CPU)) {
+                while(!stopped) {
+                    long beforeReadNS = System.nanoTime();
+                    try(DocumentContext dc = tailer.readingDocument()) {
+                        if(!dc.isPresent())
+                            continue;
+                        Bytes bytes = dc.wire().bytes();
+                        bytes.readSkip(CHUNKSIZE);
+                        long writeNS = bytes.readLong();
+
+                        long now = System.nanoTime();
+                        jlbh.sample(now - writeNS);
+                        readProbe.sampleNanos(now - beforeReadNS);
+                    }
+                }
+
+            }
+        });
+        tailerThread.start();
     }
 
     @Override
     public void run(long startTimeNS) {
         try (DocumentContext dc = appender.writingDocument()) {
             Bytes bytes = dc.wire().bytes();
-            bytes.writeSkip(4L << 10);
-            bytes.writeInt(count);
-            writeProbe.sampleNanos(System.nanoTime() - startTimeNS);
+            bytes.writeSkip(CHUNKSIZE);
+            bytes.writeLong(System.nanoTime());
         }
-
-        long beforeReadNS = System.nanoTime();
-        try (DocumentContext dc = tailer.readingDocument()) {
-            if (dc.wire() != null) {
-                Bytes bytes = dc.wire().bytes();
-                bytes.readSkip(4L << 10);
-                bytes.readInt();
-                long afterReadNS = System.nanoTime();
-                jlbh.sample(afterReadNS - startTimeNS);
-                readProbe.sampleNanos(afterReadNS - beforeReadNS);
-            }
-        }
-        count++;
+        writeProbe.sampleNanos(System.nanoTime() - startTimeNS);
     }
 
     @Override
     public void complete() {
+        stopped = true;
+        join(tailerThread);
+
         sinkQueue.close();
         sourceQueue.close();
+
+        IOTools.deleteDirWithFiles(ROOTDIR, 5);
     }
 }
