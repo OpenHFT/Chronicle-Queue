3bd171fd006446c565f09992fb1a9409825c90c0
Peter Lawrey <peter.lawrey@chronicle.software>
2022-06-17 17:37:31 +0100

    Added diagnostics for a flaky test

--- src/test/java/net/openhft/chronicle/queue/LastAppendedTest.java
+++ src/test/java/net/openhft/chronicle/queue/LastAppendedTest.java
@@ -4,6 +4,7 @@ import net.openhft.chronicle.bytes.MethodReader;
 import net.openhft.chronicle.core.annotation.RequiredForClient;
 import net.openhft.chronicle.core.time.SetTimeProvider;
 import net.openhft.chronicle.wire.DocumentContext;
+import net.openhft.chronicle.wire.MessageHistory;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -11,6 +12,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder.single;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RequiredForClient
 public class LastAppendedTest extends ChronicleQueueTestBase {
@@ -139,8 +141,19 @@ public class LastAppendedTest extends ChronicleQueueTestBase {
             {
                 ExcerptTailer excerptTailer = inQueue.createTailer().afterLastWritten(outQueue);
                 long index = excerptTailer.index();
+                try (DocumentContext dc = excerptTailer.readingDocument()) {
+                    assertTrue(dc.isPresent());
+                    MessageHistory mh = dc.wire().read("history").object(MessageHistory.get(), MessageHistory.class);
+                    assertTrue(mh.toString(), mh.toString().startsWith("VanillaMessageHistory{sources: [2=0x100000002] timings: "));
+                    final String msg0 = dc.wire().readEvent(String.class);
+                    assertEquals("msg", msg0);
+                    Object o = dc.wire().getValueIn().object();
+                    assertEquals("somedata-2", o);
+                }
+                assertTrue(excerptTailer.moveToIndex(index));
                 MethodReader methodReader = excerptTailer.methodReader((LATMsg) actualValue::set);
                 try {
+                    methodReader.readOne();
                     assertEquals("somedata-2", actualValue.get());
                     methodReader.readOne();
                     assertEquals("somedata-3", actualValue.get());
