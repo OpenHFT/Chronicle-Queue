c10e98debdbd88dd271f74ca036f2def9ce7c6dc
Rob Austin <rob.austin@chronicle.software>
2022-06-17 16:01:25 +0200

    Last appended test (#1112)
    * added pause
    
    * try closing the re-opening
    
    * ensure the uniqueId is unique between concurrent calls and different JVMs on the same machine.
    
    * change to use the flaky test runner for now
    
    * remove flaky test runner
    
    * reverted back to         <version>3.0.0-M4</version>
    
    * Updating parent POM (automated)
    
    * reverted
    
    Co-authored-by: Rob Austin <rob.austin@boundedbuffer.co.uk>
    Co-authored-by: hft-team-city <teamcity@chronicle.software>

--- src/test/java/net/openhft/chronicle/queue/LastAppendedTest.java
+++ src/test/java/net/openhft/chronicle/queue/LastAppendedTest.java
@@ -1,73 +1,87 @@
 package net.openhft.chronicle.queue;
 
 import net.openhft.chronicle.bytes.MethodReader;
+import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.annotation.RequiredForClient;
 import net.openhft.chronicle.core.time.SetTimeProvider;
+import net.openhft.chronicle.testframework.FlakyTestRunner;
 import net.openhft.chronicle.wire.DocumentContext;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder.single;
+import static net.openhft.chronicle.testframework.FlakyTestRunner.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RequiredForClient
 public class LastAppendedTest extends ChronicleQueueTestBase {
+   // @Test
+   // public void testLastWritten() {
+    //    builder(this::testLastWritten0).build().run();
+   // }
 
+
+   // public void testLastWrittenMetadata() {
+  //      builder(this::testLastWrittenMetadata0).build().run();
+  //  }
     @Test
-    public void testLastWritten() {
-        SetTimeProvider timeProvider = new SetTimeProvider(0).advanceMillis(1000);
+    void testLastWritten0() {
+        SetTimeProvider timeProvider = new SetTimeProvider();
 
-        try (ChronicleQueue outQueue = single(getTmpDir()).rollCycle(RollCycles.TEST4_SECONDLY).sourceId(1).timeProvider(timeProvider).build()) {
-            try (ChronicleQueue inQueue = single(getTmpDir()).rollCycle(RollCycles.TEST4_SECONDLY).sourceId(2).timeProvider(timeProvider).build()) {
+        final File outQueueDir = getTmpDir();
+        final File inQueueDir = getTmpDir();
 
-                // write some initial data to the inqueue
-                final LATMsg msg = inQueue.acquireAppender()
+        try (ChronicleQueue outQueue = single(outQueueDir).rollCycle(RollCycles.TEST_SECONDLY).sourceId(1).timeProvider(timeProvider).build();
+             ChronicleQueue inQueue = single(inQueueDir).rollCycle(RollCycles.TEST_SECONDLY).sourceId(2).timeProvider(timeProvider).build()) {
+
+            // write some initial data to the inqueue
+            final LATMsg msg = inQueue.acquireAppender()
+                    .methodWriterBuilder(LATMsg.class)
+                    .get();
+
+            msg.msg("somedata-0");
+
+            timeProvider.advanceMillis(1000);
+
+            // write data into the inQueue
+            msg.msg("somedata-1");
+
+            // read a message on the in queue and write it to the out queue
+            {
+                LATMsg out = outQueue.acquireAppender()
                         .methodWriterBuilder(LATMsg.class)
                         .get();
+                MethodReader methodReader = inQueue.createTailer()
+                        .methodReader((LATMsg) out::msg);
 
-                msg.msg("somedata-0");
+                // reads the somedata-0
+                methodReader.readOne();
 
-                timeProvider.advanceMillis(1000);
+                // reads the somedata-1
+                methodReader.readOne();
+            }
 
-                // write data into the inQueue
-                msg.msg("somedata-1");
+            // write data into the inQueue
+            msg.msg("somedata-2");
 
-                // read a message on the in queue and write it to the out queue
-                {
-                    LATMsg out = outQueue.acquireAppender()
-                            .methodWriterBuilder(LATMsg.class)
-                            .get();
-                    MethodReader methodReader = inQueue.createTailer()
-                            .methodReader((LATMsg) out::msg);
+            timeProvider.advanceMillis(2000);
 
-                    // reads the somedata-0
-                    methodReader.readOne();
+            msg.msg("somedata-3");
+            msg.msg("somedata-4");
 
-                    // reads the somedata-1
-                    methodReader.readOne();
-                }
 
-                // write data into the inQueue
-                msg.msg("somedata-2");
+            final AtomicReference<String> actualValue = new AtomicReference<>();
 
-                timeProvider.advanceMillis(2000);
-
-                msg.msg("somedata-3");
-                msg.msg("somedata-4");
-
-                // System.out.println(inQueue.dump());
-
-                AtomicReference<String> actualValue = new AtomicReference<>();
-
-                // check that we are able to pick up from where we left off, in other words the next read should be somedata-2
-                {
-                    ExcerptTailer excerptTailer = inQueue.createTailer().afterLastWritten(outQueue);
+            // check that we are able to pick up from where we left off, in other words the next read should be somedata-2
+            {
+                ExcerptTailer excerptTailer = inQueue.createTailer().afterLastWritten(outQueue);
                     long index = excerptTailer.index();
-                    MethodReader methodReader = excerptTailer.methodReader((LATMsg) actualValue::set);
+                MethodReader methodReader = excerptTailer.methodReader((LATMsg) actualValue::set);
 
-                    methodReader.readOne();
+                methodReader.readOne();
                     final String actual = actualValue.get();
                     try {
                         Assert.assertEquals("somedata-2", actual);
@@ -83,67 +97,71 @@ public class LastAppendedTest extends ChronicleQueueTestBase {
                         System.out.println(outQueue.dump());
                         throw ae;
                     }
-                }
             }
         }
     }
 
+
     @Test
-    void testLastWrittenMetadata() {
-        SetTimeProvider timeProvider = new SetTimeProvider();
+    void testLastWrittenMetadata0() {
+        SetTimeProvider timeProvider = new SetTimeProvider().advanceMillis(1000);
 
-        try (ChronicleQueue outQueue = single(getTmpDir()).rollCycle(RollCycles.TEST_SECONDLY).sourceId(1).timeProvider(timeProvider).build()) {
-            try (ChronicleQueue inQueue = single(getTmpDir()).rollCycle(RollCycles.TEST_SECONDLY).sourceId(2).timeProvider(timeProvider).build()) {
+        final File outQueueDir = getTmpDir();
+        final File inQueueDir = getTmpDir();
 
-                // write some initial data to the inqueue
-                final LATMsg msg = inQueue.acquireAppender()
+        try (ChronicleQueue outQueue = single(outQueueDir).rollCycle(RollCycles.TEST_SECONDLY).sourceId(1).timeProvider(timeProvider).build();
+             ChronicleQueue inQueue = single(inQueueDir).rollCycle(RollCycles.TEST_SECONDLY).sourceId(2).timeProvider(timeProvider).build()) {
+
+
+            // write some initial data to the inqueue
+            final LATMsg msg = inQueue.acquireAppender()
+                    .methodWriterBuilder(LATMsg.class)
+                    .get();
+
+            msg.msg("somedata-0");
+            msg.msg("somedata-1");
+
+            // read a message on the in queue and write it to the out queue
+            {
+                LATMsg out = outQueue.acquireAppender()
                         .methodWriterBuilder(LATMsg.class)
                         .get();
+                MethodReader methodReader = inQueue.createTailer().methodReader((LATMsg) out::msg);
 
-                msg.msg("somedata-0");
-                msg.msg("somedata-1");
+                // reads the somedata-0
+                methodReader.readOne();
 
-                // read a message on the in queue and write it to the out queue
-                {
-                    LATMsg out = outQueue.acquireAppender()
-                            .methodWriterBuilder(LATMsg.class)
-                            .get();
-                    MethodReader methodReader = inQueue.createTailer().methodReader((LATMsg) out::msg);
+                // reads the somedata-1
+                methodReader.readOne();
+            }
 
-                    // reads the somedata-0
-                    methodReader.readOne();
+            // write data into the inQueue
+            msg.msg("somedata-2");
+            msg.msg("somedata-3");
+            msg.msg("somedata-4");
 
-                    // reads the somedata-1
-                    methodReader.readOne();
-                }
+            try (DocumentContext dc = outQueue.acquireAppender().writingDocument(true)) {
+                dc.wire().write("some metadata");
+            }
 
-                // write data into the inQueue
-                msg.msg("somedata-2");
-                msg.msg("somedata-3");
-                msg.msg("somedata-4");
+            AtomicReference<String> actualValue = new AtomicReference<>();
 
-                try (DocumentContext dc = outQueue.acquireAppender().writingDocument(true)) {
-                    dc.wire().write("some metadata");
-                }
+            // check that we are able to pick up from where we left off, in other words the next read should be somedata-2
+            {
+                ExcerptTailer excerptTailer = inQueue.createTailer().afterLastWritten(outQueue);
 
-                AtomicReference<String> actualValue = new AtomicReference<>();
+                MethodReader methodReader = excerptTailer.methodReader((LATMsg) actualValue::set);
 
-                // check that we are able to pick up from where we left off, in other words the next read should be somedata-2
-                {
-                    ExcerptTailer excerptTailer = inQueue.createTailer().afterLastWritten(outQueue);
+                methodReader.readOne();
+                assertEquals("somedata-2", actualValue.get());
 
-                    MethodReader methodReader = excerptTailer.methodReader((LATMsg) actualValue::set);
+                methodReader.readOne();
+                assertEquals("somedata-3", actualValue.get());
 
-                    methodReader.readOne();
-                    assertEquals("somedata-2", actualValue.get());
-
-                    methodReader.readOne();
-                    assertEquals("somedata-3", actualValue.get());
-
-                    methodReader.readOne();
-                    assertEquals("somedata-4", actualValue.get());
-                }
+                methodReader.readOne();
+                assertEquals("somedata-4", actualValue.get());
             }
         }
     }
 }
+
