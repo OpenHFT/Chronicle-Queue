6a61eaa71b65a67ecebbcbd7b718247d8010e1c1
Peter Lawrey <peter.lawrey@chronicle.software>
2022-07-20 11:40:51 +0100

    Add logging of parameters

--- src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
+++ src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
@@ -13,27 +13,28 @@ import net.openhft.chronicle.queue.RollCycles;
 import net.openhft.chronicle.wire.DocumentContext;
 
 /*
-ARMv7 Processor rev 10 (v7l), Quad 1 GHz.
-Writing 36,699,566 messages took 20.297 seconds, at a rate of 1,808,092 per second
-Reading 36,699,566 messages took 28.283 seconds, at a rate of 1,297,576 per second
+Ryzen 9 5950X with Corsair MP600 PRO XT
+-Dtime=30 -Dsize=1000000 -Dpath=/data/tmp - DblockSizeMB=524288
+Writing 44,749 messages took 30.001 seconds, at a rate of 1,491 per second
+Reading 44,749 messages took 14.461 seconds, at a rate of 3,094 per second
  */
 public class ThroughputPerfMain {
-    private static final int time = Integer.getInteger("time", 20);
-    private static final int size = Integer.getInteger("size", 40);
-    private static final String path = System.getProperty("path", OS.TMP);
-    private static final long blockSize = Long.getLong("blockSize", OS.is64Bit() ? 4L << 30 : 256L << 20);
+    private static final int TIME = Integer.getInteger("time", 30);
+    private static final int SIZE = Integer.getInteger("size", 40);
+    private static final String PATH = System.getProperty("path", OS.TMP);
+    private static final long blockSizeMB = Long.getLong("blockSizeMB", OS.isSparseFileSupported() ? 512L << 10 : 256L);
 
     private static BytesStore nbs;
 
     public static void main(String[] args) {
-        String base = path + "/delete-" + Time.uniqueId() + ".me";
+        String base = PATH + "/delete-" + Time.uniqueId() + ".me";
         long start = System.nanoTime();
         long count = 0;
-        nbs = BytesStore.nativeStoreWithFixedCapacity(size);
+        nbs = BytesStore.nativeStoreWithFixedCapacity(SIZE);
 
         try (ChronicleQueue q = ChronicleQueue.singleBuilder(base)
                 .rollCycle(RollCycles.LARGE_HOURLY_XSPARSE)
-                .blockSize(blockSize)
+                .blockSize(blockSizeMB << 20)
                 .build()) {
 
             ExcerptAppender appender = q.acquireAppender();
@@ -42,7 +43,7 @@ public class ThroughputPerfMain {
                     dc.wire().bytes().write(nbs);
                 }
                 count++;
-            } while (start + time * 1e9 > System.nanoTime());
+            } while (start + TIME * 1e9 > System.nanoTime());
         }
 
         nbs.releaseLast();
@@ -52,7 +53,7 @@ public class ThroughputPerfMain {
         Bytes<?> bytes = Bytes.allocateElasticDirect(64);
         try (ChronicleQueue q = ChronicleQueue.singleBuilder(base)
                 .rollCycle(RollCycles.LARGE_HOURLY_XSPARSE)
-                .blockSize(blockSize)
+                .blockSize(blockSizeMB << 20)
                 .build()) {
             ExcerptTailer tailer = q.createTailer();
             for (long i = 0; i < count; i++) {
@@ -66,6 +67,10 @@ public class ThroughputPerfMain {
         long end = System.nanoTime();
         long time2 = end - mid;
 
+        System.out.println("-Dtime=" + TIME +
+                " -Dsize=" + SIZE +
+                " -Dpath=" + PATH +
+                " - DblockSizeMB=" + blockSizeMB);
         System.out.printf("Writing %,d messages took %.3f seconds, at a rate of %,d per second%n",
                 count, time1 / 1e9, (long) (1e9 * count / time1));
         System.out.printf("Reading %,d messages took %.3f seconds, at a rate of %,d per second%n",
