86efc2fab6247a16aecc689908917d5a334b3c88
Peter Lawrey <peter.lawrey@chronicle.software>
2022-07-20 12:24:38 +0100

    Add average latencies

--- src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
+++ src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.queue.bench;
 
+import net.openhft.affinity.AffinityLock;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesStore;
 import net.openhft.chronicle.core.OS;
@@ -14,29 +15,30 @@ import net.openhft.chronicle.wire.DocumentContext;
 
 /*
 Ryzen 9 5950X with Corsair MP600 PRO XT
+
 -Dtime=30 -Dsize=1000000 -Dpath=/data/tmp - DblockSizeMB=524288
-Writing 44,749 messages took 30.001 seconds, at a rate of 1,491 per second
-Reading 44,749 messages took 14.461 seconds, at a rate of 3,094 per second
+Writing 54,626 messages took 30.006 seconds, at a rate of 1,820 per second, with an average latency of 549,290 ns
+Reading 54,626 messages took 16.970 seconds, at a rate of 3,219 per second, with an average latency of 310,652 ns
 
 -Dtime=30 -Dsize=100000 -Dpath=/data/tmp - DblockSizeMB=524288
-Writing 428,833 messages took 30.001 seconds, at a rate of 14,294 per second
-Reading 428,833 messages took 14.407 seconds, at a rate of 29,764 per second
+Writing 608,464 messages took 30.001 seconds, at a rate of 20,281 per second, with an average latency of 49,306 ns
+Reading 608,464 messages took 21.147 seconds, at a rate of 28,773 per second, with an average latency of 34,754 ns
 
 -Dtime=30 -Dsize=10000 -Dpath=/data/tmp - DblockSizeMB=524288
-Writing 4,314,082 messages took 30.001 seconds, at a rate of 143,798 per second
-Reading 4,314,082 messages took 20.516 seconds, at a rate of 210,279 per second
+Writing 4,753,747 messages took 30.001 seconds, at a rate of 158,451 per second, with an average latency of 6,311 ns
+Reading 4,753,747 messages took 27.304 seconds, at a rate of 174,101 per second, with an average latency of 5,743 ns
 
 -Dtime=30 -Dsize=1000 -Dpath=/data/tmp - DblockSizeMB=524288
-Writing 41,240,632 messages took 30.001 seconds, at a rate of 1,374,647 per second
-Reading 41,240,632 messages took 17.056 seconds, at a rate of 2,418,022 per second
+Writing 42,526,957 messages took 30.001 seconds, at a rate of 1,417,508 per second, with an average latency of 705 ns
+Reading 42,526,957 messages took 16.796 seconds, at a rate of 2,532,011 per second, with an average latency of 394 ns
 
 -Dtime=30 -Dsize=300 -Dpath=/data/tmp - DblockSizeMB=524288
-Writing 107,668,614 messages took 30.001 seconds, at a rate of 3,588,840 per second
-Reading 107,668,614 messages took 5.235 seconds, at a rate of 20,567,778 per second
+Writing 107,971,666 messages took 30.001 seconds, at a rate of 3,598,924 per second, with an average latency of 277 ns
+Reading 107,971,666 messages took 5.556 seconds, at a rate of 19,433,454 per second, with an average latency of 51 ns
 
 -Dtime=30 -Dsize=100 -Dpath=/data/tmp - DblockSizeMB=524288
-Writing 188,511,678 messages took 30.001 seconds, at a rate of 6,283,489 per second
-Reading 188,511,678 messages took 6.509 seconds, at a rate of 28,959,528 per second
+Writing 202,468,247 messages took 30.001 seconds, at a rate of 6,748,769 per second, with an average latency of 148 ns
+Reading 202,468,247 messages took 7.561 seconds, at a rate of 26,776,278 per second, with an average latency of 37 ns
  */
 public class ThroughputPerfMain {
     private static final int TIME = Integer.getInteger("time", 30);
@@ -51,7 +53,7 @@ public class ThroughputPerfMain {
         long start = System.nanoTime();
         long count = 0;
         nbs = BytesStore.nativeStoreWithFixedCapacity(SIZE);
-
+        AffinityLock lock = AffinityLock.acquireCore();
         try (ChronicleQueue q = ChronicleQueue.singleBuilder(base)
                 .rollCycle(RollCycles.LARGE_HOURLY_XSPARSE)
                 .blockSize(blockSizeMB << 20)
@@ -70,7 +72,7 @@ public class ThroughputPerfMain {
         long mid = System.nanoTime();
         long time1 = mid - start;
 
-        Bytes<?> bytes = Bytes.allocateElasticDirect(64);
+        Bytes<?> bytes = Bytes.allocateElasticDirect(SIZE);
         try (ChronicleQueue q = ChronicleQueue.singleBuilder(base)
                 .rollCycle(RollCycles.LARGE_HOURLY_XSPARSE)
                 .blockSize(blockSizeMB << 20)
@@ -86,16 +88,16 @@ public class ThroughputPerfMain {
         bytes.releaseLast();
         long end = System.nanoTime();
         long time2 = end - mid;
+        lock.close();
 
         System.out.println("-Dtime=" + TIME +
                 " -Dsize=" + SIZE +
                 " -Dpath=" + PATH +
                 " - DblockSizeMB=" + blockSizeMB);
-        System.out.printf("Writing %,d messages took %.3f seconds, at a rate of %,d per second%n",
-                count, time1 / 1e9, (long) (1e9 * count / time1));
-        System.out.printf("Reading %,d messages took %.3f seconds, at a rate of %,d per second%n",
-                count, time2 / 1e9, (long) (1e9 * count / time2));
-
+        System.out.printf("Writing %,d messages took %.3f seconds, at a rate of %,d per second, with an average latency of %,d ns%n",
+                count, time1 / 1e9, (long) (1e9 * count / time1), time1/count);
+        System.out.printf("Reading %,d messages took %.3f seconds, at a rate of %,d per second, with an average latency of %,d ns%n",
+                count, time2 / 1e9, (long) (1e9 * count / time2), time2/count);
         BackgroundResourceReleaser.releasePendingResources();
         System.gc(); // make sure its cleaned up for windows to delete.
         IOTools.deleteDirWithFiles(base, 2);
