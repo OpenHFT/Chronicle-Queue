120b01e98fb1e148e3ac0d82e5abd7de0424ff8f
Peter Lawrey <peter.lawrey@chronicle.software>
2022-07-20 11:52:09 +0100

    Add logging of parameters

--- src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
+++ src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
@@ -17,6 +17,26 @@ Ryzen 9 5950X with Corsair MP600 PRO XT
 -Dtime=30 -Dsize=1000000 -Dpath=/data/tmp - DblockSizeMB=524288
 Writing 44,749 messages took 30.001 seconds, at a rate of 1,491 per second
 Reading 44,749 messages took 14.461 seconds, at a rate of 3,094 per second
+
+-Dtime=30 -Dsize=100000 -Dpath=/data/tmp - DblockSizeMB=524288
+Writing 428,833 messages took 30.001 seconds, at a rate of 14,294 per second
+Reading 428,833 messages took 14.407 seconds, at a rate of 29,764 per second
+
+-Dtime=30 -Dsize=10000 -Dpath=/data/tmp - DblockSizeMB=524288
+Writing 4,314,082 messages took 30.001 seconds, at a rate of 143,798 per second
+Reading 4,314,082 messages took 20.516 seconds, at a rate of 210,279 per second
+
+-Dtime=30 -Dsize=1000 -Dpath=/data/tmp - DblockSizeMB=524288
+Writing 41,240,632 messages took 30.001 seconds, at a rate of 1,374,647 per second
+Reading 41,240,632 messages took 17.056 seconds, at a rate of 2,418,022 per second
+
+-Dtime=30 -Dsize=300 -Dpath=/data/tmp - DblockSizeMB=524288
+Writing 107,668,614 messages took 30.001 seconds, at a rate of 3,588,840 per second
+Reading 107,668,614 messages took 5.235 seconds, at a rate of 20,567,778 per second
+
+-Dtime=30 -Dsize=100 -Dpath=/data/tmp - DblockSizeMB=524288
+Writing 188,511,678 messages took 30.001 seconds, at a rate of 6,283,489 per second
+Reading 188,511,678 messages took 6.509 seconds, at a rate of 28,959,528 per second
  */
 public class ThroughputPerfMain {
     private static final int TIME = Integer.getInteger("time", 30);
