92a50208a6f44bf2b16d6963184b09a998335b5c
Jerry Shea <jerry.shea@chronicle.software>
2022-07-20 18:29:42 +0800

    simplify ThroughputPerfMain (#1127)

--- src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
+++ src/test/java/net/openhft/chronicle/queue/bench/ThroughputPerfMain.java
@@ -2,8 +2,8 @@ package net.openhft.chronicle.queue.bench;
 
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.bytes.BytesStore;
-import net.openhft.chronicle.bytes.MappedBytes;
 import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.core.io.BackgroundResourceReleaser;
 import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.core.util.Time;
 import net.openhft.chronicle.queue.ChronicleQueue;
@@ -11,9 +11,6 @@ import net.openhft.chronicle.queue.ExcerptAppender;
 import net.openhft.chronicle.queue.ExcerptTailer;
 import net.openhft.chronicle.queue.RollCycles;
 import net.openhft.chronicle.wire.DocumentContext;
-import net.openhft.chronicle.wire.Wire;
-
-import static net.openhft.chronicle.core.UnsafeMemory.MEMORY;
 
 /*
 ARMv7 Processor rev 10 (v7l), Quad 1 GHz.
@@ -21,11 +18,12 @@ Writing 36,699,566 messages took 20.297 seconds, at a rate of 1,808,092 per seco
 Reading 36,699,566 messages took 28.283 seconds, at a rate of 1,297,576 per second
  */
 public class ThroughputPerfMain {
-    static final int time = Integer.getInteger("time", 20);
-    static final int size = Integer.getInteger("size", 40);
-    static final String path = System.getProperty("path", OS.TMP);
+    private static final int time = Integer.getInteger("time", 20);
+    private static final int size = Integer.getInteger("size", 40);
+    private static final String path = System.getProperty("path", OS.TMP);
+    private static final long blockSize = Long.getLong("blockSize", OS.is64Bit() ? 4L << 30 : 256L << 20);
 
-    static BytesStore nbs;
+    private static BytesStore nbs;
 
     public static void main(String[] args) {
         String base = path + "/delete-" + Time.uniqueId() + ".me";
@@ -33,36 +31,17 @@ public class ThroughputPerfMain {
         long count = 0;
         nbs = BytesStore.nativeStoreWithFixedCapacity(size);
 
-        long blockSize = OS.is64Bit() ? 4L << 30 : 256L << 20;
         try (ChronicleQueue q = ChronicleQueue.singleBuilder(base)
                 .rollCycle(RollCycles.LARGE_HOURLY_XSPARSE)
                 .blockSize(blockSize)
                 .build()) {
 
             ExcerptAppender appender = q.acquireAppender();
-            long lastIndex = -1;
             do {
-                int defaultIndexSpacing = q.rollCycle().defaultIndexSpacing();
-                Wire wire = appender.wire();
-                int writeCount = (int) (defaultIndexSpacing - (lastIndex & (defaultIndexSpacing - 1)) - 1);
-                if (wire != null && writeCount > 0) {
-                    MappedBytes bytes = (MappedBytes) wire.bytes();
-                    long address = bytes.addressForWrite(bytes.writePosition());
-                    long bstart = bytes.start();
-                    long bcap = bytes.realCapacity();
-                    long canWrite = bcap - (bytes.writePosition() - bstart);
-                    long lengthCount = writeMessages(address, canWrite, writeCount);
-                    bytes.writeSkip((int) lengthCount);
-                    lastIndex += lengthCount >> 32;
-                    count += lengthCount >> 32;
-
-                } else {
-                    try (DocumentContext dc = appender.writingDocument()) {
-                        dc.wire().bytes().write(nbs);
-                    }
-                    lastIndex = appender.lastIndexAppended();
-                    count++;
+                try (DocumentContext dc = appender.writingDocument()) {
+                    dc.wire().bytes().write(nbs);
                 }
+                count++;
             } while (start + time * 1e9 > System.nanoTime());
         }
 
@@ -92,24 +71,8 @@ public class ThroughputPerfMain {
         System.out.printf("Reading %,d messages took %.3f seconds, at a rate of %,d per second%n",
                 count, time2 / 1e9, (long) (1e9 * count / time2));
 
+        BackgroundResourceReleaser.releasePendingResources();
         System.gc(); // make sure its cleaned up for windows to delete.
         IOTools.deleteDirWithFiles(base, 2);
-    }
-
-    @SuppressWarnings("restriction")
-    private static long writeMessages(long address, long canWrite, int writeCount) {
-        long length = 0;
-        long count = 0;
-//        writeCount = writeCount == 1 ? 1 : ThreadLocalRandom.current().nextInt(writeCount-1)+1;
-        long fromAddress = nbs.addressForRead(0);
-        while (writeCount > count && length + 4 + size <= canWrite) {
-            MEMORY.copyMemory(fromAddress, address + 4, size);
-            MEMORY.writeOrderedInt(address, size);
-            address += 4 + size;
-            length += 4 + size;
-            count++;
-        }
-//        System.out.println("w "+count+" "+length);
-        return (count << 32) | length;
     }
 }
