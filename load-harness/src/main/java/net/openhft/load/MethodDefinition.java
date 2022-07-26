cc31383db416973daa3bdabf2f5eb9e1e266420f
Peter Lawrey <peter.lawrey@chronicle.software>
2022-07-26 10:58:23 +0100

    Clean up old projects

--- load-harness/src/main/java/net/openhft/load/MethodDefinition.java
+++
@@ -1,7 +0,0 @@
-package net.openhft.load;
-
-import net.openhft.load.messages.EightyByteMessage;
-
-public interface MethodDefinition {
-    void onEightyByteMessage(final EightyByteMessage message);
-}
