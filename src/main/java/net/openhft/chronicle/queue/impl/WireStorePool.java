0a1ed7d49d7cba565c367fe6bb7aff8112e6eb7e
Peter Lawrey <peter.lawrey@chronicle.software>
2022-08-03 14:33:18 +0100

    Added Copyright headers

--- src/main/java/net/openhft/chronicle/queue/impl/WireStorePool.java
+++ src/main/java/net/openhft/chronicle/queue/impl/WireStorePool.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2016-2020 chronicle.software
  *
- * https://chronicle.software
+ *       https://chronicle.software
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
