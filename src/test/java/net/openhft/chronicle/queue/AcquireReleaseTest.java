7e529c1bb30c29b221331c80c962fcf54024a639
Nick Tindall <nick.tindall@chronicle.software>
2022-09-15 17:30:15 +1000

    Fix flakiness in AcquireReleaseTest

--- src/test/java/net/openhft/chronicle/queue/AcquireReleaseTest.java
+++ src/test/java/net/openhft/chronicle/queue/AcquireReleaseTest.java
@@ -120,9 +120,8 @@ public class AcquireReleaseTest extends ChronicleQueueTestBase {
 
             // other appender is created
             CompletableFuture.runAsync(queue::acquireAppender).get();  // Here store is Acquired twice (second time in cleanupStoreFilesWithNoData())
-
-            Thread.sleep(10); // Let BackgroundResourceReleaser run onAcquired()/onReleased() callbacks
         }
+        BackgroundResourceReleaser.releasePendingResources();
 
         // Once is called when creating first appender, and twice when creating second appender ( extra time in cleanupStoreFilesWithNoData())
         assertEquals(3, acount.get());
