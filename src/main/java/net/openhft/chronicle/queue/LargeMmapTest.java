5b117596b8603f671d59d32d9443bf0ce13d0df4
Ivan Rakov <ivan.glukos@gmail.com>
2023-03-16 14:21:53 +0400

    up-to-date mmap test

--- src/main/java/net/openhft/chronicle/queue/LargeMmapTest.java
+++ src/main/java/net/openhft/chronicle/queue/LargeMmapTest.java
@@ -6,23 +6,25 @@ import net.openhft.chronicle.bytes.BytesStore;
 import net.openhft.chronicle.bytes.OnHeapBytes;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class LargeMmapTest {
     private static final Logger log = LoggerFactory.getLogger(LargeMmapTest.class);
+    private static final boolean FORCE_SAFEPOINTS = Boolean.getBoolean("force.safepoints");
+    private static final boolean SCATTER_BLOCK_SIZE = Boolean.getBoolean("scatter.block.size");
+    private static final boolean SHARE_READ_QUEUES = Boolean.getBoolean("share.read.queues");
+    public static final long BLOCK_SIZE = 4L * 1024 * 1024 * 1024;
 
     public static void main(String[] args) {
         new Thread("writer") {
             @Override
             public void run() {
                 try (final AffinityLock lock = AffinityLock.acquireLock(System.getProperty("writer.cpu"));
-                     final SingleChronicleQueue q = SingleChronicleQueueBuilder.builder()
-                             .path("book-builder-test")
-                             .sourceId(2)
-                             .blockSize(4L * 1024 * 1024 * 1024)
-                             .rollCycle(RollCycles.HUGE_DAILY_XSPARSE)
-                             .build()) {
+                     final SingleChronicleQueue q = createQueue()) {
                     final ExcerptAppender excerptAppender = q.acquireAppender();
 
                     final byte[] kilo = new byte[1024];
@@ -39,17 +41,44 @@ public class LargeMmapTest {
                     long now = System.currentTimeMillis();
 
                     while (!Thread.currentThread().isInterrupted()) {
+                        if (FORCE_SAFEPOINTS)
+                            safepoint();
+
                         long expectedKilosWritten = (System.currentTimeMillis() - now) * 1024 / 1000 * mbps;
 
                         while (kilosWritten < expectedKilosWritten) {
                             kilosWritten++;
 
+                            final long start = System.nanoTime();
+
                             excerptAppender.writeBytes(store);
+
+                            final long end = System.nanoTime();
+
+                            if (end - start > 200_000L)
+                                log.warn("writer stuck for " + ((end - start) / 1_000) + "us");
+
                         }
                     }
                 }
             }
         }.start();
+
+        if (System.getProperty("pretoucher.cpu") != null) {
+            new Thread("pretoucher") {
+                @Override
+                public void run() {
+                    try (final AffinityLock lock = AffinityLock.acquireLock(System.getProperty("pretoucher.cpu"));
+                         final SingleChronicleQueue q = createQueue()) {
+                        final ExcerptAppender excerptAppender = q.acquireAppender();
+
+                        while (!Thread.currentThread().isInterrupted()) {
+                            excerptAppender.pretouch();
+                        }
+                    }
+                }
+            }.start();
+        }
 
         runReader("reader");
         runReader("reader2");
@@ -64,6 +93,9 @@ public class LargeMmapTest {
 
                     long prev = System.nanoTime();
                     while (!Thread.currentThread().isInterrupted()) {
+                        if (FORCE_SAFEPOINTS)
+                            safepoint();
+
                         final long cur = System.nanoTime();
 
                         if (cur - prev > 200_000L)
@@ -76,40 +108,70 @@ public class LargeMmapTest {
         }.start();
     }
 
+    private static void safepoint() {
+        if (!Thread.currentThread().isAlive())
+            throw new IllegalStateException("");
+    }
+
     private static void runReader(String readerName) {
         if (System.getProperty(readerName + ".cpu") != null) {
             new Thread(readerName) {
                 @Override
                 public void run() {
                     try (final AffinityLock lock = AffinityLock.acquireLock(System.getProperty(readerName + ".cpu"));
-                         final SingleChronicleQueue q1 = SingleChronicleQueueBuilder.builder()
-                                 .path("book-builder-test")
-                                 .sourceId(2)
-                                 .blockSize(4L * 1024 * 1024 * 1024)
-                                 .rollCycle(RollCycles.HUGE_DAILY_XSPARSE)
-                                 .build();
+                         final SingleChronicleQueue q1 = createQueue();
                          final ExcerptTailer tailer1 = q1.createTailer();
-                         final SingleChronicleQueue q2 = SingleChronicleQueueBuilder.builder()
-                                 .path("book-builder-test")
-                                 .sourceId(2)
-                                 .blockSize(4L * 1024 * 1024 * 1024)
-                                 .rollCycle(RollCycles.HUGE_DAILY_XSPARSE)
-                                 .build();
+                         final SingleChronicleQueue q2 = createQueue();
                          final ExcerptTailer tailer2 = q2.createTailer()
                     ) {
 
                         final OnHeapBytes b = Bytes.allocateElasticOnHeap();
 
                         while (!Thread.currentThread().isInterrupted()) {
+                            if (FORCE_SAFEPOINTS)
+                                safepoint();
+
+                            final long start = System.nanoTime();
+
                             tailer1.readBytes(b);
                             b.clear();
 
                             tailer2.readBytes(b);
                             b.clear();
+
+                            final long end = System.nanoTime();
+
+                            if (end - start > 200_000L)
+                                log.warn(readerName + " stuck for " + ((end - start) / 1_000) + "us");
                         }
                     }
                 }
             }.start();
+        }
+    }
+
+    private static SingleChronicleQueue instance;
+
+    @NotNull
+    private synchronized static SingleChronicleQueue createQueue() {
+        if (SHARE_READ_QUEUES) {
+            if (instance == null) {
+                instance = SingleChronicleQueueBuilder.builder()
+                        .path("book-builder-test")
+                        .sourceId(2)
+                        .blockSize(SCATTER_BLOCK_SIZE ? ThreadLocalRandom.current().nextLong(BLOCK_SIZE / 2, BLOCK_SIZE) : BLOCK_SIZE)
+                        .rollCycle(RollCycles.HUGE_DAILY_XSPARSE)
+                        .build();
+            }
+
+            return instance;
+        } else {
+            return SingleChronicleQueueBuilder.builder()
+                    .path("book-builder-test")
+                    .sourceId(2)
+                    .blockSize(SCATTER_BLOCK_SIZE ? ThreadLocalRandom.current().nextLong(BLOCK_SIZE / 2, BLOCK_SIZE) : BLOCK_SIZE)
+                    .rollCycle(RollCycles.HUGE_DAILY_XSPARSE)
+                    .build();
         }
     }
 }
