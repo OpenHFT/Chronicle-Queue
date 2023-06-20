323bcbc3ac078d0be57728abc6cdd86e212ff87c
Nick Tindall <nick.tindall@chronicle.software>
2023-06-20 12:09:31 +1000

    Add createAppender. Fixes #1361

--- src/test/java/net/openhft/chronicle/queue/impl/single/SingleChronicleQueueTest.java
+++ src/test/java/net/openhft/chronicle/queue/impl/single/SingleChronicleQueueTest.java
@@ -145,6 +145,27 @@ public class SingleChronicleQueueTest extends ChronicleQueueTestBase {
                 appenderListenerDump.toString());
     }
 
+    @Test
+    public void createAppenderWillReturnANewAppenderEachTime() {
+        try (final ChronicleQueue queue = builder(getTmpDir(), wireType).build();
+             final ExcerptAppender appender1 = queue.createAppender();
+             final ExcerptAppender appender2 = queue.createAppender()) {
+            assertNotSame(appender1, appender2);
+        }
+    }
+
+    @Test
+    public void createAppenderWillThrowWhenQueueIsReadOnly() {
+        final File queueDir = getTmpDir();
+        try (final ChronicleQueue queue = builder(queueDir, wireType).build();
+             final ExcerptAppender appender = queue.createAppender()) {
+            appender.writeText("hello world");
+            try (final ChronicleQueue readOnlyQueue = builder(queueDir, wireType).readOnly(true).build()) {
+                assertThrows(IllegalStateException.class, readOnlyQueue::createAppender);
+            }
+        }
+    }
+
     @NotNull
     protected String expectedForTestAppend() {
         return "" +
