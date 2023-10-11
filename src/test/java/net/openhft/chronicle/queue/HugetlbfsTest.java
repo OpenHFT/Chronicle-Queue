a3396d0f4c815ea704d7a2c04d411b1607693b39
Tom Dellmann <tom.dellmann@chronicle.software>
2023-10-11 19:14:12 +0100

    Tweak acceptance tests

--- src/test/java/net/openhft/chronicle/queue/HugetlbfsTest.java
+++ src/test/java/net/openhft/chronicle/queue/HugetlbfsTest.java
@@ -1,21 +1,67 @@
 package net.openhft.chronicle.queue;
 
 import net.openhft.chronicle.bytes.MappedBytes;
+import net.openhft.chronicle.bytes.MappedFile;
+import net.openhft.chronicle.bytes.internal.ChunkedMappedBytes;
 import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.core.io.IOTools;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import static org.junit.Assert.assertEquals;
 
 public class HugetlbfsTest {
 
     @Test
-    public void mappedBytes() throws IOException {
-        File file = new File("/mnt/huge/test/testfile");
+    public void mappedBytes_tmp() throws IOException {
+        File file = new File("/tmp/testfile");
         file.createNewFile();
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
         int hugePageSize = 2048 * 1024;
-        MappedBytes bytes = MappedBytes.mappedBytes(file, OS.pageSize(), OS.pageSize(), hugePageSize, true);
+        randomAccessFile.setLength(hugePageSize);
+        MappedBytes bytes = MappedBytes.mappedBytes(file, OS.pageSize(), OS.pageSize(), hugePageSize, false);
+        bytes.writeVolatileInt(0, 1);
         int value = bytes.readVolatileInt(0);
+    }
+
+    @Test
+    public void mappedBytes_hugetlbfs() throws IOException {
+        File file = new File("/mnt/huge/tom/testfile");
+        file.createNewFile();
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+        int hugePageSize = 2048 * 1024;
+        randomAccessFile.setLength(hugePageSize);
+        MappedBytes bytes = MappedBytes.mappedBytes(file, OS.pageSize(), OS.pageSize(), false);
+        bytes.writeVolatileInt(0, 33);
+        assertEquals(33, bytes.readVolatileInt(0));
+    }
+
+    @Test
+    public void queue_tmp() {
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.single().path("/tmp/test-queue").build();
+             ExcerptAppender appender = queue.createAppender();
+             ExcerptTailer tailer = queue.createTailer()) {
+            appender.writeText("1");
+            assertEquals("1", tailer.readText());
+        }
+    }
+
+    @Test
+    public void queue_hugetlbfs() {
+        String path = "/mnt/huge/tom/test-queue";
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.single().path(path).build();
+             ExcerptAppender appender = queue.createAppender();
+             ExcerptTailer tailer = queue.createTailer()) {
+            appender.writeText("1");
+            assertEquals("1", tailer.readText());
+        }
+        IOTools.deleteDirWithFiles(path);
     }
 
 }
