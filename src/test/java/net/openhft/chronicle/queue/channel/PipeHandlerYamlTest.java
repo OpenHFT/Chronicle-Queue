b2245fe509ebe7b5f1fb2cb64916ac177d301b86
Tom Dellmann <tom.dellmann@chronicle.software>
2023-10-19 10:20:01 +0100

    hugetlbfs: Fix PipeHandler tests

--- src/test/java/net/openhft/chronicle/queue/channel/PipeHandlerYamlTest.java
+++ src/test/java/net/openhft/chronicle/queue/channel/PipeHandlerYamlTest.java
@@ -1,17 +1,20 @@
 package net.openhft.chronicle.queue.channel;
 
+import net.openhft.chronicle.bytes.PageUtil;
 import net.openhft.chronicle.core.io.IOTools;
 import net.openhft.chronicle.queue.QueueTestCommon;
 import net.openhft.chronicle.wire.utils.YamlTester;
 import org.junit.Test;
 
 import static net.openhft.chronicle.queue.channel.PubSubHandlerTest.createTargetDir;
+import static org.junit.Assume.assumeFalse;
 
 public class PipeHandlerYamlTest extends QueueTestCommon {
     @Test
     public void yamlTest() {
         ignoreException("Timeout on ");
         String tmpDir = createTargetDir("yamlTest");
+        assumeFalse("PipeHandler not supported on hugetlbfs", PageUtil.isHugePage(tmpDir));
 
         final String qname = "test-q-yaml";
         final PipeHandler handler = new PipeHandler().subscribe(qname).publish(qname);
