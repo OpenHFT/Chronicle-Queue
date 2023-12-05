327d47b2dc74c177ca70df03bab9ad97e479eb09
Tom Dellmann <tom.dellmann@chronicle.software>
2023-12-05 18:05:57 +0000

    Fix test name

--- src/test/java/net/openhft/chronicle/queue/NegativeEpochTest.java
+++ src/test/java/net/openhft/chronicle/queue/NegativeEpochTest.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class NegativeEpoch extends QueueTestCommon {
+public class NegativeEpochTest extends QueueTestCommon {
 
     @Test
     public void negativeEpoch() {
