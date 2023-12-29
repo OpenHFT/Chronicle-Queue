70e9c6122de0ff018755b33936e4456754b7f15f
Tom <tom.dellmann@chronicle.software>
2023-12-29 22:20:25 +0000

    tailer.toEnd performance for backwards tailers (#1484) fixes #1483

--- src/main/java/net/openhft/chronicle/queue/impl/single/StoreTailer.java
+++ src/main/java/net/openhft/chronicle/queue/impl/single/StoreTailer.java
@@ -31,7 +31,6 @@ import net.openhft.chronicle.core.pool.StringBuilderPool;
 import net.openhft.chronicle.core.scoped.ScopedResourcePool;
 import net.openhft.chronicle.queue.*;
 import net.openhft.chronicle.queue.impl.ExcerptContext;
-import net.openhft.chronicle.queue.impl.WireStore;
 import net.openhft.chronicle.queue.impl.WireStorePool;
 import net.openhft.chronicle.queue.impl.single.namedtailer.IndexUpdater;
 import net.openhft.chronicle.wire.*;
@@ -981,7 +980,8 @@ class StoreTailer extends AbstractCloseable
     public ExcerptTailer originalToEnd() {
         throwExceptionIfClosed();
 
-        long index = approximateLastIndex();
+        long approximateLastIndex = approximateLastIndex();
+        long index = approximateLastIndex;
 
         if (index == Long.MIN_VALUE) {
             if (state() == TailerState.CYCLE_NOT_FOUND)
@@ -996,8 +996,7 @@ class StoreTailer extends AbstractCloseable
                 break;
 
             case FOUND:
-                LoopForward:
-                while (true) {
+                LoopForward: while (originalToEndLoopCondition(approximateLastIndex, index)) {
                     final ScanResult result = moveToIndexResult(++index);
                     switch (result) {
                         case NOT_REACHED:
@@ -1016,9 +1015,6 @@ class StoreTailer extends AbstractCloseable
                     }
                 }
 
-                if (direction == BACKWARD)
-                    moveToIndexResult(--index);
-
                 break;
             case NOT_REACHED:
                 throw new NotReachedException("NOT_REACHED index: " + Long.toHexString(index));
@@ -1031,6 +1027,17 @@ class StoreTailer extends AbstractCloseable
 
         return this;
 
+    }
+
+    private boolean originalToEndLoopCondition(long approximateLastIndex, long index) {
+        if (direction == FORWARD) {
+            return true;
+        } else if (direction == BACKWARD) {
+            // Do not let index run past the approximate last index
+            return index < approximateLastIndex;
+        } else {
+            return true;
+        }
     }
 
     @Override
---
+++ src/test/java/net/openhft/chronicle/queue/impl/single/stress/backwardstailer/BackwardsTailerToEndPerfAcceptanceTest.java
@@ -0,0 +1,149 @@
+package net.openhft.chronicle.queue.impl.single.stress.backwardstailer;
+
+import net.openhft.chronicle.core.io.IOTools;
+import net.openhft.chronicle.core.time.SetTimeProvider;
+import net.openhft.chronicle.queue.*;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+import net.openhft.chronicle.queue.rollcycles.LargeRollCycles;
+import net.openhft.chronicle.queue.rollcycles.LegacyRollCycles;
+import net.openhft.chronicle.queue.rollcycles.TestRollCycles;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class BackwardsTailerToEndPerfAcceptanceTest extends QueueTestCommon {
+
+    private static final Logger log = LoggerFactory.getLogger(BackwardsTailerToEndPerfAcceptanceTest.class);
+
+    private final RollCycle rollCycle;
+
+    private final TailerIndexStartPosition tailerIndexStartPosition;
+
+    private long baseline;
+
+    public BackwardsTailerToEndPerfAcceptanceTest(RollCycle rollCycle, TailerIndexStartPosition tailerIndexStartPosition) {
+        this.rollCycle = rollCycle;
+        this.tailerIndexStartPosition = tailerIndexStartPosition;
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data() {
+        final List<Object[]> data = new ArrayList<>();
+        data.add(new Object[]{TestRollCycles.TEST_HOURLY, TailerIndexStartPosition.BEGINNING});
+
+        data.add(new Object[]{LegacyRollCycles.DAILY, TailerIndexStartPosition.BEGINNING});
+        data.add(new Object[]{LegacyRollCycles.DAILY, TailerIndexStartPosition.MIDDLE});
+
+        data.add(new Object[]{LargeRollCycles.LARGE_DAILY, TailerIndexStartPosition.BEGINNING});
+        data.add(new Object[]{TestRollCycles.TEST2_DAILY, TailerIndexStartPosition.BEGINNING});
+        return data;
+    }
+
+    @Before
+    public void before() {
+        // Capture baseline performance of toEnd
+        log.info("rollCycle={}, tailerIndexStartPosition={}", rollCycle, tailerIndexStartPosition);
+        log.info("Capturing baseline performance. rollCycle={}", rollCycle);
+        baseline = runTest(rollCycle.defaultIndexCount() * rollCycle.defaultIndexSpacing() - 1, TailerDirection.BACKWARD, tailerIndexStartPosition, rollCycle);
+        log.info("Baseline performance captured. rollCycle={}", rollCycle);
+    }
+
+    @Test
+    public void fromBeginning() {
+        long duration = runTest(rollCycle.defaultIndexCount() * rollCycle.defaultIndexSpacing() + 1, TailerDirection.BACKWARD, tailerIndexStartPosition, rollCycle);
+        assertReasonablePerformance(duration);
+    }
+
+    @Test
+    public void lessThanBoundary() {
+        long duration = runTest(rollCycle.defaultIndexCount() * rollCycle.defaultIndexSpacing() + 1, TailerDirection.BACKWARD, tailerIndexStartPosition, rollCycle);
+        assertReasonablePerformance(duration);
+    }
+
+    @Test
+    public void onBoundary() {
+        long duration = runTest(rollCycle.defaultIndexCount() * rollCycle.defaultIndexSpacing(), TailerDirection.BACKWARD, tailerIndexStartPosition, rollCycle);
+        assertReasonablePerformance(duration);
+    }
+
+    @Test
+    public void greaterThanBoundary() {
+        long duration = runTest(rollCycle.defaultIndexCount() * rollCycle.defaultIndexSpacing() - 1, TailerDirection.BACKWARD, tailerIndexStartPosition, rollCycle);
+        assertReasonablePerformance(duration);
+    }
+
+    private void assertReasonablePerformance(long duration) {
+        double factor = (double) duration / baseline;
+        long baselineUs = baseline / 1000;
+        long durationUs = duration / 1000;
+        String message = "Performance of this test was " + factor + "x baseline. baseline=" + baselineUs + "us, duration=" + durationUs + "us.";
+        log.info(message);
+        assertTrue(message, factor < 10);
+    }
+
+    private void populateQueue(int entriesToWrite, ExcerptAppender appender) {
+        for (int i = 0; i < entriesToWrite; i++) {
+            appender.writeText("message_" + i);
+
+            if (rollCycle.equals(TestRollCycles.TEST2_DAILY)) {
+                log.info("lastIndexAppended={}", appender.lastIndexAppended());
+            }
+
+        }
+    }
+
+    private long runTest(int entriesToWrite, TailerDirection tailerDirection, TailerIndexStartPosition tailerIndexStartPosition, RollCycle rollCycle) {
+        @NotNull File path = getTmpDir();
+        try (SingleChronicleQueue queue = createQueue(path, rollCycle);
+             ExcerptAppender appender = queue.acquireAppender();
+             ExcerptTailer tailer = queue.createTailer().direction(tailerDirection)) {
+            populateQueue(entriesToWrite, appender);
+
+            // Move tailer to appropriate position
+            switch (tailerIndexStartPosition) {
+                case BEGINNING:
+                    tailer.moveToIndex(0);
+                    break;
+                case MIDDLE:
+                    boolean result = tailer.moveToIndex(appender.lastIndexAppended() / 2);
+                    assertTrue(result);
+                    break;
+                default:
+                    throw new IllegalStateException("Unsupported tailerIndexStartPosition - " + tailerIndexStartPosition);
+            }
+
+            long start = System.nanoTime();
+            tailer.toEnd();
+            long stop = System.nanoTime();
+            long elapsed = stop - start;
+            return elapsed;
+
+        } finally {
+            IOTools.deleteDirWithFiles(path);
+        }
+    }
+
+    @NotNull
+    private SingleChronicleQueue createQueue(File path, RollCycle rollCycle) {
+        SetTimeProvider setTimeProvider = new SetTimeProvider();
+        return SingleChronicleQueueBuilder.builder().timeProvider(setTimeProvider).path(path).rollCycle(rollCycle).build();
+    }
+
+    public enum TailerIndexStartPosition {
+        BEGINNING, MIDDLE
+    }
+
+}
