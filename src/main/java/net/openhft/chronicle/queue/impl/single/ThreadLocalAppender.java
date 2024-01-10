f500698bdd744c204959fdac5ebd548237f6f929
Tom Dellmann <tom.dellmann@chronicle.software>
2024-01-10 12:00:28 +0000

    WIP

--- src/main/java/net/openhft/chronicle/queue/impl/single/ThreadLocalAppender.java
+++ src/main/java/net/openhft/chronicle/queue/impl/single/ThreadLocalAppender.java
@@ -5,10 +5,19 @@ import net.openhft.chronicle.queue.ExcerptAppender;
 public class ThreadLocalAppender {
 
     /**
-     * A thread local appender for the SingleChronicleQueue, this will give you the same thread local instance per thread.
+     * Returns a ExcerptAppender for the given ChronicleQueue that is local to the current Thread.
+     * <p>
+     * An Appender can be used to store new excerpts sequentially to the queue.
+     * <p>
+     * <b>
+     * An Appender is <em>NOT thread-safe</em> and, in addition to that, confined to be used <em>by the creating thread only.</em>.
+     * Sharing an Appender across threads is unsafe and will inevitably lead to errors and unspecified behaviour.
+     * </b>
+     * <p>
+     * This method returns a {@link ThreadLocal} appender, so does not produce any garbage, hence it's safe to simply call
+     * this method every time an appender is needed.
      *
-     * @param queue the SingleChronicleQueue
-     * @return a thread local appender
+     * @return Returns a ExcerptAppender for this ChronicleQueue that is local to the current Thread
      */
     public static ExcerptAppender acquireThreadLocalAppender(SingleChronicleQueue queue) {
         return queue.acquireThreadLocalAppender(queue);
