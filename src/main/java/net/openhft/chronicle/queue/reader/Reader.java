9f7f2102be72f4bed8f8065caa7d92c2315e792f
yevgen.pavlenko <yevgen.pavlenko@chronicle.software>
2024-06-20 12:12:44 +0300

    Fix ChronicleMethodReaderTest.shouldFilterByMultipleInclusionRegex. Improve javadoc

--- src/main/java/net/openhft/chronicle/queue/reader/Reader.java
+++ src/main/java/net/openhft/chronicle/queue/reader/Reader.java
@@ -24,55 +24,175 @@ import org.jetbrains.annotations.NotNull;
 import java.nio.file.Path;
 import java.util.function.Consumer;
 
+/**
+ * The Reader interface provides methods for reading messages from a Chronicle Queue.
+ * It allows for customization of the reading process through various configuration methods, and
+ * creates a Reader with the {@link #create()} method
+ */
 public interface Reader {
 
+    /**
+     * Executes the Reader.
+     */
     void execute();
 
+    /**
+     * Stops the Reader.
+     */
     void stop();
 
+    /**
+     * Sets the message sink for this Reader. If not set, messages are output to stdout.
+     *
+     * @param messageSink A Consumer function that will handle the messages read by this Reader.
+     * @return this
+     */
     Reader withMessageSink(@NotNull Consumer<String> messageSink);
 
+    /**
+     * Sets the base path for this Reader.
+     *
+     * @param path The base path.
+     * @return this
+     */
     Reader withBasePath(@NotNull Path path);
 
+    /**
+     * Adds an inclusion regex for this Reader. These are anded together.
+     *
+     * @param regex The inclusion regex.
+     * @return The Reader instance with the inclusion regex set.
+     */
     Reader withInclusionRegex(@NotNull String regex);
 
+    /**
+     * Adds exclusion regex for this Reader. These are anded together.
+     *
+     * @param regex The exclusion regex.
+     * @return this
+     */
     Reader withExclusionRegex(@NotNull String regex);
 
+    /**
+     * Sets the custom plugin for this Reader. Allows more flexibility than {@link #withMessageSink(Consumer)}
+     *
+     * @param customPlugin The custom plugin.
+     * @return this
+     */
     Reader withCustomPlugin(@NotNull ChronicleReaderPlugin customPlugin);
 
+    /**
+     * Sets the start index for this Reader.
+     *
+     * @param index The start index.
+     * @return this
+     */
     Reader withStartIndex(final long index);
 
+    /**
+     * Sets the content-based limiter for this Reader.
+     *
+     * @param contentBasedLimiter The content-based limiter.
+     * @return this
+     */
     ChronicleReader withContentBasedLimiter(ContentBasedLimiter contentBasedLimiter);
 
+    /**
+     * Sets the argument for this Reader. Used in conjunction with {@link #withBinarySearch(String)}
+     *
+     * @param arg The argument.
+     * @return this
+     */
     Reader withArg(@NotNull String arg);
 
+    /**
+     * Sets the limiter argument for this Reader. Used with {@link #withContentBasedLimiter(ContentBasedLimiter)}
+     *
+     * @param limiterArg The limiter argument.
+     * @return this
+     */
     Reader withLimiterArg(@NotNull String limiterArg);
 
+    /**
+     * Sets the Reader to tail mode.
+     *
+     * @return this
+     */
     Reader tail();
 
+    /**
+     * Sets the maximum number of history records for this Reader.
+     *
+     * @param maxHistoryRecords The maximum number of history records.
+     * @return this
+     */
     Reader historyRecords(final long maxHistoryRecords);
 
     /**
-     * specify method reader interface to use
-     * @param methodReaderInterface interface class name. If empty, a dummy reader is created
+     * Sets the method reader interface for this Reader.
+     *
+     * @param methodReaderInterface The method reader interface class name. If empty, a dummy reader is created.
      * @return this
      */
     Reader asMethodReader(@NotNull String methodReaderInterface);
 
+    /**
+     * Sets the wire type for this Reader.
+     *
+     * @param wireType The wire type.
+     * @return this
+     */
     Reader withWireType(@NotNull WireType wireType);
 
+    /**
+     * Suppresses the display index for this Reader.
+     *
+     * @return this
+     */
     Reader suppressDisplayIndex();
 
+    /**
+     * Sets the binary search for this Reader.
+     *
+     * @param binarySearch The binary search.
+     * @return this
+     */
     Reader withBinarySearch(@NotNull String binarySearch);
 
+    /**
+     * Sets whether to show message history for this Reader.
+     *
+     * @param showMessageHistory Whether to show message history.
+     * @return this
+     */
     Reader showMessageHistory(boolean showMessageHistory);
 
+    /**
+     * Retrieves the argument for this Reader.
+     *
+     * @return The argument.
+     */
     String arg();
 
+    /**
+     * Retrieves the limiter argument for this Reader.
+     *
+     * @return The limiter argument.
+     */
     String limiterArg();
 
+    /**
+     * Retrieves the method reader interface for this Reader.
+     *
+     * @return The method reader interface.
+     */
     Class<?> methodReaderInterface();
 
+    /**
+     * Creates a new Reader instance.
+     *
+     * @return A new Reader instance.
+     */
     static Reader create() {
         return new ChronicleReader();
     }
