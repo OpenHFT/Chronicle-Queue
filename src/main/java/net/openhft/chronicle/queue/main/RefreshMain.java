da50dcf2f8de4f555e46a51d1cd747f69be4b86e
Jerry Shea <jerry.shea@chronicle.software>
2024-09-24 18:58:21 +1000

    RefreshMain

---
+++ src/main/java/net/openhft/chronicle/queue/main/RefreshMain.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016-2022 chronicle.software
+ *
+ *       https://chronicle.software
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.openhft.chronicle.queue.main;
+
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
+import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
+
+import java.io.File;
+
+public final class RefreshMain {
+
+    /**
+     * Call queue.refreshDirectoryListing() on the given queue directory
+     *
+     * @param args the directory
+     */
+    public static void main(String[] args) {
+        final File path = new File(args[0]);
+        if (!path.isDirectory()) {
+            System.err.println("Path argument must be a queue directory");
+            System.exit(1);
+        }
+
+        try (SingleChronicleQueue queue = SingleChronicleQueueBuilder.binary(path).build()) {
+            queue.refreshDirectoryListing();
+        }
+    }
+}
