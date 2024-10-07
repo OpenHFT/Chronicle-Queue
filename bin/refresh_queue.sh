070a7f2018d94a4914552cff1fffdc60239a7a13
chroniclekevinpowe <kevin.powe@chronicle.software>
2024-10-07 12:33:36 +1100

    Moved command line utility documentation to common page

---
+++ bin/refresh_queue.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+PROJECT_DIR="$SCRIPT_DIR/.."
+
+UBER_JAR=$(find ${PROJECT_DIR}/target/chronicle-queue-*-all.jar | tail -n1)
+
+if [[ "$?" != "0" ]]; then
+    echo "Could not find uber-jar, please run 'mvn package' in the project root"
+    exit 1
+fi
+
+java -cp "$UBER_JAR" net.openhft.chronicle.queue.RefreshMain "$@"
