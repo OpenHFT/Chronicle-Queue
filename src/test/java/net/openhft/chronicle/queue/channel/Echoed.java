caa5e0b191b85abaf26e7b41b4fee75ead929e1c
Peter Lawrey <peter.lawrey@chronicle.software>
2024-10-25 13:43:20 +0100

    Move Channels to a dedicated module in Services

--- src/test/java/net/openhft/chronicle/queue/channel/Echoed.java
+++
@@ -1,6 +0,0 @@
-package net.openhft.chronicle.queue.channel;
-
-interface Echoed {
-    //    @MethodId(2)
-    void echoed(DummyData data);
-}
--- src/test/java/net/openhft/chronicle/queue/channel/Says.java
+++
@@ -1,7 +0,0 @@
-package net.openhft.chronicle.queue.channel;
-
-import net.openhft.chronicle.core.io.Syncable;
-
-public interface Says extends Syncable {
-    void say(String say);
-}
