8417eaf0b8a5af25239f292691e2e221e310e391
Peter Lawrey <peter.lawrey@chronicle.software>
2024-10-31 14:55:16 +0000

    Move Channels to a dedicated module in Services (#1627)

--- src/test/java/net/openhft/chronicle/queue/channel/TimedAck.java
+++
@@ -1,9 +0,0 @@
-package net.openhft.chronicle.queue.channel;
-
-import net.openhft.chronicle.wire.converter.NanoTime;
-
-public interface TimedAck {
-    TimedAck ack(@NanoTime long time);
-
-    void flush();
-}
