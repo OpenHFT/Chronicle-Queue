ff129f4928216582f9414767eccb35456a4a92db
Peter Lawrey <peter.lawrey@chronicle.software>
2025-07-25 11:33:45 +0100

    Prevent Reads from Extending the Roll File (#1667)
    * Test to show that the reader doesn't attempt to resize the underlying file incorrectly. This would throw a `java.io.IOException: Resource deadlock avoided` previously
    
    * Reduce test iterations in TableStorePutGetTest as a table store larger than 128K isn't supported. This test becomes flaky beyond that.
    
    * Refactor ReaderResizesFileTest to use a static QUEUE_DIR and improve file handling assertions

---
+++ src/test/java/net/openhft/chronicle/queue/issue/ReaderResizesFileTest.java
@@ -0,0 +1,69 @@
+package net.openhft.chronicle.queue.issue;
+
+import net.openhft.chronicle.bytes.Bytes;
+import net.openhft.chronicle.core.OS;
+import net.openhft.chronicle.core.io.IOTools;
+import net.openhft.chronicle.queue.ChronicleQueue;
+import net.openhft.chronicle.queue.ExcerptAppender;
+import net.openhft.chronicle.queue.ExcerptTailer;
+import net.openhft.chronicle.queue.rollcycles.TestRollCycles;
+import net.openhft.chronicle.wire.DocumentContext;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileLock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReaderResizesFileTest {
+    private static final File QUEUE_DIR = new File(OS.getTarget(), "ReaderResizesFileTest-" + System.nanoTime());
+
+    @After
+    public void cleanup() {
+        IOTools.deleteDirWithFiles(QUEUE_DIR);
+    }
+
+    @Test
+    public void testReaderResizesFile() throws IOException {
+        int blockSize = 64 << 10;
+        try (ChronicleQueue queue = ChronicleQueue.singleBuilder(QUEUE_DIR).rollCycle(TestRollCycles.TEST4_DAILY).blockSize(blockSize).build();
+             ExcerptAppender appender = queue.createAppender();
+             ExcerptTailer tailer = queue.createTailer()) {
+            appender.writeText("Hello World");
+
+            File[] files = QUEUE_DIR.listFiles((d, n) -> n.endsWith(".cq4"));
+            assertNotNull(files, "Queue directory must exist");
+            assertEquals(1, files.length, "Expected exactly one cycle file");
+            File firstFile = files[0];
+
+            assertEquals(blockSize * 2, firstFile.length());
+
+            // Trigger a potential resize by writing more data
+            try (DocumentContext dc = appender.writingDocument()) {
+                Bytes<?> bytes = dc.wire().bytes();
+                bytes.append("More data to increase file size");
+                for (int i = 0; i < blockSize; i += 8)
+                    bytes.writeLong(i);
+            }
+            assertEquals(blockSize * 2, firstFile.length());
+
+            try (RandomAccessFile raf = new RandomAccessFile(firstFile, "rw");
+                 FileLock lockFile = raf.getChannel().lock()) {
+                assertNotNull(lockFile);
+                for (int i = 1; i <= 2; i++) {
+                    try (DocumentContext dc = tailer.readingDocument()) {
+                        assertTrue(dc.isPresent(), "Document should be present");
+                    }
+                    assertEquals(blockSize * 2, firstFile.length());
+                }
+
+                try (DocumentContext dc = tailer.readingDocument()) {
+                    assertFalse(dc.isPresent(), "Document should not be present");
+                }
+            }
+        }
+    }
+}
