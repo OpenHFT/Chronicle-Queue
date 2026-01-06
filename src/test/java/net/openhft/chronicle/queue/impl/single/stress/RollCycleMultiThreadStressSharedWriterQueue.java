a9df7bdb2da6b9c2219ed76da8239556e42c89ce
Peter Lawrey <peter.lawrey@chronicle.software>
2026-01-06 11:30:27 +0000

    Add deprecation warnings to suppress annotations across multiple classes

---
+++ src/test/java/net/openhft/chronicle/queue/impl/single/stress/RollCycleMultiThreadStressSharedWriterQueue.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
+package net.openhft.chronicle.queue.impl.single.stress;
+
+public class RollCycleMultiThreadStressSharedWriterQueue extends RollCycleMultiThreadStressTest {
+
+    public RollCycleMultiThreadStressSharedWriterQueue() {
+        super(StressTestType.SHAREDWRITEQ);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new RollCycleMultiThreadStressSharedWriterQueue().run();
+    }
+}
---
+++ src/test/java/net/openhft/chronicle/queue/impl/single/stress/RollCycleMultiThreadStressVanillaTest.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
+package net.openhft.chronicle.queue.impl.single.stress;
+
+public class RollCycleMultiThreadStressVanillaTest extends RollCycleMultiThreadStressTest {
+
+    public RollCycleMultiThreadStressVanillaTest() {
+        super(StressTestType.VANILLA);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new RollCycleMultiThreadStressVanillaTest().run();
+    }
+}
