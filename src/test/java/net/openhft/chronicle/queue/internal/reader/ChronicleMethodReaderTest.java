08da8f53d25d8698561c939d86b83764131f964d
Peter Lawrey <peter.lawrey@chronicle.software>
2026-08-22 12:03:13 +0100

    QUEUE-1150: document repeated reader filter semantics
    Document that repeated inclusion patterns use AND semantics and exclusions use ANY semantics. Remove the obsolete Ignore import from the restored coverage.

--- src/test/java/net/openhft/chronicle/queue/internal/reader/ChronicleMethodReaderTest.java
+++ src/test/java/net/openhft/chronicle/queue/internal/reader/ChronicleMethodReaderTest.java
@@ -17,7 +17,6 @@ import net.openhft.chronicle.wire.VanillaMethodWriterBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
